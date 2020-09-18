d96f980761c0499dfbcf9d8433153f937eb8c5e0
AnghelLeonard <leoprivacy@yahoo.com>
2020-09-18 16:58:25 +0300

    jOOQ and switch between databases via Maven profiles

---
+++ jOOQ-opensource/jOOQSpringBootMavenProfiles/README.md
@@ -0,0 +1,1 @@
+jOOQ and switch between databases via Maven profiles (run against MySQL and PostgreSQL) 
---
+++ jOOQ-opensource/jOOQSpringBootMavenProfiles/src/main/java/com/classicmodels/service/OrderPaymentService.java
@@ -0,0 +1,43 @@
+package com.classicmodels.service;
+
+import com.classicmodels.repository.ClassicModelsRepository;
+import java.util.List;
+import java.util.Map;
+import jooq.generated.tables.daos.OrderRepository;
+import jooq.generated.tables.pojos.Order;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class OrderPaymentService {
+
+    private final ClassicModelsRepository classicModelsRepository;
+    private final OrderRepository orderRepository;
+
+    public OrderPaymentService(ClassicModelsRepository classicModelsRepository,
+            OrderRepository orderRepository) {
+        this.classicModelsRepository = classicModelsRepository;
+        this.orderRepository = orderRepository;
+    }
+
+    @Transactional(readOnly = true)
+    public Order fetchOrder(Long orderId) {
+
+        /* using jOOQ generated DAOs */
+        return orderRepository.fetchOneByOrderId(orderId);
+    }
+
+    @Transactional(readOnly = true)
+    public List<Order> fetchCustomerOrders(Long customerNumber) {
+
+        /* using jOOQ generated DAOs */
+        return orderRepository.fetchByCustomerNumber(customerNumber);
+    }
+
+    @Transactional(readOnly = true)
+    public Map<Long, List<Order>> fetchAndGroupOrdersByCustomerId() {
+
+        /* using application DAOs */
+        return classicModelsRepository.findAndGroupOrdersByCustomerId();
+    }
+}
