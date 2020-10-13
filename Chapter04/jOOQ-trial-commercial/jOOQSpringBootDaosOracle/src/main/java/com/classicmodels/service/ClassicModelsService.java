598de03d08593eb8ca4a5230ac59166f9cd21059
AnghelLeonard <leoprivacy@yahoo.com>
2020-10-13 18:11:06 +0300

    jOOQ generate DAOs

--- Chapter04/jOOQ-trial-commercial/jOOQSpringBootDaosOracle/src/main/java/com/classicmodels/service/ClassicModelsService.java
+++ Chapter04/jOOQ-trial-commercial/jOOQSpringBootDaosOracle/src/main/java/com/classicmodels/service/ClassicModelsService.java
@@ -4,6 +4,8 @@ import com.classicmodels.pojo.CustomerAndOrder;
 import com.classicmodels.repository.ClassicModelsRepository;
 import java.time.LocalDate;
 import java.util.List;
+import jooq.generated.tables.daos.ManagerRepository;
+import jooq.generated.tables.daos.OrderRepository;
 import jooq.generated.tables.pojos.JooqManager;
 import jooq.generated.tables.pojos.JooqOrder;
 import org.springframework.stereotype.Service;
@@ -12,22 +14,30 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class ClassicModelsService {
 
+    /* our repository */
     private final ClassicModelsRepository classicModelsRepository;
 
-    public ClassicModelsService(ClassicModelsRepository classicModelsRepository) {
+    /* jOOQ generated repositories */
+    private final ManagerRepository managerRepository;
+    private final OrderRepository orderRepository;
+
+    public ClassicModelsService(ClassicModelsRepository classicModelsRepository,
+            ManagerRepository managerRepository, OrderRepository orderRepository) {
         this.classicModelsRepository = classicModelsRepository;
+        this.managerRepository = managerRepository;
+        this.orderRepository = orderRepository;
     }
 
     @Transactional(readOnly = true)
     public JooqManager fetchManager(Long managerId) {
 
-        return classicModelsRepository.findManager(managerId);
+        return managerRepository.fetchOneByManagerId(managerId);
     }
 
     @Transactional(readOnly = true)
     public List<JooqOrder> fetchOrdersByRequiredDate(LocalDate startDate, LocalDate endDate) {
 
-        return classicModelsRepository.findOrdersByRequiredDate(startDate, endDate);
+        return orderRepository.fetchRangeOfRequiredDate(startDate, endDate);
     }
 
     @Transactional(readOnly = true)
