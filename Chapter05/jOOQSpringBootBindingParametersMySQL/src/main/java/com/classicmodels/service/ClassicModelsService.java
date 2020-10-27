6b1b4afb223f96e18ef248167d574df19c05e74b
AnghelLeonard <leoprivacy@yahoo.com>
2020-10-27 15:14:02 +0200

    jOOQ bind values

---
+++ Chapter05/jOOQSpringBootBindingParametersMySQL/src/main/java/com/classicmodels/service/ClassicModelsService.java
@@ -0,0 +1,23 @@
+package com.classicmodels.service;
+
+import com.classicmodels.repository.ClassicModelsRepository;
+import java.util.List;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class ClassicModelsService {
+
+    private final ClassicModelsRepository classicModelsRepository;
+
+    public ClassicModelsService(ClassicModelsRepository classicModelsRepository) {
+        this.classicModelsRepository = classicModelsRepository;
+    }
+
+    @Transactional
+    public void jooqQuery() {
+
+        classicModelsRepository.extractBindValueNamedParams();
+    }
+
+}
