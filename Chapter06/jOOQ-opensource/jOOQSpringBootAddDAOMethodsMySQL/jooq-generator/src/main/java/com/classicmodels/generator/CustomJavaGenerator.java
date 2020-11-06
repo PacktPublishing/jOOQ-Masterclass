100c00deadba057bd41b995f2e5c1a36ce7ad04c
AnghelLeonard <leoprivacy@yahoo.com>
2020-11-06 15:53:20 +0200

    Custom jOOQ DAO

--- Chapter06/jOOQ-opensource/jOOQSpringBootAddDAOMethodsMySQL/jooq-generator/src/main/java/com/classicmodels/generator/CustomJavaGenerator.java
+++ Chapter06/jOOQ-opensource/jOOQSpringBootAddDAOMethodsMySQL/jooq-generator/src/main/java/com/classicmodels/generator/CustomJavaGenerator.java
@@ -12,22 +12,22 @@ public class CustomJavaGenerator extends JavaGenerator {
     @Override
     protected void generateDaoClassFooter(TableDefinition table, JavaWriter out) {
 
-        final String pType = out.ref(getStrategy().getFullJavaClassName(table, Mode.POJO));
+        final String pType = getStrategy().getFullJavaClassName(table, Mode.POJO);
 
         // add a method common to all DAOs
         out.javadoc("Fetch the number of records limited by <code>value</code>");
-        out.println("public %s<%s> findLimitedTo(%s value) {", out.ref(List.class), pType, Integer.class);
+        out.println("public %s<%s> findLimitedTo(%s value) {", List.class, pType, Integer.class);
         out.println("return ctx().selectFrom(%s)", getStrategy().getFullJavaIdentifier(table));
         out.println(".limit(value)");
         out.println(".fetch(mapper());");
         out.println("}");
 
         // add a method specific to Order DAO
-        if (table.getName().equals("order")) {
+        if (table.getName().equals("order")) {            
             out.println();
-            out.javadoc("Fetch orders having status <code>statuVal</code> and order date after <code>orderDateVal</code>");
+            out.javadoc("Fetch orders having status <code>statusVal</code> and order date after <code>orderDateVal</code>");
             out.println("public %s<%s> findOrderByStatusAndOrderDate(%s statusVal, %s orderDateVal) {",
-                     out.ref(List.class), pType, String.class, out.ref(LocalDate.class));
+                     List.class, pType, String.class, LocalDate.class);
             out.println("return ctx().selectFrom(%s)", getStrategy().getFullJavaIdentifier(table));
             out.println(".where(%s.eq(statusVal))", getStrategy().getFullJavaIdentifier(table.getColumn("status")));
             out.println(".and(%s.ge(orderDateVal))", getStrategy().getFullJavaIdentifier(table.getColumn("order_date")));
