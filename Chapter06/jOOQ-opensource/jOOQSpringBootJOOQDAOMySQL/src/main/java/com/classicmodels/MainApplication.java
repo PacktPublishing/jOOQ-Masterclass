332008f3df40e3e0da68d7a22a128f09ff4c6af9
AnghelLeonard <leoprivacy@yahoo.com>
2020-11-08 07:56:20 +0200

    Extending jOOQ generated DAO

--- Chapter06/jOOQ-opensource/jOOQSpringBootJOOQDAOMySQL/src/main/java/com/classicmodels/MainApplication.java
+++ Chapter06/jOOQ-opensource/jOOQSpringBootJOOQDAOMySQL/src/main/java/com/classicmodels/MainApplication.java
@@ -1,9 +1,7 @@
 package com.classicmodels;
 
 import com.classicmodels.service.SalesManagementService;
-import java.time.LocalDate;
 import java.util.List;
-import jooq.generated.tables.pojos.Order;
 import jooq.generated.tables.pojos.Sale;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
@@ -31,20 +29,11 @@ public class MainApplication {
             System.out.println("Fetching sales over 5000:");
             List<Sale> result1 = salesManagementService.fetchSaleAscGtLimit(5000);
             System.out.println(result1);
-            
-            System.out.println("Fetching orders desc by date:");
-            List<Order> result2 = salesManagementService.fetchOrderDescByDate();
-            System.out.println(result2);
-            
+
             /* call jOOQ generated DAOs */
             System.out.println("Fetching sales in 2003:");
             List<Sale> result3 = salesManagementService.fetchSaleByFiscalYear(2003);
             System.out.println(result3);
-
-            System.out.println("Fetching orders between dates, 2003-01-01 and 2003-12-31:");
-            List<Order> result4 = salesManagementService.fetchOrderBetweenDate(
-                    LocalDate.of(2003, 1, 1), LocalDate.of(2003, 12, 31));
-            System.out.println(result4);
         };
     }
 }
--- Chapter06/jOOQ-opensource/jOOQSpringBootJOOQDAOMySQL/src/main/java/com/classicmodels/controller/SalesManagementController.java
+++ Chapter06/jOOQ-opensource/jOOQSpringBootJOOQDAOMySQL/src/main/java/com/classicmodels/controller/SalesManagementController.java
@@ -1,9 +1,7 @@
 package com.classicmodels.controller;
 
 import com.classicmodels.service.SalesManagementService;
-import java.time.LocalDate;
 import java.util.List;
-import jooq.generated.tables.pojos.Order;
 import jooq.generated.tables.pojos.Sale;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -23,25 +21,12 @@ public class SalesManagementController {
     public List<Sale> fetchSaleAscGtLimit(@RequestParam int limit) {
 
         return salesManagementService.fetchSaleAscGtLimit(limit);
-    }    
-
-    @GetMapping("/orderDescByDate")
-    public List<Order> fetchOrderDescByDate() {
-
-        return salesManagementService.fetchOrderDescByDate();
     }
-    
+
     /* call jOOQ generated DAOs */
     @GetMapping("/saleByFiscalYear")
     public List<Sale> fetchSaleByFiscalYear(@RequestParam int year) {
 
         return salesManagementService.fetchSaleByFiscalYear(year);
-    }
-
-    @GetMapping("/orderBetweenDate")
-    public List<Order> fetchOrderBetweenDate(@RequestParam String sd, @RequestParam String ed) {
-
-        return salesManagementService.fetchOrderBetweenDate(
-                LocalDate.parse(sd), LocalDate.parse(ed));
     }
 }
