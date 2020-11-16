59c943948390b2912ecf7cd4e080bf85df0d3399
AnghelLeonard <leoprivacy@yahoo.com>
2020-11-16 18:56:48 +0200

    Use jOOQ to build and execute SQL

--- Chapter03/jOOQ-opensource/Kotlin/Maven/jOOQSpringBootSQLBuilderMySQL/src/main/kotlin/com/classicmodels/MainApplication.kt
+++ Chapter03/jOOQ-opensource/Kotlin/Maven/jOOQSpringBootSQLBuilderMySQL/src/main/kotlin/com/classicmodels/MainApplication.kt
@@ -2,11 +2,10 @@ package com.classicmodels
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-
+ 
 @SpringBootApplication
 class MainApplication
 
-fun main(args: Array<String>) {
-  println("Hello World!")
-  runApplication<MainApplication>(*args)
+fun main(args: Array<String>) {   
+    runApplication<MainApplication>(*args)
 }
