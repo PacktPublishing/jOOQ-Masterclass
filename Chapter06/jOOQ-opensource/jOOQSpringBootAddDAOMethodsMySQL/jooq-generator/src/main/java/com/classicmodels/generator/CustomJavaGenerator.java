4a9413d0019e06bacdcb579308840cf8e8d7adc0
AnghelLeonard <leoprivacy@yahoo.com>
2020-11-16 16:57:09 +0200

    Add methods to jOOQ generated DAO

--- Chapter06/jOOQ-opensource/jOOQSpringBootAddDAOMethodsMySQL/jooq-generator/src/main/java/com/classicmodels/generator/CustomJavaGenerator.java
+++ Chapter06/jOOQ-opensource/jOOQSpringBootAddDAOMethodsMySQL/jooq-generator/src/main/java/com/classicmodels/generator/CustomJavaGenerator.java
@@ -15,24 +15,24 @@ public class CustomJavaGenerator extends JavaGenerator {
         final String pType = getStrategy().getFullJavaClassName(table, Mode.POJO);
 
         // add a method common to all DAOs
-        out.javadoc("Fetch the number of records limited by <code>value</code>");
-        out.println("public %s<%s> findLimitedTo(%s value) {", List.class, pType, Integer.class);
-        out.println("return ctx().selectFrom(%s)", getStrategy().getFullJavaIdentifier(table));
-        out.println(".limit(value)");
-        out.println(".fetch(mapper());");
-        out.println("}");
+        out.tab(1).javadoc("Fetch the number of records limited by <code>value</code>");
+        out.tab(1).println("public %s<%s> findLimitedTo(%s value) {", List.class, pType, Integer.class);
+        out.tab(2).println("return ctx().selectFrom(%s)", getStrategy().getFullJavaIdentifier(table));
+        out.tab(3).println(".limit(value)");
+        out.tab(3).println(".fetch(mapper());");
+        out.tab(1).println("}");
 
         // add a method specific to Order DAO
         if (table.getName().equals("order")) {            
             out.println();
-            out.javadoc("Fetch orders having status <code>statusVal</code> and order date after <code>orderDateVal</code>");
-            out.println("public %s<%s> findOrderByStatusAndOrderDate(%s statusVal, %s orderDateVal) {",
+            out.tab(1).javadoc("Fetch orders having status <code>statusVal</code> and order date after <code>orderDateVal</code>");
+            out.tab(1).println("public %s<%s> findOrderByStatusAndOrderDate(%s statusVal, %s orderDateVal) {",
                      List.class, pType, String.class, LocalDate.class);
-            out.println("return ctx().selectFrom(%s)", getStrategy().getFullJavaIdentifier(table));
-            out.println(".where(%s.eq(statusVal))", getStrategy().getFullJavaIdentifier(table.getColumn("status")));
-            out.println(".and(%s.ge(orderDateVal))", getStrategy().getFullJavaIdentifier(table.getColumn("order_date")));
-            out.println(".fetch(mapper());");
-            out.println("}");
+            out.tab(2).println("return ctx().selectFrom(%s)", getStrategy().getFullJavaIdentifier(table));
+            out.tab(3).println(".where(%s.eq(statusVal))", getStrategy().getFullJavaIdentifier(table.getColumn("status")));
+            out.tab(3).println(".and(%s.ge(orderDateVal))", getStrategy().getFullJavaIdentifier(table.getColumn("order_date")));
+            out.tab(3).println(".fetch(mapper());");
+            out.tab(1).println("}");
         }
     }
 }
