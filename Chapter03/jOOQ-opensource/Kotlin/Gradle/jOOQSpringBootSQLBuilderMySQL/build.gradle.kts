9f5ee49e91c7d45e4aa33f4686e42342ab8d40ba
AnghelLeonard <leoprivacy@yahoo.com>
2020-11-17 19:17:14 +0200

    Use jOOQ to build and execute SQL

---
+++ Chapter03/jOOQ-opensource/Kotlin/Gradle/jOOQSpringBootSQLBuilderMySQL/build.gradle.kts
@@ -0,0 +1,43 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	id("org.springframework.boot") version "2.4.0"
+	id("io.spring.dependency-management") version "1.0.10.RELEASE"
+        id("nu.studer.jooq") version "5.2"
+        id("org.flywaydb.flyway") version "7.2.0"
+	kotlin("jvm") version "1.4.10"
+	kotlin("plugin.spring") version "1.4.10"
+}
+
+group = "com.classicmodels"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_11
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter")
+        implementation("org.springframework.boot:spring-boot-starter-jooq")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+        implementation("mysql:mysql-connector-java")
+        implementation("org.flywaydb:flyway-core")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs = listOf("-Xjsr305=strict")
+		jvmTarget = "11"
+	}
+}
+
+sourceSets.main {
+    java.srcDirs("src/main/java", "src/main/kotlin")
+}
