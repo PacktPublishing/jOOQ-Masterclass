393cc447f3491a72c74a8f7caadc9de4d0dee82c
AnghelLeonard <leoprivacy@yahoo.com>
2020-11-19 18:05:19 +0200

    select

---
+++ Chapter07/jOOQ-opensource/select/jOOQSpringBootCommonlyUsedProjectionsPostgreSQL/src/main/java/com/classicmodels/service/ClassicModelsService.java
@@ -0,0 +1,20 @@
+package com.classicmodels.service;
+
+import com.classicmodels.repository.ClassicModelsRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClassicModelsService {
+
+    private final ClassicModelsRepository classicModelsRepository;
+
+    public ClassicModelsService(ClassicModelsRepository classicModelsRepository) {
+        this.classicModelsRepository = classicModelsRepository;
+    }
+
+    public void callAll() {
+        classicModelsRepository.selectCommonlyUsedValues1();     // EXAMPLE 1
+        classicModelsRepository.selectCommonlyUsedValues2();     // EXAMPLE 2
+        classicModelsRepository.selectCommonlyUsedValues3();     // EXAMPLE 3
+    }
+}
