5e579b26b91c9f51b791be4024d7e42e579c00fc
AnghelLeonard <leoprivacy@yahoo.com>
2020-11-25 14:25:08 +0200

    jOOQ insert ... values

--- Chapter07/jOOQ-trial-commercial/insert/jOOQSpringBootInsertValuesOracle/src/main/java/com/classicmodels/repository/ClassicModelsRepository.java
+++ Chapter07/jOOQ-trial-commercial/insert/jOOQSpringBootInsertValuesOracle/src/main/java/com/classicmodels/repository/ClassicModelsRepository.java
@@ -657,6 +657,10 @@ public class ClassicModelsRepository {
                         .values(srp.valuesRow().fields())
                         .execute()
         );
+        
+        System.out.println("EXAMPLE 9.6 (affected rows): "
+                + ctx.executeInsert(srp)
+        );
     }
 
     // EXAMPLE 10
