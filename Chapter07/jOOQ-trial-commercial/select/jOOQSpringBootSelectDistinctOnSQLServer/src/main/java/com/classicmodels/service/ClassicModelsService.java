6eee7193508f89ebbc2448f7cf2bac7bd546e652
AnghelLeonard <leoprivacy@yahoo.com>
2020-11-26 11:59:17 +0200

    jOOQ distinct

---
+++ Chapter07/jOOQ-trial-commercial/select/jOOQSpringBootSelectDistinctOnSQLServer/src/main/java/com/classicmodels/service/ClassicModelsService.java
@@ -0,0 +1,28 @@
+package com.classicmodels.service;
+
+import com.classicmodels.repository.ClassicModelsRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClassicModelsService {
+
+    private final ClassicModelsRepository classicModelsRepository;
+
+    public ClassicModelsService(ClassicModelsRepository classicModelsRepository) {
+        this.classicModelsRepository = classicModelsRepository;
+    }
+
+    public void callAll() {
+
+        classicModelsRepository.findDistinctOfficesCityCountry();                            // EXAMPLE 1
+        classicModelsRepository.findOfficeDistinctFromAddress();                             // EXAMPLE 2
+        classicModelsRepository.findDistinctAndNotDistinctPaymentDates();                    // EXAMPLE 3
+        classicModelsRepository.findOfficeAndCustomerOfficePostalCodeDistinctCityCountry();  // EXAMPLE 4
+        classicModelsRepository.countPaymentCachingDate();                                   // EXAMPLE 5
+        classicModelsRepository.findProductLineHavingMaxNrOfProducts();                      // EXAMPLE 6 
+        classicModelsRepository.avgSumMinMaxPriceEach();                                     // EXAMPLE 7
+        classicModelsRepository.groupConcatOfficeCountries();                                // EXAMPLE 8
+        classicModelsRepository.findProductsByVendorScale();                                 // EXAMPLE 9
+        classicModelsRepository.findEmployeeNumberOfMaxSalePerFiscalYear();                  // EXAMPLE 10
+    }
+}
