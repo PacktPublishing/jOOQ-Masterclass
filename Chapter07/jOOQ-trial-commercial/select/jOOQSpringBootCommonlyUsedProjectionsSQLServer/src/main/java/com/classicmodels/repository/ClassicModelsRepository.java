2a6353488db30d2ad8e7c709b8189db9c056d7fe
AnghelLeonard <leoprivacy@yahoo.com>
2020-11-26 14:37:23 +0200

    jOOQ commonly used projections

---
+++ Chapter07/jOOQ-trial-commercial/select/jOOQSpringBootCommonlyUsedProjectionsSQLServer/src/main/java/com/classicmodels/repository/ClassicModelsRepository.java
@@ -0,0 +1,86 @@
+package com.classicmodels.repository;
+
+import static jooq.generated.tables.Customer.CUSTOMER;
+import static jooq.generated.tables.Customerdetail.CUSTOMERDETAIL;
+import static jooq.generated.tables.Manager.MANAGER;
+import org.jooq.DSLContext;
+import org.jooq.SQLDialect;
+import org.jooq.impl.DSL;
+import static org.jooq.impl.DSL.count;
+import static org.jooq.impl.DSL.inline;
+import static org.jooq.impl.DSL.val;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+@Transactional(readOnly = true)
+public class ClassicModelsRepository {
+
+    private final DSLContext ctx;
+
+    public ClassicModelsRepository(DSLContext ctx) {
+        this.ctx = ctx;
+    }
+
+    // EXAMPLE 1
+    public void selectCommonlyUsedValues1() {
+
+        // select count(*)
+        System.out.println("EXAMPLE 1.1\n"
+                + ctx.selectCount().fetch()
+        );
+
+        // select 0 [zero]
+        System.out.println("EXAMPLE 1.2\n"
+                + ctx.selectZero().fetch()
+        );
+
+        // select 1 [one]
+        System.out.println("EXAMPLE 1.3\n"
+                + ctx.selectOne().fetch()
+        );
+    }
+
+    // EXAMPLE 2
+    public void selectCommonlyUsedValues2() {
+
+        // select count(*)
+        System.out.println("EXAMPLE 2.1\n"
+                + ctx.select(count()).fetch()
+        );
+
+        // select 0
+        System.out.println("EXAMPLE 2.2\n"
+                + ctx.select(inline(0)).fetch()
+        );
+
+        // select 1
+        System.out.println("EXAMPLE 2.3\n"
+                + ctx.select(val(1)).fetch()
+        );
+    }
+
+    // EXAMPLE 3
+    public void selectCommonlyUsedValues3() {
+
+        // select 1 [one] from [classicmodels].[dbo].[manager]
+        System.out.println("EXAMPLE 3.1\n"
+                + ctx.selectOne().from(MANAGER).fetch()
+        );
+
+        // select 1 [one] from [classicmodels].[dbo].[customer], [classicmodels].[dbo].[customerdetail]
+        System.out.println("EXAMPLE 3.2\n"
+                + ctx.selectOne().from(CUSTOMER, CUSTOMERDETAIL).fetch()
+        );
+
+        // select 1 [one] from [classicmodels].[dbo].[customer] [c], [classicmodels].[dbo].[customerdetail] [cd]
+        System.out.println("EXAMPLE 3.3\n"
+                + ctx.selectOne().from(CUSTOMER.as("c"), CUSTOMERDETAIL.as("cd")).fetch()
+        );
+
+        // select 1 [one]
+        System.out.println("EXAMPLE 3.4\n"
+                + DSL.using(SQLDialect.SQLSERVER).selectOne().getSQL()
+        );
+    }
+}
