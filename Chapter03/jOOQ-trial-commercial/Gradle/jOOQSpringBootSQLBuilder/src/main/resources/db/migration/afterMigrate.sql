317c592adb6a2a9ea2e4ec8b25274dccf39f97b9
AnghelLeonard <leoprivacy@yahoo.com>
2020-11-27 07:42:37 +0200

    Use jOOQ to build and execute SQL

--- Chapter03/jOOQ-trial-commercial/Gradle/jOOQSpringBootSQLBuilder/src/main/resources/db/migration/afterMigrate.sql
+++ Chapter03/jOOQ-trial-commercial/Gradle/jOOQSpringBootSQLBuilder/src/main/resources/db/migration/afterMigrate.sql
@@ -31,7 +31,29 @@ insert /*+ ignore_row_on_dupkey_index(office(office_code)) */ into office(office
 
  select '6','Sydney','+61 2 9264 2451','5-11 Wentworth Avenue','Floor --2',NULL,'Australia','NSW 2010','APAC' from dual union all 
 
- select '7','London','+44 20 7877 2041','25 Old Broad Street','Level 7',NULL,'UK','EC2N 1HN','EMEA' from dual;
+ select '7','London','+44 20 7877 2041','25 Old Broad Street','Level 7',NULL,'UK','EC2N 1HN','EMEA' from dual union all
+ 
+ select '8',NULL,'+44 20 1827 21411','25 Hum Street','Level 2',NULL,'USA','EC2N 1HN','EMEA' from dual union all 
+
+ select '9','Bucharest','+44 20 1827 21411','22 DN1','Level 12',NULL,NULL,'EC2N 1HN','NA' from dual union all
+
+ select '10',NULL,'+44 20 1827 21411','12 Home','Level 22',NULL,NULL,'EC2N 1HN','NA' from dual union all
+
+ select '11','Paris','+32 12 713 4304','43 Rue 2',NULL,NULL,'France','25017','EMEA' from dual union all
+
+ select '12','Tokyo','+81 33 224 3444','4-1 Kioicho',NULL,'Koil-Ku','Japan','101-6578','Japan' from dual;
+
+/*Data for the table `department` */
+
+insert /*+ ignore_row_on_dupkey_index(department(department_id)) */ into department(department_id,name,phone,code,office_code) 
+
+select '1','Lumber','-int 4782','1333','1' from dual union all
+
+select '2','Sales','-int 41233','1441','1' from dual union all
+
+select '3','Accounting','-int 8233','2311','2' from dual union all
+
+select '4','Finance','-int 4421','3222','3' from dual;
 
 /*Data for the table `manager` */
 
@@ -114,6 +136,14 @@ insert /*+ ignore_row_on_dupkey_index(employee(employee_number)) */ into employe
 /*Data for the table `customer` */
 
 insert /*+ ignore_row_on_dupkey_index(customer(customer_number)) */ into customer(customer_number,customer_name,contact_last_name,contact_first_name,phone,sales_rep_employee_number,credit_limit) 
+
+ select 99,'Australian Home','Paoule','Sart ','40.11.2555',1370,'21000.00' from dual union all
+
+ select 100,'Joliyon','Schmitt','Rue ','10.22.2535',1370,'21000.00' from dual union all
+
+ select 101,'Marquez Xioa','Calor','Sar ','`11.12.2525',1370,'21000.00' from dual union all
+
+ select 102,'Falafel 3','Hor','Carine ','20.12.2525',1370,'21000.00' from dual union all
 
  select 103,'Atelier graphique','Schmitt','Carine ','40.32.2555',1370,'21000.00' from dual union all 
 
@@ -363,6 +393,14 @@ insert /*+ ignore_row_on_dupkey_index(customer(customer_number)) */ into custome
 
 insert /*+ ignore_row_on_dupkey_index(customerdetail(customer_number)) */ into customerdetail(customer_number,address_line_first,address_line_second,city,state,postal_code,country) 
 
+ select 99,'32, Avenue 90',NULL, 'Paris' ,NULL,'43000','France' from dual union all
+
+ select 100,'51, Avenue 3',NULL, NULL ,NULL,'43000',NULL from dual union all
+
+ select 101,'51, St 5',NULL, NULL ,NULL,'44000','USA' from dual union all
+
+ select 102,'51, St AQ',NULL, 'Bucharest' ,NULL,'12000',NULL from dual union all
+
  select 103,'54, rue Royale',NULL,'Nantes',NULL,'44000','France' from dual union all
 
  select 112,'8489 Strong St.',NULL,'Las Vegas','NV','83030','USA' from dual union all
@@ -611,21 +649,21 @@ insert /*+ ignore_row_on_dupkey_index(customerdetail(customer_number)) */ into c
 
 for the table `productline` */
 
-insert /*+ ignore_row_on_dupkey_index(productline(product_line)) */ into productline(product_line,text_description,html_description,image) 
+insert /*+ ignore_row_on_dupkey_index(productline(product_line)) */ into productline(product_line,text_description,html_description,image,created_on) 
 
- select 'Classic Cars','Attention car enthusiasts: Make your wildest car ownership dreams come true. Whether you are looking for classic muscle cars, dream sports cars or movie-inspired miniatures, you will find great choices in this category. These replicas feature superb attention to detail and craftsmanship and offer features such as working steering system, opening forward compartment, opening rear trunk with removable spare wheel, 4-wheel independent spring suspension, and so on. The models range in size from 1:10 to 1:24 scale and include numerous limited edition and several out-of-production vehicles. All models include a certificate of authenticity from their manufacturers and come fully assembled and ready for display in the home or office.',NULL,NULL from dual union all 
+ select 'Classic Cars','Attention car enthusiasts: Make your wildest car ownership dreams come true. Whether you are looking for classic muscle cars, dream sports cars or movie-inspired miniatures, you will find great choices in this category. These replicas feature superb attention to detail and craftsmanship and offer features such as working steering system, opening forward compartment, opening rear trunk with removable spare wheel, 4-wheel independent spring suspension, and so on. The models range in size from 1:10 to 1:24 scale and include numerous limited edition and several out-of-production vehicles. All models include a certificate of authenticity from their manufacturers and come fully assembled and ready for display in the home or office.',NULL,NULL,'2005-02-03' from dual union all 
 
- select 'Motorcycles','Our motorcycles are state of the art replicas of classic as well as contemporary motorcycle legends such as Harley Davidson, Ducati and Vespa. Models contain stunning details such as official logos, rotating wheels, working kickstand, front suspension, gear-shift lever, footbrake lever, and drive chain. Materials used include diecast and plastic. The models range in size from 1:10 to 1:50 scale and include numerous limited edition and several out-of-production vehicles. All models come fully assembled and ready for display in the home or office. Most include a certificate of authenticity.',NULL,NULL from dual union all 
+ select 'Motorcycles','Our motorcycles are state of the art replicas of classic as well as contemporary motorcycle legends such as Harley Davidson, Ducati and Vespa. Models contain stunning details such as official logos, rotating wheels, working kickstand, front suspension, gear-shift lever, footbrake lever, and drive chain. Materials used include diecast and plastic. The models range in size from 1:10 to 1:50 scale and include numerous limited edition and several out-of-production vehicles. All models come fully assembled and ready for display in the home or office. Most include a certificate of authenticity.',NULL,NULL,'2004-12-12' from dual union all 
 
- select 'Planes','Unique, diecast airplane and helicopter replicas suitable for collections, as well as home, office or classroom decorations. Models contain stunning details such as official logos and insignias, rotating jet engines and propellers, retractable wheels, and so on. Most come fully assembled and with a certificate of authenticity from their manufacturers.',NULL,NULL from dual union all 
+ select 'Planes','Unique, diecast airplane and helicopter replicas suitable for collections, as well as home, office or classroom decorations. Models contain stunning details such as official logos and insignias, rotating jet engines and propellers, retractable wheels, and so on. Most come fully assembled and with a certificate of authenticity from their manufacturers.',NULL,NULL,'2004-02-14' from dual union all 
 
- select 'Ships','The perfect holiday or anniversary gift for executives, clients, friends, and family. These handcrafted model ships are unique, stunning works of art that will be treasured for generations! They come fully assembled and ready for display in the home or office. We guarantee the highest quality, and best value.',NULL,NULL from dual union all 
+ select 'Ships','The perfect holiday or anniversary gift for executives, clients, friends, and family. These handcrafted model ships are unique, stunning works of art that will be treasured for generations! They come fully assembled and ready for display in the home or office. We guarantee the highest quality, and best value.',NULL,NULL,'2005-12-12' from dual union all 
 
- select 'Trains','Model trains are a rewarding hobby for enthusiasts of all ages. Whether you''re looking for collectible wooden trains, electric streetcars or locomotives, you''ll find a number of great choices for any budget within this category. The interactive aspect of trains makes toy trains perfect for young children. The wooden train sets are ideal for children under the age of 5.',NULL,NULL from dual union all 
+ select 'Trains','Model trains are a rewarding hobby for enthusiasts of all ages. Whether you''re looking for collectible wooden trains, electric streetcars or locomotives, you''ll find a number of great choices for any budget within this category. The interactive aspect of trains makes toy trains perfect for young children. The wooden train sets are ideal for children under the age of 5.',NULL,NULL,'2004-03-03' from dual union all 
 
- select 'Trucks and Buses','The Truck and Bus models are realistic replicas of buses and specialized trucks produced from the early 1920s to present. The models range in size from 1:12 to 1:50 scale and include numerous limited edition and several out-of-production vehicles. Materials used include tin, diecast and plastic. All models include a certificate of authenticity from their manufacturers and are a perfect ornament for the home and office.',NULL,NULL from dual union all 
+ select 'Trucks and Buses','The Truck and Bus models are realistic replicas of buses and specialized trucks produced from the early 1920s to present. The models range in size from 1:12 to 1:50 scale and include numerous limited edition and several out-of-production vehicles. Materials used include tin, diecast and plastic. All models include a certificate of authenticity from their manufacturers and are a perfect ornament for the home and office.',NULL,NULL,'2005-02-25' from dual union all 
 
- select 'Vintage Cars','Our Vintage Car models realistically portray automobiles produced from the early 1900s through the 1940s. Materials used include Bakelite, diecast, plastic and wood. Most of the replicas are in the 1:18 and 1:24 scale sizes, which provide the optimum in detail and accuracy. Prices range from $30.00 up to $180.00 for some special limited edition replicas. All models include a certificate of authenticity from their manufacturers and come fully assembled and ready for display in the home or office.',NULL,NULL from dual;
+ select 'Vintage Cars','Our Vintage Car models realistically portray automobiles produced from the early 1900s through the 1940s. Materials used include Bakelite, diecast, plastic and wood. Most of the replicas are in the 1:18 and 1:24 scale sizes, which provide the optimum in detail and accuracy. Prices range from $30.00 up to $180.00 for some special limited edition replicas. All models include a certificate of authenticity from their manufacturers and come fully assembled and ready for display in the home or office.',NULL,NULL,'2004-04-02' from dual;
 
 
 /*Data for the table `product` */
