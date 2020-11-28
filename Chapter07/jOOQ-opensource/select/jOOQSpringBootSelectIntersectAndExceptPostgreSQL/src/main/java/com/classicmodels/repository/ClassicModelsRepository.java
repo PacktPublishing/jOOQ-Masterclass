992caa9afac7f6953b76db868f55dba85355111c
AnghelLeonard <leoprivacy@yahoo.com>
2020-11-28 17:05:04 +0200

    select

--- Chapter07/jOOQ-opensource/select/jOOQSpringBootSelectIntersectAndExceptPostgreSQL/src/main/java/com/classicmodels/repository/ClassicModelsRepository.java
+++ Chapter07/jOOQ-opensource/select/jOOQSpringBootSelectIntersectAndExceptPostgreSQL/src/main/java/com/classicmodels/repository/ClassicModelsRepository.java
@@ -5,8 +5,10 @@ import static jooq.generated.tables.Office.OFFICE;
 import static jooq.generated.tables.Orderdetail.ORDERDETAIL;
 import static jooq.generated.tables.Product.PRODUCT;
 import org.jooq.DSLContext;
+import org.jooq.Row2;
 import static org.jooq.impl.DSL.row;
 import static org.jooq.impl.DSL.select;
+import static org.jooq.impl.DSL.values;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -31,26 +33,26 @@ public class ClassicModelsRepository {
         "public"."orderdetail"."price_each"
       from
         "public"."orderdetail"
-    */
+     */
     public void intersectBuyPriceWithPriceEach() {
-        
-        System.out.println("EXAMPLE 1.1\n " + 
-                ctx.select(PRODUCT.BUY_PRICE)
+
+        System.out.println("EXAMPLE 1.1\n "
+                + ctx.select(PRODUCT.BUY_PRICE)
                         .from(PRODUCT)
                         .intersect(select(ORDERDETAIL.PRICE_EACH).from(ORDERDETAIL))
                         .fetch()
         );
-        
+
         // if duplicates are needed in the result set then use intersectAll()        
         // in this case, both queries produces the same result
-        System.out.println("EXAMPLE 1.2\n" +
-                ctx.select(PRODUCT.BUY_PRICE)
+        System.out.println("EXAMPLE 1.2\n"
+                + ctx.select(PRODUCT.BUY_PRICE)
                         .from(PRODUCT)
                         .intersectAll(select(ORDERDETAIL.PRICE_EACH).from(ORDERDETAIL))
                         .fetch()
         );
     }
-    
+
     // EXAMPLE 2
     /*
     select
@@ -62,31 +64,31 @@ public class ClassicModelsRepository {
         "public"."orderdetail"."price_each"
       from
         "public"."orderdetail"
-    */
+     */
     public void exceptBuyPriceFromPriceEach() {
-        
-        System.out.println("EXAMPLE 2.1\n" + 
-                ctx.select(PRODUCT.BUY_PRICE)
+
+        System.out.println("EXAMPLE 2.1\n"
+                + ctx.select(PRODUCT.BUY_PRICE)
                         .from(PRODUCT)
                         .except(select(ORDERDETAIL.PRICE_EACH).from(ORDERDETAIL))
                         .orderBy(PRODUCT.BUY_PRICE)
                         .fetch()
         );
-        
+
         // if duplicates are needed in the result set then use exceptAll()        
         // in this case, both queries produces the same result
-        System.out.println("EXAMPLE 2.2\n" + 
-                ctx.select(PRODUCT.BUY_PRICE)
+        System.out.println("EXAMPLE 2.2\n"
+                + ctx.select(PRODUCT.BUY_PRICE)
                         .from(PRODUCT)
                         .exceptAll(select(ORDERDETAIL.PRICE_EACH).from(ORDERDETAIL))
                         .orderBy(PRODUCT.BUY_PRICE)
                         .fetch()
         );
     }
-    
+
     // EXAMPLE 3
-    /* Fetch cities and countries where we have offices and customers */    
-    /*
+    /* Fetch cities and countries where we have offices and customers */
+ /*
     select
       "public"."office"."city",
       "public"."office"."country"
@@ -101,11 +103,11 @@ public class ClassicModelsRepository {
       order by
         "city",
         "country"
-    */
+     */
     public void intersectOfficeCustomerCityAndCountry() {
 
-        System.out.println("EXAMPLE 3.1\n" + 
-                ctx.select(OFFICE.CITY, OFFICE.COUNTRY)
+        System.out.println("EXAMPLE 3.1\n"
+                + ctx.select(OFFICE.CITY, OFFICE.COUNTRY)
                         .from(OFFICE)
                         .intersect(select(CUSTOMERDETAIL.CITY, CUSTOMERDETAIL.COUNTRY)
                                 .from(CUSTOMERDETAIL))
@@ -114,19 +116,19 @@ public class ClassicModelsRepository {
         );
 
         // if duplicates are needed in the result set then use intersectAll()        
-        System.out.println("EXAMPLE 3.2\n" + 
-                ctx.select(OFFICE.CITY, OFFICE.COUNTRY)
+        System.out.println("EXAMPLE 3.2\n"
+                + ctx.select(OFFICE.CITY, OFFICE.COUNTRY)
                         .from(OFFICE)
                         .intersectAll(select(CUSTOMERDETAIL.CITY, CUSTOMERDETAIL.COUNTRY)
                                 .from(CUSTOMERDETAIL))
                         .orderBy(OFFICE.CITY, OFFICE.COUNTRY)
                         .fetch()
-        );        
-    }    
-                    
+        );
+    }
+
     // EXAMPLE 4
     /* Fetch cities and countries where we have customers but we don't have offices */
-    /*
+ /*
     select
       "public"."customerdetail"."city",
       "public"."customerdetail"."country"
@@ -141,11 +143,11 @@ public class ClassicModelsRepository {
       order by
         "city",
         "country"
-    */
+     */
     public void exceptOfficeCustomerCityAndCountry() {
-        
-        System.out.println("EXAMPLE 4.1\n" + 
-                ctx.select(OFFICE.CITY, OFFICE.COUNTRY)
+
+        System.out.println("EXAMPLE 4.1\n"
+                + ctx.select(OFFICE.CITY, OFFICE.COUNTRY)
                         .from(OFFICE)
                         .except(select(CUSTOMERDETAIL.CITY, CUSTOMERDETAIL.COUNTRY)
                                 .from(CUSTOMERDETAIL))
@@ -154,8 +156,8 @@ public class ClassicModelsRepository {
         );
 
         // if duplicates are needed in the result set then use exceptAll()        
-        System.out.println("EXAMPLE 4.2\n" + 
-                ctx.select(OFFICE.CITY, OFFICE.COUNTRY)
+        System.out.println("EXAMPLE 4.2\n"
+                + ctx.select(OFFICE.CITY, OFFICE.COUNTRY)
                         .from(OFFICE)
                         .exceptAll(select(CUSTOMERDETAIL.CITY, CUSTOMERDETAIL.COUNTRY)
                                 .from(CUSTOMERDETAIL))
@@ -163,14 +165,48 @@ public class ClassicModelsRepository {
                         .fetch()
         );
     }
-    
-    /* Emulating INTERSECT(ALL)/EXCEPT(ALL) for databases that don't support them (e.g., MySQL) */                    
-    
-    // EXAMPLE 5    
+
+    // EXAMPLE 5
+    /*
+    select 
+      "p"."city", 
+      "p"."country" 
+    from 
+      (
+        values 
+          (?, ?), 
+          (?, ?), 
+          (?, ?), 
+          (?, ?), 
+          (?, ?), 
+          (?, ?)
+      ) as "p"("city", "country") 
+    except 
+    select 
+      "public"."office"."city", 
+      "public"."office"."country" 
+    from 
+      "public"."office"    
+    */
+    public void findCitiesWithNoOffices() {
+
+        Row2[] rows = {row("Paris", "France"), row("Lion", "France"), row("Nisa", "France"),
+            row("Boston", "USA"), row("Los Angeles", "USA"), row("Sydney", "Australia")};
+        
+        System.out.println("EXAMPLE 5\n"
+                + ctx.select().from(values(rows
+                ).as("p", OFFICE.CITY.getName(), OFFICE.COUNTRY.getName()))
+                        .except(select(OFFICE.CITY, OFFICE.COUNTRY).from(OFFICE))
+                        .fetch(OFFICE.CITY)
+        );
+    }
+
+    /* Emulating INTERSECT(ALL)/EXCEPT(ALL) for databases that don't support them (e.g., MySQL) */
+    // EXAMPLE 6    
     // Emulate INTERSECT via IN (useful when no duplicates and NULLs are present)
     // Emulate INTERSECT via WHERE EXISTS (useful when duplicates and/or NULLs are present)     
     public void emulateIntersectOfficeCustomerCityAndCountry() {
-        
+
         /*
         select distinct 
           "public"."office"."city",
@@ -189,17 +225,17 @@ public class ClassicModelsRepository {
             from
               "public"."customerdetail"
           )
-        */
-        System.out.println("EXAMPLE 5.1\n" +
-                //ctx.select for duplicates an no NULLs are present
-                ctx.selectDistinct(OFFICE.CITY, OFFICE.COUNTRY) 
+         */
+        System.out.println("EXAMPLE 6.1\n"
+                + //ctx.select for duplicates an no NULLs are present
+                ctx.selectDistinct(OFFICE.CITY, OFFICE.COUNTRY)
                         .from(OFFICE)
                         .where(row(OFFICE.CITY, OFFICE.COUNTRY)
                                 .in(select(CUSTOMERDETAIL.CITY, CUSTOMERDETAIL.COUNTRY)
                                         .from(CUSTOMERDETAIL)))
-                        .fetch()                                       
-        );                    
-        
+                        .fetch()
+        );
+
         /*
         select
           "public"."office"."city",
@@ -236,9 +272,9 @@ public class ClassicModelsRepository {
               )
             )
         )
-        */
-        System.out.println("EXAMPLE 5.2\n +"+
-                ctx.select(OFFICE.CITY, OFFICE.COUNTRY)
+         */
+        System.out.println("EXAMPLE 6.2\n +"
+                + ctx.select(OFFICE.CITY, OFFICE.COUNTRY)
                         .from(OFFICE)
                         .whereExists(select().from(CUSTOMERDETAIL)
                                 .where(CUSTOMERDETAIL.CITY.eq(OFFICE.CITY)
@@ -246,14 +282,14 @@ public class ClassicModelsRepository {
                                         .and(CUSTOMERDETAIL.COUNTRY.eq(OFFICE.COUNTRY)
                                                 .or(OFFICE.COUNTRY.isNull().and(CUSTOMERDETAIL.COUNTRY.isNull())))))
                         .fetch()
-        );        
-    }     
-    
-    // EXAMPLE 6
+        );
+    }
+
+    // EXAMPLE 7
     // Emulate EXCEPT via LEFT OUTER JOIN (useful when NULLs are present)
     // Emulate EXCEPT via WHERE NOT EXISTS (useful when duplicates and/or NULLs are present)     
     public void emulateExceptOfficeCustomerCityAndCountry() {
-      
+
         /*
         select
           "public"."office"."city",
@@ -266,18 +302,18 @@ public class ClassicModelsRepository {
             )
         where
           "public"."customerdetail"."city" is null
-        */
-        System.out.println("EXAMPLE 6.1\n" + 
-                //ctx.select for duplicates an no NULLs are present
+         */
+        System.out.println("EXAMPLE 7.1\n"
+                + //ctx.select for duplicates an no NULLs are present
                 ctx.selectDistinct(OFFICE.CITY, OFFICE.COUNTRY)
                         .from(OFFICE)
                         .leftJoin(CUSTOMERDETAIL)
-                                .on(OFFICE.CITY.eq(CUSTOMERDETAIL.CITY)
+                        .on(OFFICE.CITY.eq(CUSTOMERDETAIL.CITY)
                                 .and(OFFICE.COUNTRY.eq(CUSTOMERDETAIL.COUNTRY)))
                         .where(CUSTOMERDETAIL.CITY.isNull())
-                        .fetch()                
+                        .fetch()
         );
-        
+
         /*
         select
           "public"."office"."city",
@@ -314,16 +350,16 @@ public class ClassicModelsRepository {
               )
             )
         )
-        */
-        System.out.println("EXAMPLE 6.2\n" +
-                 ctx.select(OFFICE.CITY, OFFICE.COUNTRY)
-                         .from(OFFICE)
-                         .whereNotExists(select().from(CUSTOMERDETAIL)
-                                 .where(CUSTOMERDETAIL.CITY.eq(OFFICE.CITY)
-                                         .or(OFFICE.CITY.isNull().and(CUSTOMERDETAIL.CITY.isNull()))
-                                         .and(CUSTOMERDETAIL.COUNTRY.eq(OFFICE.COUNTRY)
-                                                 .or(OFFICE.COUNTRY.isNull().and(CUSTOMERDETAIL.COUNTRY.isNull())))))
-                         .fetch()
-         );      
-    }              
-}                   
+         */
+        System.out.println("EXAMPLE 7.2\n"
+                + ctx.select(OFFICE.CITY, OFFICE.COUNTRY)
+                        .from(OFFICE)
+                        .whereNotExists(select().from(CUSTOMERDETAIL)
+                                .where(CUSTOMERDETAIL.CITY.eq(OFFICE.CITY)
+                                        .or(OFFICE.CITY.isNull().and(CUSTOMERDETAIL.CITY.isNull()))
+                                        .and(CUSTOMERDETAIL.COUNTRY.eq(OFFICE.COUNTRY)
+                                                .or(OFFICE.COUNTRY.isNull().and(CUSTOMERDETAIL.COUNTRY.isNull())))))
+                        .fetch()
+        );
+    }
+}
--- Chapter07/jOOQ-opensource/select/jOOQSpringBootSelectIntersectAndExceptPostgreSQL/src/main/java/com/classicmodels/service/ClassicModelsService.java
+++ Chapter07/jOOQ-opensource/select/jOOQSpringBootSelectIntersectAndExceptPostgreSQL/src/main/java/com/classicmodels/service/ClassicModelsService.java
@@ -18,9 +18,10 @@ public class ClassicModelsService {
         classicModelsRepository.exceptBuyPriceFromPriceEach();                  // EXAMPLE 2
         classicModelsRepository.intersectOfficeCustomerCityAndCountry();        // EXAMPLE 3 
         classicModelsRepository.exceptOfficeCustomerCityAndCountry();           // EXAMPLE 4
+        classicModelsRepository.findCitiesWithNoOffices();                      // EXAMPLE 5
 
-        classicModelsRepository.emulateIntersectOfficeCustomerCityAndCountry(); // EXAMPLE 5
-        classicModelsRepository.emulateExceptOfficeCustomerCityAndCountry();    // EXAMPLE 6       
+        classicModelsRepository.emulateIntersectOfficeCustomerCityAndCountry(); // EXAMPLE 6
+        classicModelsRepository.emulateExceptOfficeCustomerCityAndCountry();    // EXAMPLE 7       
     }
 
 }
