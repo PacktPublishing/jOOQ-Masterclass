6bff320e2d0d149c1896deb4907a711e5fca93f1
AnghelLeonard <leoprivacy@yahoo.com>
2020-11-30 11:57:09 +0200

    select

--- Chapter07/jOOQ-opensource/select/jOOQSpringBootCommonlyUsedProjectionsMySQL/src/main/java/com/classicmodels/repository/ClassicModelsRepository.java
+++ Chapter07/jOOQ-opensource/select/jOOQSpringBootCommonlyUsedProjectionsMySQL/src/main/java/com/classicmodels/repository/ClassicModelsRepository.java
@@ -58,6 +58,12 @@ public class ClassicModelsRepository {
         System.out.println("EXAMPLE 2.3\n"
                 + ctx.select(val(1)).fetch()
         );
+        
+        // select 1 as `A`, 'John' as `B`, 4333 as `C`, false as `D` from dual
+        System.out.println("EXAMPLE 2.4\n"
+                + ctx.select(val(1).as("A"), val("John").as("B"), 
+                        val(4333).as("C"), val(false).as("D")).fetch()
+        );
     }
 
     // EXAMPLE 3
