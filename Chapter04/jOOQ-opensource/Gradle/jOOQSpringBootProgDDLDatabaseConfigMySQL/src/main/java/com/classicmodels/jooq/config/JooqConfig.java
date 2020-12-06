3238cd1b5d4ff5075b204801c594d8a327a8f79c
AnghelLeonard <leoprivacy@yahoo.com>
2020-12-06 18:48:50 +0200

    DDLDatabase

---
+++ Chapter04/jOOQ-opensource/Gradle/jOOQSpringBootProgDDLDatabaseConfigMySQL/src/main/java/com/classicmodels/jooq/config/JooqConfig.java
@@ -0,0 +1,100 @@
+package com.classicmodels.jooq.config;
+
+import com.classicmodels.properties.DataSourceProperties;
+import com.classicmodels.properties.JooqProperties;
+import javax.sql.DataSource;
+import org.flywaydb.core.Flyway;
+import org.jooq.codegen.GenerationTool;
+import org.jooq.conf.Settings;
+import org.jooq.meta.jaxb.Configuration;
+import org.jooq.meta.jaxb.Database;
+import org.jooq.meta.jaxb.Generate;
+import org.jooq.meta.jaxb.Generator;
+import org.jooq.meta.jaxb.MatcherRule;
+import org.jooq.meta.jaxb.MatcherTransformType;
+import org.jooq.meta.jaxb.Matchers;
+import org.jooq.meta.jaxb.MatchersTableType;
+import org.jooq.meta.jaxb.Property;
+import org.jooq.meta.jaxb.Strategy;
+import org.jooq.meta.jaxb.Target;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.flyway.FlywayDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JooqConfig {
+
+    private final DataSource ds;
+
+    public JooqConfig(DataSource ds) {
+        this.ds = ds;
+    }
+
+    @Bean(name = "dsProperties")
+    public DataSourceProperties dsProperties() {
+        return new DataSourceProperties();
+    }
+
+    @Bean(name = "jooqProperties")
+    public JooqProperties jooqProperties() {
+        return new JooqProperties();
+    }
+
+    @FlywayDataSource
+    @Bean(initMethod = "migrate", name = "dsFlyway")
+    public Flyway primaryFlyway() {
+
+        return Flyway.configure()
+                .dataSource(ds)
+                .locations("classpath:/db/migration", "classpath:/db/aftermigration")
+                .load();
+    }
+
+    @Bean
+    public Settings jooqSettings() {
+        return new Settings()
+                .withRenderCatalog(Boolean.FALSE)
+                .withRenderSchema(Boolean.FALSE);
+    }
+
+    @Bean
+    @DependsOn("dsFlyway")
+    public Configuration jooqCodeGen(
+            @Qualifier("dsProperties") DataSourceProperties dsProperties,
+            @Qualifier("jooqProperties") JooqProperties jooqProperties) throws Exception {
+
+        Configuration configuration = new Configuration()
+                .withGenerator(new Generator()
+                        .withDatabase(new Database()
+                                .withName(jooqProperties.getWithName())
+                                .withInputSchema(jooqProperties.getWithInputSchema())
+                                .withProperties(new Property().withKey("scripts").withValue(jooqProperties.getWithScripts()),
+                                        new Property().withKey("sort").withValue(jooqProperties.getWithSort()),
+                                        new Property().withKey("unqualifiedSchema").withValue(jooqProperties.getWithUnqualifiedSchema()),
+                                        new Property().withKey("defaultNameCase").withValue(jooqProperties.getWithDefaultNameCase())))
+                        .withGenerate(new Generate()
+                                .withDaos(true)
+                                .withValidationAnnotations(Boolean.TRUE)
+                                .withSpringAnnotations(Boolean.TRUE)
+                        )
+                        .withStrategy(new Strategy()
+                                .withMatchers(new Matchers()
+                                        .withTables(new MatchersTableType()
+                                                .withPojoClass(new MatcherRule()
+                                                        .withExpression("Jooq_$0")
+                                                        .withTransform(MatcherTransformType.PASCAL))
+                                                .withDaoClass(new MatcherRule()
+                                                        .withExpression("$0_Repository")
+                                                        .withTransform(MatcherTransformType.PASCAL))))
+                        )
+                        .withTarget(new Target()
+                                .withPackageName(jooqProperties.getWithPackageName())
+                                .withDirectory(jooqProperties.getWithDirectory())));
+
+        GenerationTool.generate(configuration);
+
+        return configuration;
+    }
+}
