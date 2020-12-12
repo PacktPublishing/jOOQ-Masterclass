759880d947306da800e08edf979651151c8d237a
anghelleonard <leoprivacy@yahoo.com>
2020-12-12 16:52:25 +0200

    LATERAL JOIN

---
+++ Chapter08/jOOQ-trial-commercial/jOOQSpringBootLateralJoinOracle/src/main/java/com/classicmodels/service/ClassicModelsService.java
@@ -0,0 +1,24 @@
+package com.classicmodels.service;
+
+import com.classicmodels.repository.ClassicModelsRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClassicModelsService {
+
+    private final ClassicModelsRepository classicModelsRepository;
+
+    public ClassicModelsService(ClassicModelsRepository classicModelsRepository) {
+        this.classicModelsRepository = classicModelsRepository;
+    }
+
+    public void callAll() {
+
+        classicModelsRepository.lateralOfficeHasDepartments();                     // EXAMPLE 1
+        classicModelsRepository.leftOuterJoinLateralOfficeHasDepartments();        // EXAMPLE 2 
+        classicModelsRepository.lateralEmployeeAvgSales();                         // EXAMPLE 3
+        classicModelsRepository.lateralOfficeCityCountryHasDepartments();          // EXAMPLE 4         
+        classicModelsRepository.lateralDepartmentUnnest();                         // EXAMPLE 5                
+        classicModelsRepository.lateralDepartmentUnnestOrdinality();               // EXAMPLE 6
+    }
+}
