64fde83d8c7e6eea922c91965d11d08bc7ddc009
anghelleonard <leoprivacy@yahoo.com>
2020-12-14 16:24:06 +0200

    NATURAL JOIN

--- Chapter08/jOOQ-opensource/jOOQSpringBootNaturalJoinPostgreSQL/src/main/java/com/classicmodels/repository/ClassicModelsRepository.java
+++ Chapter08/jOOQ-opensource/jOOQSpringBootNaturalJoinPostgreSQL/src/main/java/com/classicmodels/repository/ClassicModelsRepository.java
@@ -1,7 +1,10 @@
 package com.classicmodels.repository;
 
+import static jooq.generated.tables.Customer.CUSTOMER;
 import static jooq.generated.tables.Customerdetail.CUSTOMERDETAIL;
 import static jooq.generated.tables.Office.OFFICE;
+import static jooq.generated.tables.Order.ORDER;
+import static jooq.generated.tables.Payment.PAYMENT;
 import org.jooq.DSLContext;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -29,9 +32,17 @@ public class ClassicModelsRepository {
     // EXAMPLE 2
     public void naturalLeftOuterJoinOfficeCustomerdetail() {
 
-        System.out.println("EXAMPLE 2\n"
+        System.out.println("EXAMPLE 2.1\n"
                 + ctx.select()
                         .from(OFFICE.naturalLeftOuterJoin(CUSTOMERDETAIL))
+                        .where(OFFICE.CITY.isNotNull())
+                        .fetch()
+        );
+        
+        System.out.println("EXAMPLE 2.2\n"
+                + ctx.select()
+                        .from(OFFICE.naturalLeftOuterJoin(CUSTOMERDETAIL))
+                        .where(OFFICE.CITY.isNotNull())
                         .fetch()
         );
     }
@@ -52,6 +63,16 @@ public class ClassicModelsRepository {
         System.out.println("EXAMPLE 4\n"
                 + ctx.select()
                         .from(OFFICE.naturalFullOuterJoin(CUSTOMERDETAIL))                        
+                        .fetch()
+        );
+    }
+    
+    // EXAMPLE 5
+    public void naturalJoinOrderCustomerPayment() {
+
+        System.out.println("EXAMPLE 5\n"
+                + ctx.select()
+                        .from(ORDER.naturalJoin(CUSTOMER).naturalJoin(PAYMENT))
                         .fetch()
         );
     }
