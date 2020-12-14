e873a1c04d8d310ed5da202fcbff2ced6962a873
anghelleonard <leoprivacy@yahoo.com>
2020-12-14 16:11:13 +0200

    NATURAL JOIN

---
+++ Chapter08/jOOQ-trial-commercial/jOOQSpringBootNaturalJoinOracle/src/main/java/com/classicmodels/repository/ClassicModelsRepository.java
@@ -0,0 +1,58 @@
+package com.classicmodels.repository;
+
+import org.jooq.DSLContext;
+import static org.jooq.impl.DSL.name;
+import static org.jooq.impl.DSL.table;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+@Transactional(readOnly = true)
+public class ClassicModelsRepository {
+
+    private final DSLContext ctx;
+
+    public ClassicModelsRepository(DSLContext ctx) {
+        this.ctx = ctx;
+    }
+
+    // EXAMPLE 1
+    public void naturalJoinOfficeCustomerdetail() {
+
+        System.out.println("EXAMPLE 1\n"
+                + ctx.select()
+                        .from(table(name("OFFICE")).naturalJoin(table(name("CUSTOMERDETAIL"))))
+                        .fetch()
+        );
+    }
+
+    // EXAMPLE 2
+    public void naturalLeftOuterJoinOfficeCustomerdetail() {
+
+        System.out.println("EXAMPLE 2\n"
+                + ctx.select()
+                        .from(table(name("OFFICE")).naturalLeftOuterJoin(table(name("CUSTOMERDETAIL"))))
+                        .fetch()
+        );
+    }
+
+    // EXAMPLE 3
+    public void naturalRightOuterJoinOfficeCustomerdetail() {
+
+        System.out.println("EXAMPLE 3\n"
+                + ctx.select()
+                        .from(table(name("OFFICE")).naturalRightOuterJoin(table(name("CUSTOMERDETAIL"))))
+                        .fetch()
+        );
+    }
+    
+    // EXAMPLE 4
+    public void naturalFullOuterJoinOfficeCustomerdetail() {
+
+        System.out.println("EXAMPLE 4\n"
+                + ctx.select()
+                        .from(table(name("OFFICE")).naturalFullOuterJoin(table(name("CUSTOMERDETAIL"))))
+                        .fetch()
+        );
+    }
+}
