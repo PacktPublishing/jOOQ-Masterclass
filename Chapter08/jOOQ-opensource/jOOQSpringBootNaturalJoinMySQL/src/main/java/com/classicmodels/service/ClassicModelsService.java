d7a07ceed206b15ce288cad21be289c1f53afcd3
anghelleonard <leoprivacy@yahoo.com>
2020-12-14 14:00:30 +0200

    NATURAL JOIN

---
+++ Chapter08/jOOQ-opensource/jOOQSpringBootNaturalJoinMySQL/src/main/java/com/classicmodels/service/ClassicModelsService.java
@@ -0,0 +1,21 @@
+package com.classicmodels.service;
+
+import com.classicmodels.repository.ClassicModelsRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClassicModelsService {
+
+    private final ClassicModelsRepository classicModelsRepository;
+
+    public ClassicModelsService(ClassicModelsRepository classicModelsRepository) {
+        this.classicModelsRepository = classicModelsRepository;
+    }
+
+    public void callAll() {
+
+        classicModelsRepository.naturalJoinOfficeCustomerdetail();             // EXAMPLE 1
+        classicModelsRepository.naturalLeftOuterJoinOfficeCustomerdetail();    // EXAMPLE 2
+        classicModelsRepository.naturalRightOuterJoinOfficeCustomerdetail();   // EXAMPLE 3        
+    }
+}
