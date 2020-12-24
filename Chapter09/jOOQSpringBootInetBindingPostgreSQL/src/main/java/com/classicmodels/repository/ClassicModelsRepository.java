976d64ea3751d8e3eb4021cf0c829cbe88d74d12
anghelleonard <leoprivacy@yahoo.com>
2020-12-24 19:10:50 +0200

    Inet Binding

--- Chapter09/jOOQSpringBootInetBindingPostgreSQL/src/main/java/com/classicmodels/repository/ClassicModelsRepository.java
+++ Chapter09/jOOQSpringBootInetBindingPostgreSQL/src/main/java/com/classicmodels/repository/ClassicModelsRepository.java
@@ -5,6 +5,8 @@ import java.net.UnknownHostException;
 import java.util.List;
 import static jooq.generated.Sequences.DEPARTMENT_DEPARTMENT_ID_SEQ;
 import static jooq.generated.tables.Department.DEPARTMENT;
+import jooq.generated.tables.pojos.Department;
+import jooq.generated.tables.records.DepartmentRecord;
 import org.jooq.DSLContext;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -40,9 +42,23 @@ public class ClassicModelsRepository {
         // use the InetConverter
         ctx.insertInto(DEPARTMENT)
                 .values(DEPARTMENT_DEPARTMENT_ID_SEQ.nextval(),
-                        "HR", "-int 8799", 2231, 4, new String[]{"hiring", "interiew"},
+                        "HR", "-int 8799", (short) 2231, "4", new String[]{"hiring", "interiew"},
                         202010, InetAddress.getByName("128.10.124.12"))
                 .execute();
+        
+        // use the InetConverter and Department POJO
+        Integer nextId1 = ctx.select(DEPARTMENT_DEPARTMENT_ID_SEQ.nextval()).fetchOneInto(Integer.class);
+        Department department = new Department(nextId1,
+                        "HR", "-int 8799", (short) 2231, "4", new String[]{"hiring", "interiew"},
+                        202010, InetAddress.getByName("128.10.124.12"));
+        ctx.newRecord(DEPARTMENT, department).insert();        
+        
+        // use the InetConverter and DepartmentRecord
+        Integer nextId2 = ctx.select(DEPARTMENT_DEPARTMENT_ID_SEQ.nextval()).fetchOneInto(Integer.class);
+        DepartmentRecord departmentr = new DepartmentRecord(nextId2,
+                        "HR", "-int 8799", (short) 2231, "4", new String[]{"hiring", "interiew"},
+                        202010, InetAddress.getByName("128.10.124.12"));
+        ctx.insertInto(DEPARTMENT).values(departmentr.valuesRow().fields()).execute();
     }
 
     public void fetchDepInet() {
