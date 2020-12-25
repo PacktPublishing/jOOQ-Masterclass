60db1806a9b4e5ef89151051208a3c9303785f57
anghelleonard <leoprivacy@yahoo.com>
2020-12-25 17:45:05 +0200

    Enum Converter

---
+++ Chapter09/jOOQSpringBootEnumConverterForceTypesMySQL/src/main/java/com/classicmodels/repository/ClassicModelsRepository.java
@@ -0,0 +1,90 @@
+package com.classicmodels.repository;
+
+import static com.classicmodels.converter.SaleRateIntConverter.SALE_RATE_INT_CONVERTER;
+import static com.classicmodels.converter.SaleRateStarConverter.SALE_RATE_STAR_CONVERTER;
+import static com.classicmodels.converter.SaleStrTrendConverter.SALE_STR_TREND_TYPE;
+import static com.classicmodels.converter.SaleStrTrendConverter.SALE_STR_TREND_CONVERTER;
+import com.classicmodels.enums.StarType;
+import com.classicmodels.enums.TrendType;
+import java.util.List;
+import jooq.generated.enums.RateType;
+import static jooq.generated.tables.Sale.SALE;
+import org.jooq.DSLContext;
+import static org.jooq.impl.DSL.val;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+@Transactional(readOnly = true)
+public class ClassicModelsRepository {
+
+    private final DSLContext ctx;
+
+    public ClassicModelsRepository(DSLContext ctx) {
+        this.ctx = ctx;
+    }
+
+    @Transactional
+    public void insertSale() {
+
+        // no explicit converter
+        ctx.insertInto(SALE, SALE.FISCAL_YEAR, SALE.SALE_, SALE.EMPLOYEE_NUMBER, SALE.RATE)
+                .values(2005, 56444.32, 1370L, StarType.FIVE_STARS)
+                .execute();
+
+        // use SALE_RATE_STAR_CONVERTER
+        ctx.insertInto(SALE, SALE.FISCAL_YEAR, SALE.SALE_, SALE.EMPLOYEE_NUMBER, SALE.RATE)
+                .values(2005, 56444.32, 1370L, StarType.FIVE_STARS)
+                .execute();
+
+        // use SALE_RATE_INT_CONVERTER
+//        ctx.insertInto(SALE, SALE.FISCAL_YEAR, SALE.SALE_, SALE.EMPLOYEE_NUMBER, SALE.RATE)
+  //              .values(2005, 56444.32, 1370L, 1000)
+    //            .execute();
+        
+        // use SALE_STR_TREND_CONVERTER
+        ctx.insertInto(SALE, SALE.FISCAL_YEAR, SALE.SALE_, SALE.EMPLOYEE_NUMBER, SALE.TREND)
+                .values(2005, 56444.32, 1370L, TrendType.UP)
+                .execute();
+                
+        // use SALE_STR_TREND_TYPE
+        ctx.insertInto(SALE)
+                .values(null, 2005, 56444.32, 1370L, 0, SALE_RATE_INT_CONVERTER.to(1000), 
+                        val(TrendType.UP, SALE_STR_TREND_TYPE))
+                .execute();
+    }
+
+    public void fetchSale() {
+
+        List<StarType> rates = ctx.select(SALE.RATE)
+                .from(SALE)
+                .where(SALE.RATE.isNotNull())
+                .fetch(SALE.RATE);
+
+        System.out.println("Rates: " + rates);
+
+        // convert from RateType to StarType via explicit call of the converter
+        List<StarType> stars = ctx.select(SALE.RATE)
+                .from(SALE)
+                .where(SALE.RATE.isNotNull())
+                .fetch(SALE.RATE);
+
+        System.out.println("Stars: " + stars);
+
+        // convert from RateType to Integer via explicit call of the converter
+      //  List<Integer> ints = ctx.select(SALE.RATE)
+        //        .from(SALE)
+          //      .where(SALE.RATE.isNotNull())
+            //    .fetch(SALE.RATE);
+
+      //  System.out.println("Stars as integers: " + ints);
+        
+        // convert from String to TrendType via explicit call of the converter
+        List<TrendType> trends = ctx.select(SALE.TREND)
+                .from(SALE)
+                .where(SALE.TREND.isNotNull())
+                .fetch(SALE.TREND);
+        
+        System.out.println("Trends: " + trends);
+    }    
+}
