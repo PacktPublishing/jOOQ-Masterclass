7951f38d027eb9b7784322d3288348c031b3b993
anghelleonard <leoprivacy@yahoo.com>
2020-12-28 09:46:34 +0200

    Prog Converter

---
+++ Chapter09/jOOQSpringBootProgEnumConverterSQLServer/src/main/java/com/classicmodels/converter/SaleRateStarConverter.java
@@ -0,0 +1,58 @@
+package com.classicmodels.converter;
+
+import com.classicmodels.enums.StarType;
+import org.jooq.Converter;
+
+public class SaleRateStarConverter implements Converter<String, StarType> {
+
+    public final static SaleRateStarConverter SALE_RATE_STAR_CONVERTER = new SaleRateStarConverter();
+
+    @Override
+    public StarType from(String t) {
+
+        if (t != null) {
+
+            return switch (t) {
+                case "SILVER" ->
+                    StarType.THREE_STARS;
+                case "GOLD" ->
+                    StarType.FOUR_STARS;
+                case "PLATINUM" ->
+                    StarType.FIVE_STARS;
+                default ->
+                    null;
+            };
+        }
+
+        return null;
+    }
+
+    @Override
+    public String to(StarType u) {
+
+        if (u != null) {
+
+            return switch (u) {
+                case THREE_STARS ->
+                    "SILVER";
+                case FOUR_STARS ->
+                    "GOLD";
+                case FIVE_STARS ->
+                    "PLATINUM";
+            };
+        }
+
+        return null;
+    }
+
+    @Override
+    public Class<String> fromType() {
+        return String.class;
+    }
+
+    @Override
+    public Class<StarType> toType() {
+        return StarType.class;
+    }
+
+}
