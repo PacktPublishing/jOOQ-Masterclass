5860d3535534d276ab6b976b37a0950fb2eb394b
anghelleonard <leoprivacy@yahoo.com>
2021-01-01 13:29:36 +0200

    binding

--- Chapter09/jOOQ-opensource/binding/jOOQSpringBootJsonBindingPostgreSQL/src/main/java/com/classicmodels/binding/JsonBinding.java
+++ Chapter09/jOOQ-opensource/binding/jOOQSpringBootJsonBindingPostgreSQL/src/main/java/com/classicmodels/binding/JsonBinding.java
@@ -2,6 +2,7 @@ package com.classicmodels.binding;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import java.sql.SQLException;
+import java.sql.SQLFeatureNotSupportedException;
 import java.sql.Types;
 import java.util.Objects;
 import org.jooq.Binding;
@@ -26,9 +27,10 @@ public class JsonBinding implements Binding<JSON, JsonNode> {
         return converter;
     }
 
+    // Rending a bind variable for the binding context's value and casting it to the JSON type
     @Override
     public void sql(BindingSQLContext<JsonNode> ctx) throws SQLException {
-       
+
         if (ctx.render().paramType() == ParamType.INLINED) {
             ctx.render().visit(inline(ctx.convert(converter()).value())).sql("::json");
         } else {
@@ -36,33 +38,39 @@ public class JsonBinding implements Binding<JSON, JsonNode> {
         }
     }
 
+    // Registering VARCHAR types for JDBC CallableStatement OUT parameters
     @Override
     public void register(BindingRegisterContext<JsonNode> ctx) throws SQLException {
         ctx.statement().registerOutParameter(ctx.index(), Types.VARCHAR);
     }
 
+    // Converting the JsonNode to a String value and setting that on a JDBC PreparedStatement
     @Override
     public void set(BindingSetStatementContext<JsonNode> ctx) throws SQLException {
         ctx.statement().setString(ctx.index(), Objects.toString(ctx.convert(converter()).value(), null));
-    }    
+    }
 
+    // Getting a String value from a JDBC ResultSet and converting that to a JsonNode
     @Override
     public void get(BindingGetResultSetContext<JsonNode> ctx) throws SQLException {
         ctx.convert(converter()).value(JSON.valueOf(ctx.resultSet().getString(ctx.index())));
     }
 
+    // Getting a String value from a JDBC CallableStatement and converting that to JsonNode
     @Override
     public void get(BindingGetStatementContext<JsonNode> ctx) throws SQLException {
         ctx.convert(converter()).value(JSON.valueOf(ctx.statement().getString(ctx.index())));
     }
 
+    // Getting a value from a JDBC SQLInput (useful for Oracle OBJECT types)
     @Override
     public void get(BindingGetSQLInputContext<JsonNode> bgsqlc) throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); 
+        throw new SQLFeatureNotSupportedException("Not supported yet.");
     }
-    
+
+    // Setting a value on a JDBC SQLOutput (useful for Oracle OBJECT types)
     @Override
     public void set(BindingSetSQLOutputContext<JsonNode> bsqlc) throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); 
+        throw new SQLFeatureNotSupportedException("Not supported yet.");
     }
 }
