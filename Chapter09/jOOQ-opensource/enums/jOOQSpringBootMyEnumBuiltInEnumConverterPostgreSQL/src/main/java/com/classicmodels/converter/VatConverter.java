f37f342ae45411f73fb969d79ae74bad92d3c3e4
anghelleonard <leoprivacy@yahoo.com>
2021-01-04 10:27:26 +0200

    enum

--- Chapter09/jOOQ-opensource/enums/jOOQSpringBootMyEnumBuiltInEnumConverterPostgreSQL/src/main/java/com/classicmodels/converter/VatConverter.java
+++ Chapter09/jOOQ-opensource/enums/jOOQSpringBootMyEnumBuiltInEnumConverterPostgreSQL/src/main/java/com/classicmodels/converter/VatConverter.java
@@ -1,19 +1,19 @@
 package com.classicmodels.converter;
 
-import com.classicmodels.enums.VatType;
 import org.jooq.DataType;
 import org.jooq.impl.EnumConverter;
 import static org.jooq.impl.SQLDataType.VARCHAR;
 
-public class VatConverter extends EnumConverter<Object, VatType> {
+public class VatConverter extends
+        EnumConverter<jooq.generated.enums.VatType, com.classicmodels.enums.VatType> {
 
     public final static VatConverter VAT_CONVERTER
             = new VatConverter();
 
-    public final static DataType<VatType> VATTYPE
-            = VARCHAR.asConvertedDataType(VAT_CONVERTER);
+    public final static DataType<com.classicmodels.enums.VatType> VATTYPE
+            = VARCHAR.asEnumDataType(jooq.generated.enums.VatType.class).asConvertedDataType(VAT_CONVERTER);
 
     public VatConverter() {
-        super(Object.class, VatType.class);
+        super(jooq.generated.enums.VatType.class, com.classicmodels.enums.VatType.class);
     }
 }
