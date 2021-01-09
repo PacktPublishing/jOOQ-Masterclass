cbe7c1c0cc6f69bb649a6ed4d762991d42380ba0
anghelleonard <leoprivacy@yahoo.com>
2021-01-09 18:57:35 +0200

    converter

--- Chapter09/jOOQ-opensource/converter/Maven/jOOQSpringBootInlineYearMonthConverterMySQL/src/main/java/com/classicmodels/repository/ClassicModelsRepository.java
+++ Chapter09/jOOQ-opensource/converter/Maven/jOOQSpringBootInlineYearMonthConverterMySQL/src/main/java/com/classicmodels/repository/ClassicModelsRepository.java
@@ -24,7 +24,7 @@ public class ClassicModelsRepository {
         // converter is not used (we insert the Integer '202010' directly)
         ctx.insertInto(CUSTOMER)
                 .values(null, "Atelier One", "Markus", "Alop", "0892 339 423",
-                        1370L, 50000, 202010)
+                        1370L, 50000, 24249)
                 .execute();
 
         // non type-safe,
