847f177b06c59db5dd242f9959269bd90d5a2850
anghelleonard <leoprivacy@yahoo.com>
2021-01-26 14:37:46 +0200

    json

--- Chapter10/json/jOOQSpringBootJsonRelationshipsIntoSQLServer/src/main/java/com/classicmodels/service/ClassicModelsService.java
+++ Chapter10/json/jOOQSpringBootJsonRelationshipsIntoSQLServer/src/main/java/com/classicmodels/service/ClassicModelsService.java
@@ -15,6 +15,6 @@ public class ClassicModelsService {
     public void callAll() {
 
         classicModelsRepository.oneToOneToJsonToPojo();
-        classicModelsRepository.oneToManyToJsonToPojo();
+        classicModelsRepository.oneToManyToJsonToPojo();        
     }
 }
