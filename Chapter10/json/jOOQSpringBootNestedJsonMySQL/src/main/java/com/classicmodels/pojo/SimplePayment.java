d0c25344a412fff65e18585d233d01a605e0e9f4
anghelleonard <leoprivacy@yahoo.com>
2021-01-27 09:04:55 +0200

    json

--- Chapter10/json/jOOQSpringBootNestedJsonMySQL/src/main/java/com/classicmodels/pojo/SimplePayment.java
+++ Chapter10/json/jOOQSpringBootNestedJsonMySQL/src/main/java/com/classicmodels/pojo/SimplePayment.java
@@ -5,22 +5,91 @@ import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
 
-public class SimplePayment {
+public class SimplePayment implements Serializable {
 
-    public Long paymentNumber;
-    public BigDecimal invoiceAmount;
+    private static final long serialVersionUID = 1;
+
+    private Long paymentNumber;
+    private BigDecimal invoiceAmount;
     
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss.000000")
     @JsonSerialize(using = LocalDateTimeSerializer.class)
     @JsonDeserialize(using = LocalDateTimeDeserializer.class)
-    public LocalDateTime cachingDate;
+    private LocalDateTime cachingDate;
     
-    public List<SimpleBank> transactions;
+    private List<SimpleBank> transactions;
 
+    public Long getPaymentNumber() {
+        return paymentNumber;
+    }
+
+    public void setPaymentNumber(Long paymentNumber) {
+        this.paymentNumber = paymentNumber;
+    }
+
+    public BigDecimal getInvoiceAmount() {
+        return invoiceAmount;
+    }
+
+    public void setInvoiceAmount(BigDecimal invoiceAmount) {
+        this.invoiceAmount = invoiceAmount;
+    }
+
+    public LocalDateTime getCachingDate() {
+        return cachingDate;
+    }
+
+    public void setCachingDate(LocalDateTime cachingDate) {
+        this.cachingDate = cachingDate;
+    }
+
+    public List<SimpleBank> getTransactions() {
+        return transactions;
+    }
+
+    public void setTransactions(List<SimpleBank> transactions) {
+        this.transactions = transactions;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 83 * hash + Objects.hashCode(this.paymentNumber);
+        hash = 83 * hash + Objects.hashCode(this.invoiceAmount);
+        hash = 83 * hash + Objects.hashCode(this.cachingDate);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final SimplePayment other = (SimplePayment) obj;
+        if (!Objects.equals(this.paymentNumber, other.paymentNumber)) {
+            return false;
+        }
+        if (!Objects.equals(this.invoiceAmount, other.invoiceAmount)) {
+            return false;
+        }
+        if (!Objects.equals(this.cachingDate, other.cachingDate)) {
+            return false;
+        }
+        return true;
+    }
+        
     @Override
     public String toString() {
         return "SimplePayment{" + "paymentNumber=" + paymentNumber
