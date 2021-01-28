9c10ead1a5bd51b4dc9e2493c5a3525b82d667a6
anghelleonard <leoprivacy@yahoo.com>
2021-01-28 11:35:46 +0200

    lazy loading

---
+++ Chapter10/lazy/jOOQSpringBootLazyLoadingOracle/src/main/java/com/classicmodels/controller/ClassicModelController.java
@@ -0,0 +1,9 @@
+package com.classicmodels.controller;
+
+public class ClassicModelController {
+    
+    protected static final String CURSOR_ATTR = "cursor";
+    private static final String BINDING_RESULT = "org.springframework.validation.BindingResult." + CURSOR_ATTR;
+    
+    
+}
