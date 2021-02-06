9762ba1472fa3bd17d64c445975f0c2fb4603531
anghelleonard <leoprivacy@yahoo.com>
2021-02-06 07:44:45 +0200

    xml

--- Chapter10/xml/jOOQSpringBootSimpleXmlSQLServer/src/main/java/com/classicmodels/repository/ClassicModelsRepository.java
+++ Chapter10/xml/jOOQSpringBootSimpleXmlSQLServer/src/main/java/com/classicmodels/repository/ClassicModelsRepository.java
@@ -28,33 +28,45 @@ public class ClassicModelsRepository {
 
         Result<Record1<XML>> result2 = ctx.select(OFFICE.OFFICE_CODE, OFFICE.CITY, OFFICE.COUNTRY)
                 .from(OFFICE)
-                .forXML().auto().elements()
+                .forXML().raw()
                 .fetch();
         System.out.println("Example 1.2:\n" + result2.formatXML());
-
+        
         Result<Record1<XML>> result3 = ctx.select(OFFICE.OFFICE_CODE, OFFICE.CITY, OFFICE.COUNTRY)
                 .from(OFFICE)
-                .forXML().auto().root()
+                .forXML().raw().elements().root("offices")
                 .fetch();
         System.out.println("Example 1.3:\n" + result3.formatXML());
 
         Result<Record1<XML>> result4 = ctx.select(OFFICE.OFFICE_CODE, OFFICE.CITY, OFFICE.COUNTRY)
                 .from(OFFICE)
-                .forXML().auto().root("offices")
+                .forXML().auto().elements()
                 .fetch();
         System.out.println("Example 1.4:\n" + result4.formatXML());
 
         Result<Record1<XML>> result5 = ctx.select(OFFICE.OFFICE_CODE, OFFICE.CITY, OFFICE.COUNTRY)
                 .from(OFFICE)
-                .forXML().auto().elements().root()
+                .forXML().auto().root()
                 .fetch();
         System.out.println("Example 1.5:\n" + result5.formatXML());
 
         Result<Record1<XML>> result6 = ctx.select(OFFICE.OFFICE_CODE, OFFICE.CITY, OFFICE.COUNTRY)
                 .from(OFFICE)
-                .forXML().auto().elements().root("offices")
+                .forXML().auto().root("offices")
                 .fetch();
         System.out.println("Example 1.6:\n" + result6.formatXML());
+
+        Result<Record1<XML>> result7 = ctx.select(OFFICE.OFFICE_CODE, OFFICE.CITY, OFFICE.COUNTRY)
+                .from(OFFICE)
+                .forXML().auto().elements().root()
+                .fetch();
+        System.out.println("Example 1.7:\n" + result7.formatXML());
+
+        Result<Record1<XML>> result8 = ctx.select(OFFICE.OFFICE_CODE, OFFICE.CITY, OFFICE.COUNTRY)
+                .from(OFFICE)
+                .forXML().auto().elements().root("offices")
+                .fetch();
+        System.out.println("Example 1.8:\n" + result8.formatXML());
     }
 
     public void fetchOfficesAsXMLPath() {
@@ -100,7 +112,7 @@ public class ClassicModelsRepository {
                 .forXML().path("office").elements().root("offices")
                 .fetch();
         System.out.println("Example 1.7.1:\n" + result71.formatXML());
-        
+
         String result72 = ctx.select(OFFICE.OFFICE_CODE, OFFICE.CITY, OFFICE.COUNTRY)
                 .from(OFFICE)
                 .forXML().path("office").elements().root("offices")
