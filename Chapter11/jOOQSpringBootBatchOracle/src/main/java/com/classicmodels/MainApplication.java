bc7bb9ea9526c8f33667c53e39b47d1474e93106
anghelleonard <leoprivacy@yahoo.com>
2021-02-17 12:27:22 +0200

    batching

---
+++ Chapter11/jOOQSpringBootBatchOracle/src/main/java/com/classicmodels/MainApplication.java
@@ -0,0 +1,40 @@
+package com.classicmodels;
+
+import com.classicmodels.service.ClassicModelsService;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class MainApplication {
+
+    private final ClassicModelsService classicModelsService;
+
+    public MainApplication(ClassicModelsService classicModelsService) {
+        this.classicModelsService = classicModelsService;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+
+    @Bean
+    public ApplicationRunner init() {
+        return args -> {
+
+            classicModelsService.batchInserts();
+            classicModelsService.batchUpdates();
+            classicModelsService.batchDeletes();
+            classicModelsService.batchMerges();
+            classicModelsService.batchStores();
+            classicModelsService.combineBatch();
+            classicModelsService.batchCollectionOfObjects();
+            classicModelsService.batchedInsertsAndUpdates1();
+            classicModelsService.batchedInsertsAndUpdates2();
+            classicModelsService.batchedConnectionUsage();
+            classicModelsService.batchedRecords();
+            classicModelsService.batchingOneToMany();
+        };
+    }
+}
