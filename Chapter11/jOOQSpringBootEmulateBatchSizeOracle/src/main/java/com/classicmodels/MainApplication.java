354759699df583a61778f2924d7ad2fa300bbe1f
anghelleonard <leoprivacy@yahoo.com>
2021-02-23 14:07:30 +0200

    emulate batch size

---
+++ Chapter11/jOOQSpringBootEmulateBatchSizeOracle/src/main/java/com/classicmodels/MainApplication.java
@@ -0,0 +1,33 @@
+package com.classicmodels;
+
+import com.classicmodels.service.ClassicModelsService;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class MainApplication {
+
+    private final ClassicModelsService classicModelsService;
+
+    public MainApplication(ClassicModelsService classicModelsService) {
+        this.classicModelsService = classicModelsService;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+
+    // This code result in:
+    // ORA-00001: unique constraint (SYSTEM.SYS_C0010940) violated
+    
+    @Bean
+    public ApplicationRunner init() {
+        return args -> {
+
+            classicModelsService.batchQueriesInOneTransaction();
+            classicModelsService.batchQueriesInTransactionPerBatch();
+        };
+    }
+}
