bafb0d2b597a658e742126d90766b6d973355694
anghelleonard <leoprivacy@yahoo.com>
2021-02-23 12:30:39 +0200

    emulate batch

---
+++ Chapter11/jOOQSpringBootEmulateBatchSizeMySQL/src/main/java/com/classicmodels/MainApplication.java
@@ -0,0 +1,33 @@
+package com.classicmodels;
+
+import com.classicmodels.service.ClassicModelsService;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class MainApplication {
+
+    private final ClassicModelsService classicModelsService;
+
+    public MainApplication(ClassicModelsService classicModelsService) {
+        this.classicModelsService = classicModelsService;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+
+    // This code result in:
+    // java.sql.SQLIntegrityConstraintViolationException: Duplicate entry '1' for key 'sale.PRIMARY'
+    
+    @Bean
+    public ApplicationRunner init() {
+        return args -> {
+
+            classicModelsService.batchQueriesInOneTransaction();
+            classicModelsService.batchQueriesInTransactionPerBatch();
+        };
+    }
+}
