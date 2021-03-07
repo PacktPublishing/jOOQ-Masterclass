0cb28f8fe2b317e691302419d1746237f22fbfe2
anghelleonard <leoprivacy@yahoo.com>
2021-03-07 18:37:40 +0200

    supports

---
+++ Chapter11/jOOQSpringBootPropagationNotSupportsMySQL/src/main/java/com/classicmodels/MainApplication.java
@@ -0,0 +1,31 @@
+package com.classicmodels;
+
+import com.classicmodels.service.FirstService;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class MainApplication {
+
+    private final FirstService firstService;
+
+    public MainApplication(FirstService firstService) {
+        this.firstService = firstService;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+
+    @Bean
+    public ApplicationRunner init() {
+        return args -> {
+
+            System.out.println("============================================");
+            firstService.insertFirstSale();
+            System.out.println("============================================");
+        };
+    }
+}
