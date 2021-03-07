3c9ef96a378408ce282e8456237c4258a437e0cc
anghelleonard <leoprivacy@yahoo.com>
2021-03-07 18:33:00 +0200

    not supported

---
+++ Chapter11/jOOQSpringBootPropagationNotSupportedMySQL/src/main/java/com/classicmodels/service/FirstService.java
@@ -0,0 +1,30 @@
+package com.classicmodels.service;
+
+import static jooq.generated.tables.Sale.SALE;
+import org.jooq.DSLContext;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class FirstService {
+
+    private final DSLContext ctx;
+    private final SecondService secondService;
+
+    public FirstService(DSLContext ctx, SecondService secondService) {
+        this.ctx = ctx;
+        this.secondService = secondService;
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    public void insertFirstSale() {
+
+        ctx.insertInto(SALE)
+                .set(SALE.SALE_, 12222.22)
+                .set(SALE.FISCAL_YEAR, 2022)
+                .execute();
+
+        secondService.insertSecondSale();
+    }
+}
