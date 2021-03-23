de9745c5ee944ed998d209346561eee2af7abea7
anghelleonard <leoprivacy@yahoo.com>
2021-03-23 07:53:32 +0200

    keys

---
+++ Chapter13/jOOQSpringBootKeysSQLServer/src/main/java/com/classicmodels/repository/ClassicModelsRepository.java
@@ -0,0 +1,94 @@
+package com.classicmodels.repository;
+
+import static jooq.generated.tables.Productline.PRODUCTLINE;
+import static jooq.generated.tables.Sale.SALE;
+import jooq.generated.tables.records.SaleRecord;
+import org.jooq.DSLContext;
+import org.jooq.Query;
+import org.jooq.conf.Settings;
+import static org.jooq.impl.DSL.row;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+@Transactional(readOnly = true)
+public class ClassicModelsRepository {
+
+    private final DSLContext ctx;
+
+    public ClassicModelsRepository(DSLContext ctx) {
+        this.ctx = ctx;
+    }
+
+    /* Primary keys and updatable records */
+    
+    @Transactional
+    public void suppressPrimaryKeyReturnOnUpdatableRecord() {
+
+        // Insert without returning the generated primary key
+        DSLContext derivedCtx = ctx.configuration().derive(new Settings()
+                .withReturnIdentityOnUpdatableRecord(false)).dsl();
+
+        SaleRecord srNoReturnId = derivedCtx.newRecord(SALE);
+
+        srNoReturnId.setFiscalYear(2021);
+        srNoReturnId.setSale(4500.25);
+        srNoReturnId.setEmployeeNumber(1504L);
+
+        srNoReturnId.insert();
+
+        System.out.println("The inserted record ID (should be null): " + srNoReturnId.getSaleId());
+    }
+
+    /* Insert and return primary key */
+    
+    @Transactional
+    public void insertAndReturnPrimaryKey() {
+
+        var insertedId = ctx.insertInto(SALE, SALE.FISCAL_YEAR, SALE.SALE_, SALE.EMPLOYEE_NUMBER)
+                .values(2004, 2311.42, 1370L)
+                .returningResult(SALE.SALE_ID)
+                .fetchOne();
+
+        System.out.println("Inserted ID:\n" + insertedId);
+
+        var insertedIds = ctx.insertInto(SALE, SALE.FISCAL_YEAR, SALE.SALE_, SALE.EMPLOYEE_NUMBER)
+                .values(2004, 2311.42, 1370L)
+                .values(2003, 900.21, 1504L)
+                .values(2005, 1232.2, 1166L)
+                .returningResult(SALE.SALE_ID)
+                .fetch();
+
+        System.out.println("Inserted IDs:\n" + insertedIds);                
+    }
+    
+    /* Insert IDENTITY values */
+    
+    public void insertIdentity() {
+        
+        Query q1 = ctx.query("SET IDENTITY_INSERT [sale] ON");
+        Query q2 = ctx.insertInto(SALE, SALE.SALE_ID, SALE.FISCAL_YEAR, SALE.SALE_, SALE.EMPLOYEE_NUMBER)
+                .values(5555L, 2004, 2311.42, 1370L)
+                .onDuplicateKeyIgnore(); // this will lead to a MERGE
+        Query q3 = ctx.query("SET IDENTITY_INSERT [sale] OFF");
+
+        ctx.batch(q1, q2, q3).execute();
+    }
+
+    /* Compare composed keys */
+    
+    public void compareComposedPrimaryKey() {
+
+        var result1 = ctx.selectFrom(PRODUCTLINE)
+                .where(PRODUCTLINE.PRODUCT_LINE.eq("Classic Cars")
+                        .and(PRODUCTLINE.CODE.eq(599302L)))
+                .fetchSingle();
+        System.out.println("Result:\n" + result1);
+
+        var result2 = ctx.selectFrom(PRODUCTLINE)
+                .where(row(PRODUCTLINE.PRODUCT_LINE, PRODUCTLINE.CODE)
+                        .eq(row("Classic Cars", 599302L)))
+                .fetchSingle();
+        System.out.println("Result:\n" + result2);
+    }    
+}
