1899553674266a29e33a6d9c1535d1cb68a2de5e
anghelleonard <leoprivacy@yahoo.com>
2021-03-24 16:37:35 +0200

    embedded

--- Chapter13/jOOQSpringBootEmbeddedCompositeKeysOracle/src/main/java/com/classicmodels/repository/ClassicModelsRepository.java
+++ Chapter13/jOOQSpringBootEmbeddedCompositeKeysOracle/src/main/java/com/classicmodels/repository/ClassicModelsRepository.java
@@ -1,6 +1,5 @@
 package com.classicmodels.repository;
 
-import java.math.BigInteger;
 import java.util.List;
 import jooq.generated.embeddables.pojos.ProductlinePkEmbedded;
 import jooq.generated.embeddables.records.ProductlinePkEmbeddedRecord;
