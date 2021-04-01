7a2e9ab3d10db38e88dba5192b865a315afb78ed
anghelleonard <leoprivacy@yahoo.com>
2021-04-01 12:13:33 +0300

    keys

---
+++ Chapter13/jOOQSpringBootDetectIdentityPostgreSQL/src/main/java/com/classicmodels/repository/ClassicModelsRepository.java
@@ -0,0 +1,35 @@
+package com.classicmodels.repository;
+
+import static jooq.generated.tables.Product.PRODUCT;
+import jooq.generated.tables.records.ProductRecord;
+import org.jooq.DSLContext;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+@Transactional(readOnly = true)
+public class ClassicModelsRepository {
+
+    private final DSLContext ctx;
+  
+    public ClassicModelsRepository(DSLContext ctx) {
+        this.ctx = ctx;        
+    }
+
+    @Transactional
+    public void returnIdentitiesOnUpdatableRecord() {
+
+        ProductRecord pr = ctx.newRecord(PRODUCT);
+
+        pr.setProductLine("Classic Cars");
+        pr.setCode(599302L);
+        pr.setProductName("1967 Chevrolet Camaro RS");
+
+        pr.insert();
+
+        System.out.println("The inserted record ID: " + pr.getProductId());
+        
+        // the *pr.getProductUid()* works thanks to the synthetic identity configured in pom.xml
+        System.out.println("The inserted record 'product_uid' IDENTITY: " + pr.getProductUid());
+    }
+}
