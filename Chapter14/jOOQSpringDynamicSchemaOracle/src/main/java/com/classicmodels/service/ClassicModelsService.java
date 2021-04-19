8d3fadcd5238e07f83630cf947ed1964e877fa43
anghelleonard <leoprivacy@yahoo.com>
2021-04-19 08:10:17 +0300

    dyn

---
+++ Chapter14/jOOQSpringDynamicSchemaOracle/src/main/java/com/classicmodels/service/ClassicModelsService.java
@@ -0,0 +1,28 @@
+package com.classicmodels.service;
+
+import com.classicmodels.repository.ClassicModelsRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClassicModelsService {
+
+    private final ClassicModelsRepository classicModelsRepository;
+
+    public ClassicModelsService(ClassicModelsRepository classicModelsRepository) {
+        this.classicModelsRepository = classicModelsRepository;
+    }
+
+    public void callAll() {
+        
+        classicModelsRepository.ddlFromJavaSchema();
+        classicModelsRepository.setSchema();
+        classicModelsRepository.createSequence();
+        classicModelsRepository.populateSchema();
+        classicModelsRepository.alterSchema();        
+        classicModelsRepository.createDropIndexes();
+        classicModelsRepository.createTableFromAnotherTable();
+        classicModelsRepository.createTempTable1();
+        classicModelsRepository.createTempTable2();       
+        classicModelsRepository.createView();
+    }
+}
