333e4e92a0b8cf4c9105e35c2e08dfb78e5535c8
anghelleonard <leoprivacy@yahoo.com>
2021-04-22 15:59:00 +0300

    percent_rank()

---
+++ Chapter15/percent_rank/jOOQSpringPercentRankPostgreSQL/src/main/java/com/classicmodels/repository/ClassicModelsRepository.java
@@ -0,0 +1,72 @@
+package com.classicmodels.repository;
+
+import static jooq.generated.tables.Employee.EMPLOYEE;
+import static jooq.generated.tables.Office.OFFICE;
+import org.jooq.DSLContext;
+import static org.jooq.impl.DSL.case_;
+import static org.jooq.impl.DSL.cast;
+import static org.jooq.impl.DSL.count;
+import static org.jooq.impl.DSL.percentRank;
+import static org.jooq.impl.DSL.rank;
+import static org.jooq.impl.DSL.round;
+import org.jooq.impl.SQLDataType;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+@Transactional(readOnly = true)
+public class ClassicModelsRepository {
+
+    private final DSLContext ctx;
+
+    public ClassicModelsRepository(DSLContext ctx) {
+        this.ctx = ctx;
+    }
+
+    /*  The PERCENT_RANK() window function calculates the percentile  
+        ((rank - 1) / (total_rows - 1)) rankings of rows in a result set. */
+    public void percentileRankEmployeesBySalary() {
+
+        ctx.select(EMPLOYEE.FIRST_NAME, EMPLOYEE.LAST_NAME, EMPLOYEE.SALARY,
+                round(cast(percentRank().over().orderBy(EMPLOYEE.SALARY), SQLDataType.NUMERIC), 2).as("percentile_rank"))
+                .from(EMPLOYEE)
+                .fetch();
+
+        // emulate percentRank() via rank()        
+        ctx.select(EMPLOYEE.FIRST_NAME, EMPLOYEE.LAST_NAME, EMPLOYEE.SALARY, round(case_()
+                .when(count().over().orderBy(EMPLOYEE.SALARY).rangeBetweenUnboundedPreceding()
+                        .andUnboundedFollowing().eq(1), cast(0, Double.class))
+                .else_((cast(rank().over().orderBy(EMPLOYEE.SALARY), Double.class).minus(1d))
+                        .divide((count().over().orderBy(EMPLOYEE.SALARY).rangeBetweenUnboundedPreceding()
+                                .andUnboundedFollowing()).minus(1d))), 2).as("percentile_rank"))
+                .from(EMPLOYEE)
+                .fetch();
+    }
+
+    public void percentileRankEmployeesBySalaryAndOffice() {
+
+        ctx.select(EMPLOYEE.FIRST_NAME, EMPLOYEE.LAST_NAME, EMPLOYEE.SALARY,
+                OFFICE.OFFICE_CODE, OFFICE.CITY, OFFICE.COUNTRY,
+                round(cast(percentRank().over().partitionBy(OFFICE.OFFICE_CODE)
+                        .orderBy(EMPLOYEE.SALARY).mul(100), SQLDataType.NUMERIC), 2).concat("%").as("percentile_rank"))
+                .from(EMPLOYEE)
+                .innerJoin(OFFICE)
+                .on(EMPLOYEE.OFFICE_CODE.eq(OFFICE.OFFICE_CODE))
+                .fetch();
+
+        // emulate percentRank() via rank()           
+        ctx.select(EMPLOYEE.FIRST_NAME, EMPLOYEE.LAST_NAME, EMPLOYEE.SALARY, round(case_()
+                .when(count().over().partitionBy(OFFICE.OFFICE_CODE)
+                        .orderBy(EMPLOYEE.SALARY).rangeBetweenUnboundedPreceding()
+                        .andUnboundedFollowing().eq(1), cast(0, Double.class))
+                .else_((cast(rank().over().partitionBy(OFFICE.OFFICE_CODE)
+                        .orderBy(EMPLOYEE.SALARY), Double.class).minus(1d))
+                        .divide((count().over().partitionBy(OFFICE.OFFICE_CODE)
+                                .orderBy(EMPLOYEE.SALARY).rangeBetweenUnboundedPreceding()
+                                .andUnboundedFollowing()).minus(1d))), 2).as("percentile_rank"))
+                .from(EMPLOYEE)
+                .innerJoin(OFFICE)
+                .on(EMPLOYEE.OFFICE_CODE.eq(OFFICE.OFFICE_CODE))
+                .fetch();
+    }
+}
