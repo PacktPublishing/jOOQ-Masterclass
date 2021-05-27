15345283da6a08e96b0e38475db8643f55d6c825
anghelleonard <leoprivacy@yahoo.com>
2021-05-27 08:56:31 +0300

    agg

---
+++ Chapter16/jOOQSpringBootCTEOracle/src/main/java/com/classicmodels/repository/ClassicModelsRepository.java
@@ -0,0 +1,204 @@
+package com.classicmodels.repository;
+
+import java.math.BigDecimal;
+import static jooq.generated.tables.Orderdetail.ORDERDETAIL;
+import static jooq.generated.tables.Product.PRODUCT;
+import static jooq.generated.tables.Productline.PRODUCTLINE;
+import static jooq.generated.tables.Sale.SALE;
+import org.jooq.CommonTableExpression;
+import org.jooq.DSLContext;
+import org.jooq.Field;
+import org.jooq.Record2;
+import static org.jooq.impl.DSL.count;
+import static org.jooq.impl.DSL.field;
+import static org.jooq.impl.DSL.max;
+import static org.jooq.impl.DSL.name;
+import static org.jooq.impl.DSL.select;
+import static org.jooq.impl.DSL.sum;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+@Transactional
+public class ClassicModelsRepository {
+
+    private final DSLContext ctx;
+
+    public ClassicModelsRepository(DSLContext ctx) {
+        this.ctx = ctx;
+    }
+
+    public void cte1() {
+
+        CommonTableExpression<Record2<Long, BigDecimal>> t = name("cte_sales")
+                .fields("employee_nr", "sales")
+                .as(select(SALE.EMPLOYEE_NUMBER, sum(SALE.SALE_))
+                        .from(SALE)
+                        .groupBy(SALE.EMPLOYEE_NUMBER));
+
+        ctx.with(t)
+                .select(t.field("employee_nr"), t.field("sales"))
+                .from(t)
+                .where(t.field("sales", Double.class)
+                        .eq(select(max(t.field("sales", Double.class))).from(t)))
+                .fetch();
+
+        ctx.with(t)
+                .select()
+                .from(t)
+                .where(t.field("sales", Double.class)
+                        .eq(select(max(t.field("sales", Double.class))).from(t)))
+                .fetch();
+    }
+
+    public void cte2() {
+
+        Field<Long> e = SALE.EMPLOYEE_NUMBER;
+        Field<BigDecimal> s = sum(SALE.SALE_);
+
+        CommonTableExpression<Record2<Long, BigDecimal>> t = name("cte_sales")
+                .fields(e.getName(), s.getName())
+                .as(select(e, s)
+                        .from(SALE)
+                        .groupBy(e));
+
+        ctx.with(t)
+                .select(t.field(e.getName()), t.field(s.getName()))
+                .from(t)
+                .where(t.field(s.getName(), s.getType())
+                        .eq(select(max(t.field(s.getName(), s.getType()))).from(t)))
+                .fetch();
+    }
+
+    public void cte3() {
+
+        Field<Long> e = SALE.EMPLOYEE_NUMBER;
+        Field<BigDecimal> s = sum(SALE.SALE_);
+
+        var t = name("cte_sales")
+                .fields(e.getName(), s.getName())
+                .as(select(e, s)
+                        .from(SALE)
+                        .groupBy(e));
+
+        ctx.with(t)
+                .select(t.field(e), t.field(s))
+                .from(t)
+                .where(t.field(s)
+                        .eq(select(max(t.field(s))).from(t)))
+                .fetch();
+
+        ctx.with(t)
+                .select()
+                .from(t)
+                .where(t.field(s)
+                        .eq(select(max(t.field(s))).from(t)))
+                .fetch();
+    }
+
+    public void cte4() {
+
+        ctx.with("cte_sales")
+                .as(select(SALE.EMPLOYEE_NUMBER.as("employee_nr"), sum(SALE.SALE_).as("sales"))
+                        .from(SALE)
+                        .groupBy(SALE.EMPLOYEE_NUMBER))
+                .select() // field("employee_nr"), field("sales")
+                .from(name("cte_sales"))
+                .where(field(name("sales"))
+                        .eq(select(max(field(name("sales")))).from(name("cte_sales"))))
+                .fetch();
+    }
+
+    // two CTEs
+    public void cte5() {
+
+        var cte1 = name("cte_productline_counts")
+                .fields("product_line", "code", "product_count", "description")
+                .as(select(PRODUCT.PRODUCT_LINE, PRODUCT.CODE, PRODUCTLINE.TEXT_DESCRIPTION,
+                        count(PRODUCT.PRODUCT_ID))
+                        .from(PRODUCTLINE)
+                        .join(PRODUCT)
+                        .onKey()
+                        .groupBy(PRODUCT.PRODUCT_LINE, PRODUCT.CODE, PRODUCTLINE.TEXT_DESCRIPTION));
+
+        var cte2 = name("cte_productline_sales")
+                .fields("product_line", "sales")
+                .as(select(PRODUCT.PRODUCT_LINE, sum(ORDERDETAIL.QUANTITY_ORDERED.mul(ORDERDETAIL.PRICE_EACH)))
+                        .from(PRODUCT)
+                        .join(ORDERDETAIL)
+                        .onKey()
+                        .groupBy(PRODUCT.PRODUCT_LINE));
+
+        ctx.with(cte1, cte2)
+                .select(cte1.field("product_line"), cte1.field("code"),
+                        cte1.field("product_count"), cte1.field("description"),
+                        cte2.field("sales"))
+                .from(cte1)
+                .join(cte2)
+                .on(cte1.field("product_line", String.class)
+                        .eq(cte2.field("product_line", String.class)))
+                .orderBy(cte1.field("product_line"))
+                .fetch();
+    }
+
+    public void cte6() {
+
+        Field<String> pl = PRODUCT.PRODUCT_LINE;
+
+        var cte1 = name("cte_productline_counts")
+                .fields(pl.getName(), "code", "product_count", "description")
+                .as(select(pl, PRODUCT.CODE, PRODUCTLINE.TEXT_DESCRIPTION,
+                        count(PRODUCT.PRODUCT_ID))
+                        .from(PRODUCTLINE)
+                        .join(PRODUCT)
+                        .onKey()
+                        .groupBy(pl, PRODUCT.CODE, PRODUCTLINE.TEXT_DESCRIPTION));
+
+        var cte2 = name("cte_productline_sales")
+                .fields(pl.getName(), "sales")
+                .as(select(pl, sum(ORDERDETAIL.QUANTITY_ORDERED.mul(ORDERDETAIL.PRICE_EACH)))
+                        .from(PRODUCT)
+                        .join(ORDERDETAIL)
+                        .onKey()
+                        .groupBy(pl));
+
+        ctx.with(cte1, cte2)
+                .select(cte1.field(pl), cte1.field("code"),
+                        cte1.field("product_count"), cte1.field("description"),
+                        cte2.field("sales"))
+                .from(cte1)
+                .join(cte2)
+                .on(cte1.field(pl)
+                        .eq(cte2.field(pl)))
+                .orderBy(cte1.field(pl))
+                .fetch();
+    }
+
+    public void cte7() {
+
+        ctx.with("cte_productline_counts")
+                .as(select(PRODUCT.PRODUCT_LINE.as("product_line"), PRODUCT.CODE.as("code"),
+                        count(PRODUCT.PRODUCT_ID).as("product_count"),
+                        PRODUCTLINE.TEXT_DESCRIPTION.as("description"))
+                        .from(PRODUCTLINE)
+                        .join(PRODUCT)
+                        .onKey()
+                        .groupBy(PRODUCT.PRODUCT_LINE, PRODUCT.CODE, PRODUCTLINE.TEXT_DESCRIPTION))
+                .with("cte_productline_sales")
+                .as(select(PRODUCT.PRODUCT_LINE.as("product_line"),
+                        sum(ORDERDETAIL.QUANTITY_ORDERED.mul(ORDERDETAIL.PRICE_EACH)).as("sales"))
+                        .from(PRODUCT)
+                        .join(ORDERDETAIL)
+                        .onKey()
+                        .groupBy(PRODUCT.PRODUCT_LINE))
+                .select(field(name("cte_productline_counts", "product_line")), field(name("code")),
+                        field(name("product_count")), field(name("description")),
+                        field(name("sales")))
+                .from(name("cte_productline_counts"))
+                .join(name("cte_productline_sales"))
+                .on(field(name("cte_productline_counts", "product_line"))
+                        .eq(field(name("cte_productline_sales", "product_line"))))
+                .orderBy(field(name("cte_productline_counts", "product_line")))
+                .fetch();
+    }
+}
