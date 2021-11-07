8325e67dcad07a7a14a3045f9488c09471ddd590
anghelleonard <leoprivacy@yahoo.com>
2021-11-07 17:33:59 +0200

    gn

---
+++ Chapter19/jOOQSpringMTCOMySQL/src/main/java/com/classicmodels/MainApplication.java
@@ -0,0 +1,15 @@
+package com.classicmodels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.r2dbc.R2dbcAutoConfiguration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@SpringBootApplication(exclude = { R2dbcAutoConfiguration.class })
+@EnableWebSecurity
+public class MainApplication {
+   
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+}
