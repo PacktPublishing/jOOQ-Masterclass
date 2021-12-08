3ddceb177942dee28e20212cde3082f0e26feede
anghelleonard <leoprivacy@yahoo.com>
2021-12-08 18:32:56 +0200

    Using jOOQ DSL to build and execute SQL

---
+++ Chapter01/PostgreSQL/Kotlin/Gradle/DSLBuildExecuteSQL/src/main/kotlin/com/classicmodels/Initializer.kt
@@ -0,0 +1,16 @@
+package com.classicmodels
+
+import com.classicmodels.service.ClassicModelsService
+import org.springframework.boot.ApplicationArguments
+import org.springframework.boot.ApplicationRunner
+import org.springframework.stereotype.Component
+
+@Component
+class Initializer (private val classicModelsService: ClassicModelsService) : ApplicationRunner {
+
+    @Throws(Exception::class)
+    override fun run(args: ApplicationArguments) {
+        println("Fetching offices from 'NA' territory:");
+        println(classicModelsService.fetchOfficesInTerritory("NA"));
+    }
+}
