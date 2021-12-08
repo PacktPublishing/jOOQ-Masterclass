0ab4db64f39321b0746422012e986cc3ff0897e7
anghelleonard <leoprivacy@yahoo.com>
2021-12-08 19:01:25 +0200

    Using jOOQ DSL to build and execute SQL

---
+++ Chapter01/SQLServer/Kotlin/Gradle/DSLBuildExecuteSQL/build.gradle.kts
@@ -0,0 +1,50 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("application")
+    id("org.springframework.boot") version "2.5.7"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    id("nu.studer.jooq") version "6.0.1"
+    id("org.flywaydb.flyway") version "8.2.0"
+    kotlin("jvm") version "1.6.0"
+    kotlin("plugin.spring") version "1.6.0"
+}
+
+group = "com.classicmodels"
+version = "1.0"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+application {
+    mainClass.value("com.classicmodels.MainApplicationKt")
+}
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+}
+
+jooq {
+  version.set("3.15.5")  // if omitted, then the default is used
+  edition.set(nu.studer.gradle.jooq.JooqEdition.TRIAL_JAVA_8)
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-jooq")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("com.microsoft.sqlserver:mssql-jdbc") 
+    implementation("org.flywaydb:flyway-core")        
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs = listOf("-Xjsr305=strict")
+		jvmTarget = "17"
+	}
+}
