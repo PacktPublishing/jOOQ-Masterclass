d9a8cdd50399f426ae7313a7b21081a4cd360f55
anghelleonard <leoprivacy@yahoo.com>
2021-12-13 17:56:06 +0200

    Generating DAOs

--- Chapter02/SQLServer/Maven/GenerateDaos/src/main/java/com/classicmodels/MainApplication.java
+++ Chapter02/SQLServer/Maven/GenerateDaos/src/main/java/com/classicmodels/MainApplication.java
@@ -9,10 +9,11 @@ import jooq.generated.tables.pojos.JooqOrder;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.r2dbc.R2dbcAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {R2dbcAutoConfiguration.class})
 @ComponentScan(basePackages = { "jooq.generated.tables.daos", "com.classicmodels.*" })
 public class MainApplication {
 
--- Chapter02/SQLServer/Maven/GenerateDaos/src/main/java/com/classicmodels/controller/ClassicModelsController.java
+++ Chapter02/SQLServer/Maven/GenerateDaos/src/main/java/com/classicmodels/controller/ClassicModelsController.java
@@ -18,16 +18,18 @@ public class ClassicModelsController {
     public ClassicModelsController(ClassicModelsService classicModelsService) {
         this.classicModelsService = classicModelsService;
     }
-   
+
     @GetMapping("/officesInTerritory")
-    public List<JooqOffice> fetchOfficesInTerritory(@RequestParam String territory) {
+    public List<JooqOffice> fetchOfficesInTerritory(
+            @RequestParam(name = "territory") String territory) {
 
         return classicModelsService.fetchOfficesInTerritory(territory);
     }
 
     @GetMapping("/ordersByRequiredDate")
     public List<JooqOrder> fetchOrdersByRequiredDate(
-            @RequestParam String startDate, @RequestParam String endDate) {
+            @RequestParam(name = "startDate") String startDate,
+            @RequestParam(name = "endDate") String endDate) {
 
         return classicModelsService.fetchOrdersByRequiredDate(
                 LocalDate.parse(startDate), LocalDate.parse(endDate));
