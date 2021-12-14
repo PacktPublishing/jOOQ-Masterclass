88fec400c8e99012c10a0162b45ce7e342d92bb5
anghelleonard <leoprivacy@yahoo.com>
2021-12-14 14:30:13 +0200

    Programmatic DDLDatabase

---
+++ Chapter02/SQLServer/Gradle/ProgrammaticGenerator/jooq-code-generator/src/main/java/com/classicmodels/jooq/config/JooqConfig.java
@@ -0,0 +1,57 @@
+package com.classicmodels.jooq.config;
+
+import org.jooq.codegen.GenerationTool;
+import org.jooq.meta.jaxb.Configuration;
+import org.jooq.meta.jaxb.Database;
+import org.jooq.meta.jaxb.Generate;
+import org.jooq.meta.jaxb.Generator;
+import org.jooq.meta.jaxb.Jdbc;
+import org.jooq.meta.jaxb.MatcherRule;
+import org.jooq.meta.jaxb.MatcherTransformType;
+import org.jooq.meta.jaxb.Matchers;
+import org.jooq.meta.jaxb.MatchersTableType;
+import org.jooq.meta.jaxb.Strategy;
+import org.jooq.meta.jaxb.Target;
+
+public class JooqConfig {
+
+    public static void main(String[] args) throws Exception {
+
+        Configuration configuration = new Configuration()
+                .withJdbc(new Jdbc()
+                        .withDriver("com.microsoft.sqlserver.jdbc.SQLServerDriver")
+                        .withUrl("jdbc:sqlserver://localhost:1433;databaseName=classicmodels")
+                        .withUser("sa")
+                        .withPassword("root"))
+                .withGenerator(new Generator()
+                        .withDatabase(new Database()
+                                .withName("org.jooq.meta.sqlserver.SQLServerDatabase")
+                                .withSchemaVersionProvider("SELECT MAX([version]) FROM [flyway_schema_history]")
+                                .withIncludes(".*")
+                                .withExcludes("flyway_schema_history | concatenate | .*_master")
+                                .withInputCatalog("classicmodels")
+                                .withInputSchema("dbo")
+                        )
+                        .withGenerate(new Generate()
+                                .withDaos(true)
+                                .withValidationAnnotations(Boolean.TRUE)
+                                .withSpringAnnotations(Boolean.TRUE)
+                        )
+                        .withStrategy(new Strategy()
+                                .withMatchers(new Matchers()
+                                        .withTables(new MatchersTableType()
+                                                .withPojoClass(new MatcherRule()
+                                                        .withExpression("Jooq_$0")
+                                                        .withTransform(MatcherTransformType.PASCAL))
+                                                .withDaoClass(new MatcherRule()
+                                                        .withExpression("$0_Repository")
+                                                        .withTransform(MatcherTransformType.PASCAL))))
+                        )
+                        .withTarget(new Target()
+                                .withPackageName("jooq.generated")
+                                .withDirectory(System.getProperty("user.dir") 
+								+ "/../webapp/build/generated-sources")));
+
+        GenerationTool.generate(configuration);		
+    }
+}
