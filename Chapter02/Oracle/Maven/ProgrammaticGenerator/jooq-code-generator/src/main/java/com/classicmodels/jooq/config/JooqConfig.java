a1d750eb047405184b602a53e76716e418c97a42
anghelleonard <leoprivacy@yahoo.com>
2021-12-15 10:44:42 +0200

    Programmatic generator

---
+++ Chapter02/Oracle/Maven/ProgrammaticGenerator/jooq-code-generator/src/main/java/com/classicmodels/jooq/config/JooqConfig.java
@@ -0,0 +1,62 @@
+package com.classicmodels.jooq.config;
+
+import org.jooq.codegen.GenerationTool;
+import org.jooq.meta.jaxb.Configuration;
+import org.jooq.meta.jaxb.Database;
+import org.jooq.meta.jaxb.Generate;
+import org.jooq.meta.jaxb.Generator;
+import org.jooq.meta.jaxb.Jdbc;
+import org.jooq.meta.jaxb.MatcherRule;
+import org.jooq.meta.jaxb.MatcherTransformType;
+import org.jooq.meta.jaxb.Matchers;
+import org.jooq.meta.jaxb.MatchersTableType;
+import org.jooq.meta.jaxb.Strategy;
+import org.jooq.meta.jaxb.Target;
+
+public class JooqConfig {
+
+    public static void main(String[] args) throws Exception {
+
+        Configuration configuration = new Configuration()
+                .withJdbc(new Jdbc()
+                        .withDriver("oracle.jdbc.driver.OracleDriver")
+                        .withUrl("jdbc:oracle:thin:@localhost:1521:xe")
+                        .withUser("CLASSICMODELS")
+                        .withPassword("root"))
+                .withGenerator(new Generator()
+                        .withDatabase(new Database()
+                                .withName("org.jooq.meta.oracle.OracleDatabase")
+                                .withSchemaVersionProvider("SELECT MAX(\"version\") FROM \"flyway_schema_history\"")
+                                .withIncludes(".*")
+                                .withExcludes("flyway_schema_history | DEPARTMENT_PKG | GET_.*"
+                                        + " | CARD_COMMISSION | PRODUCT_OF_PRODUCT_LINE |"
+                                        + " | REFRESH_TOP3_PRODUCT | SALE_PRICE | SECOND_MAX"
+                                        + " | SET_COUNTER | SWAP | TOP_THREE_SALES_PER_EMPLOYEE"
+                                        + " | EVALUATION_CRITERIA | SECOND_MAX_IMPL | TABLE_.*_OBJ"
+                                        + " | .*_MASTER | BGT | .*_ARR | TABLE_POPL | TABLE_RES")
+                                .withInputSchema("classicmodels")
+                        )
+                        .withGenerate(new Generate()
+                                .withDaos(true)
+                                .withValidationAnnotations(Boolean.TRUE)
+                                .withSpringAnnotations(Boolean.TRUE)
+                        )
+                        .withStrategy(new Strategy()
+                                .withMatchers(new Matchers()
+                                        .withTables(new MatchersTableType()
+                                                .withPojoClass(new MatcherRule()
+                                                        .withExpression("Jooq_$0")
+                                                        .withTransform(MatcherTransformType.PASCAL))
+                                                .withDaoClass(new MatcherRule()
+                                                        .withExpression("$0_Repository")
+                                                        .withTransform(MatcherTransformType.PASCAL))))
+                        )
+                        .withTarget(new Target()
+                                .withPackageName("jooq.generated")
+                                .withDirectory(System.getProperty("user.dir").endsWith("webapp")
+                                        ? "target/generated-sources"
+                                        : "webapp/target/generated-sources")));
+
+        GenerationTool.generate(configuration);
+    }
+}
--- Chapter02/Oracle/Maven/ProgrammaticGenerator/webapp/src/main/java/com/classicmodels/controller/ClassicModelsController.java
+++ Chapter02/Oracle/Maven/ProgrammaticGenerator/webapp/src/main/java/com/classicmodels/controller/ClassicModelsController.java
@@ -1,11 +1,11 @@
 package com.classicmodels.controller;
 
 import com.classicmodels.pojo.CustomerAndOrder;
-import com.classicmodels.pojo.Office;
-import com.classicmodels.pojo.Order;
 import com.classicmodels.service.ClassicModelsService;
 import java.time.LocalDate;
 import java.util.List;
+import jooq.generated.tables.pojos.JooqOffice;
+import jooq.generated.tables.pojos.JooqOrder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,13 +20,13 @@ public class ClassicModelsController {
     }
 
     @GetMapping("/officesInTerritory")
-    public List<Office> fetchOfficesInTerritory(@RequestParam String territory) {
+    public List<JooqOffice> fetchOfficesInTerritory(@RequestParam String territory) {
 
         return classicModelsService.fetchOfficesInTerritory(territory);
     }
 
     @GetMapping("/ordersByRequiredDate")
-    public List<Order> fetchOrdersByRequiredDate(
+    public List<JooqOrder> fetchOrdersByRequiredDate(
             @RequestParam String startDate, @RequestParam String endDate) {
 
         return classicModelsService.fetchOrdersByRequiredDate(
