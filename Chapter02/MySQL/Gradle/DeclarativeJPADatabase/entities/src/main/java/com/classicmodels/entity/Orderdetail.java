0e7d51d2ba61a49cf8ed3974338e5e791d784a2c
anghelleonard <leoprivacy@yahoo.com>
2021-12-17 14:44:48 +0200

    Declarative JPADatabase

---
+++ Chapter02/MySQL/Gradle/DeclarativeJPADatabase/entities/src/main/java/com/classicmodels/entity/Orderdetail.java
@@ -0,0 +1,107 @@
+package com.classicmodels.entity;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Orderdetail implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @EmbeddedId
+    private OrderdetailId id;
+
+    @Column(nullable = false)
+    private Integer quantityOrdered;
+
+    @Column(precision = 10, scale = 2, nullable = false)
+    private Float priceEach;
+
+    @Column(nullable = false)
+    private Short orderLineNumber;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id", insertable = false, updatable = false)
+    private Order order;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", insertable = false, updatable = false)
+    private Product product;
+
+    public OrderdetailId getId() {
+        return id;
+    }
+
+    public void setId(OrderdetailId id) {
+        this.id = id;
+    }
+
+    public Integer getQuantityOrdered() {
+        return quantityOrdered;
+    }
+
+    public void setQuantityOrdered(Integer quantityOrdered) {
+        this.quantityOrdered = quantityOrdered;
+    }
+
+    public Float getPriceEach() {
+        return priceEach;
+    }
+
+    public void setPriceEach(Float priceEach) {
+        this.priceEach = priceEach;
+    }
+
+    public Short getOrderLineNumber() {
+        return orderLineNumber;
+    }
+
+    public void setOrderLineNumber(Short orderLineNumber) {
+        this.orderLineNumber = orderLineNumber;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    @Override
+    public int hashCode() {
+        return 2021;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+
+        if (obj == null) {
+            return false;
+        }
+
+        if (this == obj) {
+            return true;
+        }
+
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+
+        return id != null && id.equals(((Orderdetail) obj).id);
+    }
+
+}
