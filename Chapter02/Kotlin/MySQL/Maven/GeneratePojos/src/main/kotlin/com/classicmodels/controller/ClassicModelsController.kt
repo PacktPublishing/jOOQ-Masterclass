a304b670974331dc3cef08ad7dfd7c6a8554dd28
anghelleonard <leoprivacy@yahoo.com>
2021-12-18 13:07:37 +0200

    Generating POJOs

--- Chapter02/Kotlin/MySQL/Maven/GeneratePojos/src/main/kotlin/com/classicmodels/controller/ClassicModelsController.kt
+++ Chapter02/Kotlin/MySQL/Maven/GeneratePojos/src/main/kotlin/com/classicmodels/controller/ClassicModelsController.kt
@@ -8,7 +8,7 @@ import com.classicmodels.pojo.CustomerAndOrder
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
-
+ 
 @RestController
 public class ClassicModelsController(private val classicModelsService: ClassicModelsService) {
 
