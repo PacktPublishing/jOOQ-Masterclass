66c0d7330f57076a7d1b3f5a3ce4916b3f671ccf
anghelleonard <leoprivacy@yahoo.com>
2021-12-19 07:52:46 +0200

    Declarative JPADatabase

---
+++ Chapter02/Kotlin/MySQL/Maven/DeclarativeJPADatabase/webapp/src/main/kotlin/com/classicmodels/pojo/EmployeeNoCntr.kt
@@ -0,0 +1,8 @@
+package com.classicmodels.pojo
+
+import java.io.Serializable
+
+data class EmployeeNoCntr (val firstName: String?, 
+                           val lastName: String?,
+                           val salary: kotlin.Int?,
+                           val leastSalary: String?): Serializable
