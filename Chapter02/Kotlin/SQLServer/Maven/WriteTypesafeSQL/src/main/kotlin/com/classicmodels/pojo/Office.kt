dcd1b08074932c7564b3e1c8a9d2baa7d0e864ca
anghelleonard <leoprivacy@yahoo.com>
2021-12-20 07:36:26 +0200

    Using jOOQ to build/execute typesafe SQL

---
+++ Chapter02/Kotlin/SQLServer/Maven/WriteTypesafeSQL/src/main/kotlin/com/classicmodels/pojo/Office.kt
@@ -0,0 +1,13 @@
+package com.classicmodels.pojo
+
+import java.io.Serializable
+
+data class Office(val officecode: String?, 
+                  val addressLineFirst: String?,
+                  val addressLineSecond: String?,
+                  val city: String?,
+                  val country: String?,
+                  val phone: String?,
+                  val postalCode: String?,
+                  val state: String?,
+                  val territory: String?): Serializable
