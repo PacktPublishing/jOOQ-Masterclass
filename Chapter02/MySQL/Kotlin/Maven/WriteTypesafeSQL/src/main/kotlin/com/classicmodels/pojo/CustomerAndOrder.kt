e168857056858af7ddd4c876000938cad146500e
anghelleonard <leoprivacy@yahoo.com>
2021-12-22 15:45:44 +0200

    Build and execute type-safe SQL

--- Chapter02/MySQL/Kotlin/Maven/WriteTypesafeSQL/src/main/kotlin/com/classicmodels/pojo/CustomerAndOrder.kt
+++
@@ -1,7 +0,0 @@
-package com.classicmodels.pojo
-
-import java.time.LocalDate
-import java.io.Serializable
-
-data class CustomerAndOrder(val customerName: String?, 
-                            val orderDate: LocalDate?): Serializable
