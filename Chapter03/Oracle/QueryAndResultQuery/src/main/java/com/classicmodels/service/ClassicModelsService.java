b7dc4412f8ace94c1766437156c0ee80f78b3c8c
anghelleonard <leoprivacy@yahoo.com>
2021-12-27 08:45:20 +0200

    jOOQ Core Concepts

---
+++ Chapter03/Oracle/QueryAndResultQuery/src/main/java/com/classicmodels/service/ClassicModelsService.java
@@ -0,0 +1,25 @@
+package com.classicmodels.service;
+
+import com.classicmodels.repository.ClassicModelsRepository;
+import java.util.List;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClassicModelsService {
+
+    private final ClassicModelsRepository classicModelsRepository;
+
+    public ClassicModelsService(ClassicModelsRepository classicModelsRepository) {
+        this.classicModelsRepository = classicModelsRepository;
+    }
+
+    public int jooqQuery() {
+
+        return classicModelsRepository.jooqQuery();
+    }
+    
+    public List<String> jooqResultQuery() {
+        
+        return classicModelsRepository.jooqResultQuery();
+    }
+}
---
+++ Chapter03/SQLServer/QueryAndResultQuery/src/main/java/com/classicmodels/repository/ClassicModelsRepository.java
@@ -0,0 +1,56 @@
+package com.classicmodels.repository;
+
+import java.util.List;
+import static jooq.generated.tables.Employee.EMPLOYEE;
+import static jooq.generated.tables.Payment.PAYMENT;
+import org.jooq.DSLContext;
+import org.jooq.Query;
+import org.jooq.Record1;
+import org.jooq.Result;
+import org.jooq.Record;
+import org.jooq.ResultQuery;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+@Transactional(readOnly = true)
+public class ClassicModelsRepository {
+
+    private final DSLContext ctx;
+
+    public ClassicModelsRepository(DSLContext ctx) {
+        this.ctx = ctx;
+    }
+
+    @Transactional
+    public int jooqQuery() {
+
+        // Query query = ctx.query("DELETE FROM [payment] WHERE [customer_number] = 103");
+        
+        Query query = ctx.deleteFrom(PAYMENT)
+                .where(PAYMENT.CUSTOMER_NUMBER.eq(103L));
+
+        int affectedRows = query.execute();
+
+        return affectedRows;
+    }
+
+    public List<String> jooqResultQuery() {
+
+        /*
+        ResultQuery<Record> resultQuery = ctx.resultQuery(
+                "SELECT [job_title] FROM [employee] WHERE [office_code] = '4'");        
+        Result<Record> fetched = resultQuery.fetch();
+         */
+        
+        ResultQuery<Record1<String>> resultQuery = ctx.select(EMPLOYEE.JOB_TITLE)
+                .from(EMPLOYEE)
+                .where(EMPLOYEE.OFFICE_CODE.eq("4"));
+        Result<Record1<String>> fetched = resultQuery.fetch();
+
+        List<String> result = fetched.into(String.class);
+
+        return result;
+    }
+
+}
