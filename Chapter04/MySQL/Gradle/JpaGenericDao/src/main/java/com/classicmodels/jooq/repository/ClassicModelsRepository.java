f81392c9f8c39f2e36c3020a033d45ef8133be9a
anghelleonard <leoprivacy@yahoo.com>
2022-01-01 18:11:25 +0200

    Building a DAO layer

---
+++ Chapter04/MySQL/Gradle/JpaGenericDao/src/main/java/com/classicmodels/jooq/repository/ClassicModelsRepository.java
@@ -0,0 +1,16 @@
+package com.classicmodels.jooq.repository;
+
+import java.util.List;
+import org.jooq.TableRecord;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+@Transactional(readOnly = true)
+public interface ClassicModelsRepository<R extends TableRecord<R>, P, T> {
+
+    List<P> fetchAll();
+
+    @Transactional
+    void deleteById(T id);
+}
