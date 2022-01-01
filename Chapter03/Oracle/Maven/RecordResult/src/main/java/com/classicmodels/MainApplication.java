db3fa77183f66323aa5660256b1d1f73e71f4b7f
anghelleonard <leoprivacy@yahoo.com>
2022-01-01 10:18:14 +0200

    jOOQ Core Concepts

--- Chapter03/Oracle/Maven/RecordResult/src/main/java/com/classicmodels/MainApplication.java
+++ Chapter03/Oracle/Maven/RecordResult/src/main/java/com/classicmodels/MainApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.r2dbc.R2dbcAutoConfiguration;
 import org.springframework.context.annotation.Bean;
-
+ 
 @SpringBootApplication(exclude = {R2dbcAutoConfiguration.class})
 public class MainApplication {
 
