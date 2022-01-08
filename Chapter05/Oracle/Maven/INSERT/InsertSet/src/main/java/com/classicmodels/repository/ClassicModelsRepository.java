e334cd7496843eab71dffe3de739261698fda2af
anghelleonard <leoprivacy@yahoo.com>
2022-01-08 19:11:51 +0200

    Tackling different kind of SELECTs, INSERTs, UPDATEs, DELETEs and MERGEs

--- Chapter05/Oracle/Maven/INSERT/InsertSet/src/main/java/com/classicmodels/repository/ClassicModelsRepository.java
+++ Chapter05/Oracle/Maven/INSERT/InsertSet/src/main/java/com/classicmodels/repository/ClassicModelsRepository.java
@@ -19,71 +19,88 @@ public class ClassicModelsRepository {
 
     // EXAMPLE 1
     /*
-    insert into "SYSTEM"."SALE" (
-      "FISCAL_YEAR", "SALE", "EMPLOYEE_NUMBER"
+    insert into "CLASSICMODELS"."SALE" (
+      "SALE_ID", "FISCAL_YEAR", "SALE", 
+      "EMPLOYEE_NUMBER", "FISCAL_MONTH", 
+      "REVENUE_GROWTH"
     ) 
     values 
-      (?, ?, ?)   
+      (?, ?, ?, ?, ?, ?)   
      */
     public void insertOneSale() {
 
         System.out.println("EXAMPLE 1 (affected rows): "
                 + ctx.insertInto(SALE)
-                        .set(SALE.FISCAL_YEAR, BigInteger.valueOf(2005))
+                        .set(SALE.SALE_ID, (long) (Math.random() * 500000))
+                        .set(SALE.FISCAL_YEAR, 2005)
                         .set(SALE.SALE_, 4523.33)
                         .set(SALE.EMPLOYEE_NUMBER, 1504L)
+                        .set(SALE.FISCAL_MONTH, 3)
+                        .set(SALE.REVENUE_GROWTH, 12.22)
                         .execute()
         );
     }
 
     // EXAMPLE 2
     /*
-    insert into "SYSTEM"."SALE" (
-      "FISCAL_YEAR", "SALE", "EMPLOYEE_NUMBER"
+    insert into "CLASSICMODELS"."SALE" (
+      "FISCAL_YEAR", "SALE", "EMPLOYEE_NUMBER", 
+      "FISCAL_MONTH", "REVENUE_GROWTH"
     ) 
     select 
       ?, 
       ?, 
+      ?, 
+      ?, 
       ? 
     from 
-      dual 
+      DUAL 
     union all 
     select 
       ?, 
       ?, 
+      ?, 
+      ?, 
       ? 
     from 
-      dual   
+      DUAL   
      */
     public void insertTwoSale() {
 
         System.out.println("EXAMPLE 2 (affected rows): "
                 + ctx.insertInto(SALE)
-                        .set(SALE.FISCAL_YEAR, BigInteger.valueOf(2005))
+                        .set(SALE.FISCAL_YEAR, 2005)
                         .set(SALE.SALE_, 4523.33)
                         .set(SALE.EMPLOYEE_NUMBER, 1504L)
+                        .set(SALE.FISCAL_MONTH, 3)
+                        .set(SALE.REVENUE_GROWTH, 12.22)
                         .newRecord()
-                        .set(SALE.FISCAL_YEAR, BigInteger.valueOf(2005))
+                        .set(SALE.FISCAL_YEAR, 2005)
                         .set(SALE.SALE_, 4523.33)
                         .set(SALE.EMPLOYEE_NUMBER, 1504L)
+                        .set(SALE.FISCAL_MONTH, 4)
+                        .set(SALE.REVENUE_GROWTH, 22.12)
                         .execute()
         );
     }
 
     // EXAMPLE 3
     /*
-    insert into "SYSTEM"."SALE" (
-      "SALE_ID", "FISCAL_YEAR", "SALE", 
-      "EMPLOYEE_NUMBER"
+    insert into "CLASSICMODELS"."SALE" (
+      "FISCAL_YEAR", "SALE", "EMPLOYEE_NUMBER", 
+      "HOT", "RATE", "VAT", "FISCAL_MONTH", 
+      "REVENUE_GROWTH", "TREND"
     ) 
     values 
-      (?, ?, ?, ?)   
+      (?, ?, ?, ?, ?, ?, ?, ?, ?)   
      */
     public void insertRecordSale() {
 
-        SaleRecord sr = new SaleRecord(null, BigInteger.valueOf(2003), 123.32, 1370L,
-                null, null, null, null);
+        SaleRecord sr = new SaleRecord(null,
+                2003, 123.32, 1370L, null, "SILVER", "MAX", 3, 12.22, "UP");
 
+        sr.reset(SALE.SALE_ID);
+        
         System.out.println("EXAMPLE 3 (affected rows): "
                 + ctx.insertInto(SALE)
                         .set(sr)
