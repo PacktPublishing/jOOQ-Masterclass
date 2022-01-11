ecc6639547938a4d6cbbd45100ae3135ec1d84bf
anghelleonard <leoprivacy@yahoo.com>
2022-01-11 16:45:33 +0200

    Tackling different kind of SELECTs, INSERTs, UPDATEs, DELETEs and MERGEs

---
+++ Chapter05/MySQL/Gradle/DELETE/DeleteSamples/src/main/java/com/classicmodels/service/ClassicModelsService.java
@@ -0,0 +1,26 @@
+package com.classicmodels.service;
+
+import com.classicmodels.repository.ClassicModelsRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClassicModelsService {
+
+    private final ClassicModelsRepository classicModelsRepository;
+
+    public ClassicModelsService(ClassicModelsRepository classicModelsRepository) {
+        this.classicModelsRepository = classicModelsRepository;
+    }
+
+    public void callAll() {
+
+        classicModelsRepository.simpleDeletes();                             // EXAMPLE 1
+        classicModelsRepository.deletePayment();                             // EXAMPLE 2
+        classicModelsRepository.deleteCustomerDetailViaNotIn();              // EXAMPLE 3
+        classicModelsRepository.deleteOrderByAndLimit();                     // EXAMPLE 4
+        classicModelsRepository.deleteCascade();                             // EXAMPLE 5
+        classicModelsRepository.deleteRecordImplicitWhere();                 // EXAMPLE 6
+        classicModelsRepository.moreDeleteRecordExamples();                  // EXAMPLE 7
+        classicModelsRepository.throwExceptionForDeleteWithoutWhereClause(); // EXAMPLE 8        
+    }
+}
