9e5b417d6f38c9a1851a010cb03a29dd4f950ec7
anghelleonard <leoprivacy@yahoo.com>
2022-01-11 17:26:11 +0200

    Tackling different kind of SELECTs, INSERTs, UPDATEs, DELETEs and MERGEs

---
+++ Chapter05/MySQL/Gradle/INSERT/InsertSet/src/main/java/com/classicmodels/repository/ClassicModelsRepository.java
@@ -0,0 +1,94 @@
+package com.classicmodels.repository;
+
+import jooq.generated.enums.SaleRate;
+import jooq.generated.enums.SaleVat;
+import static jooq.generated.tables.Sale.SALE;
+import jooq.generated.tables.records.SaleRecord;
+import org.jooq.DSLContext;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+@Transactional
+public class ClassicModelsRepository {
+
+    private final DSLContext ctx;
+
+    public ClassicModelsRepository(DSLContext ctx) {
+        this.ctx = ctx;
+    }
+
+    // EXAMPLE 1
+    /*
+    insert into `classicmodels`.`sale` (
+      `fiscal_year`, `sale`, `employee_number`, 
+      `fiscal_month`, `revenue_growth`
+    ) 
+    values 
+      (?, ?, ?, ?, ?)    
+     */
+    public void insertOneSale() {
+
+        System.out.println("EXAMPLE 1 (affected rows): "
+                + ctx.insertInto(SALE)
+                        .set(SALE.FISCAL_YEAR, 2005)
+                        .set(SALE.SALE_, 4523.33)
+                        .set(SALE.EMPLOYEE_NUMBER, 1504L)
+                        .set(SALE.FISCAL_MONTH, 3)
+                        .set(SALE.REVENUE_GROWTH, 12.22)
+                        .execute()
+        );
+    }
+
+    // EXAMPLE 2
+    /*
+    insert into `classicmodels`.`sale` (
+      `fiscal_year`, `sale`, `employee_number`, 
+      `fiscal_month`, `revenue_growth`
+    ) 
+    values 
+      (?, ?, ?, ?, ?), 
+      (?, ?, ?, ?, ?)    
+     */
+    public void insertTwoSale() {
+
+        System.out.println("EXAMPLE 2 (affected rows): "
+                + ctx.insertInto(SALE)
+                        .set(SALE.FISCAL_YEAR, 2005)
+                        .set(SALE.SALE_, 4523.33)
+                        .set(SALE.EMPLOYEE_NUMBER, 1504L)
+                        .set(SALE.FISCAL_MONTH, 3)
+                        .set(SALE.REVENUE_GROWTH, 12.22)
+                        .newRecord()
+                        .set(SALE.FISCAL_YEAR, 2005)
+                        .set(SALE.SALE_, 4523.33)
+                        .set(SALE.EMPLOYEE_NUMBER, 1504L)
+                        .set(SALE.FISCAL_MONTH, 4)
+                        .set(SALE.REVENUE_GROWTH, 22.12)
+                        .execute()
+        );
+    }
+
+    // EXAMPLE 3
+    /*
+    insert into `classicmodels`.`sale` (
+      `sale_id`, `fiscal_year`, `sale`, 
+      `employee_number`, `hot`, `rate`, 
+      `vat`, `fiscal_month`, `revenue_growth`, 
+      `trend`
+    ) 
+    values 
+      (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)    
+     */
+    public void insertRecordSale() {
+
+        SaleRecord sr = new SaleRecord(null,
+                2003, 123.32, 1370L, null, SaleRate.SILVER, SaleVat.MAX, 3, 12.22, "UP");
+
+        System.out.println("EXAMPLE 3 (affected rows): "
+                + ctx.insertInto(SALE)
+                        .set(sr)
+                        .execute()
+        );
+    }
+}
