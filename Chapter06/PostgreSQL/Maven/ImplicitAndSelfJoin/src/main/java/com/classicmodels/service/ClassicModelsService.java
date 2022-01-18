4816cda96ac098336c657011aa94e1f5e883c762
anghelleonard <leoprivacy@yahoo.com>
2022-01-18 18:36:48 +0200

    Tackling different kind of JOINs

---
+++ Chapter06/PostgreSQL/Maven/ImplicitAndSelfJoin/src/main/java/com/classicmodels/service/ClassicModelsService.java
@@ -0,0 +1,26 @@
+package com.classicmodels.service;
+
+import com.classicmodels.repository.ClassicModelsRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClassicModelsService {
+
+    private final ClassicModelsRepository classicModelsRepository;
+
+    public ClassicModelsService(ClassicModelsRepository classicModelsRepository) {
+        this.classicModelsRepository = classicModelsRepository;
+    }
+
+    public void callAll() {
+
+        classicModelsRepository.implicitJoinOfficeEmployeeViaWhere();                   // EXAMPLE 1
+        classicModelsRepository.implicitJoinOfficeEmployeeViaNavigationMethod();        // EXAMPLE 2
+        classicModelsRepository.implicitJoinPaymentCustomerViaNavigationMethod();       // EXAMPLE 3
+        classicModelsRepository.implicitJoinOrderCustomerEmployeeViaNavigationMethod(); // EXAMPLE 4
+        classicModelsRepository.selfJoinEmployee();                                     // EXAMPLE 5
+        classicModelsRepository.selfJoinEmployeeViaNavigationMethod();                  // EXAMPLE 6
+        classicModelsRepository.selfJoinComparingEmployeeViaNavigationMethod();         // EXAMPLE 7
+        classicModelsRepository.selfJoinThreeTimes();                                   // EXAMPLE 8
+    }
+}
