5aacabc85d13b314e2c9de0ce0f32b77c7a619a5
anghelleonard <leoprivacy@yahoo.com>
2022-01-18 09:08:07 +0200

    Tackling different kind of JOINs

--- Chapter06/Oracle/Maven/CrossJoin/src/main/java/com/classicmodels/repository/ClassicModelsRepository.java
+++ Chapter06/Oracle/Maven/CrossJoin/src/main/java/com/classicmodels/repository/ClassicModelsRepository.java
@@ -13,12 +13,13 @@ import static jooq.generated.tables.Product.PRODUCT;
 import org.jooq.DSLContext;
 import static org.jooq.impl.DSL.concat;
 import static org.jooq.impl.DSL.field;
+import static org.jooq.impl.DSL.inline;
 import static org.jooq.impl.DSL.name;
 import static org.jooq.impl.DSL.nvl;
 import static org.jooq.impl.DSL.row;
 import static org.jooq.impl.DSL.select;
 import static org.jooq.impl.DSL.sum;
-import static org.jooq.impl.DSL.val;
+import static org.jooq.impl.DSL.trueCondition;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -61,7 +62,9 @@ public class ClassicModelsRepository {
                 + ctx.select()
                         .from(OFFICE)
                         .innerJoin(DEPARTMENT)
-                        .on(val(1).eq(val(1)))
+                        .on(trueCondition())
+                        // .on(one().eq(one()))
+                        // .on(val(1).eq(val(1)))
                         .fetch()
         );
     }
@@ -94,8 +97,8 @@ public class ClassicModelsRepository {
     public void joinOfficeDepartmentConcatCertainColsViaCrossJoin() {
 
         System.out.println("EXAMPLE 6\n"
-                + ctx.select(concat(OFFICE.CITY, val(", "), OFFICE.COUNTRY,
-                        val(": "), DEPARTMENT.NAME).as("offices"))
+                + ctx.select(concat(OFFICE.CITY, inline(", "), OFFICE.COUNTRY,
+                        inline(": "), DEPARTMENT.NAME).as("offices"))
                         .from(OFFICE)
                         .crossJoin(DEPARTMENT)
                         .where(row(OFFICE.CITY, OFFICE.COUNTRY).isNotNull())
@@ -105,39 +108,45 @@ public class ClassicModelsRepository {
 
     // EXAMPLE 7
     /*
-    select 
-      "c"."CUSTOMER_NUMBER", 
-      "p"."PRODUCT_ID", 
-      nvl("sales", 0) 
-    from 
-      "SYSTEM"."CUSTOMER" "c" cross 
-      join "SYSTEM"."PRODUCT" "p" 
-      left outer join (
-        select 
-          "c"."CUSTOMER_NUMBER" "cn", 
-          "p"."PRODUCT_ID" "pi", 
-          sum(
-            (
-              "i"."QUANTITY_ORDERED" * "i"."PRICE_EACH"
-            )
-          ) "sales" 
-        from 
-          "SYSTEM"."ORDER" "o" 
-          join "SYSTEM"."ORDERDETAIL" "i" on "i"."ORDER_ID" = "o"."ORDER_ID" 
-          join "SYSTEM"."CUSTOMER" "c" on "c"."CUSTOMER_NUMBER" = "o"."CUSTOMER_NUMBER" 
-          join "SYSTEM"."PRODUCT" "p" on "p"."PRODUCT_ID" = "i"."PRODUCT_ID" 
-        group by 
-          "c"."CUSTOMER_NUMBER", 
-          "p"."PRODUCT_ID"
-      ) "alias_95246217" on (
-        "cn" = "c"."CUSTOMER_NUMBER" 
-        and "pi" = "p"."PRODUCT_ID"
-      ) 
-    where 
-      "sales" is null 
-    order by 
-      "p"."PRODUCT_ID", 
-      "c"."CUSTOMER_NUMBER"    
+    select
+        "c"."CUSTOMER_NUMBER",
+        "p"."PRODUCT_ID",
+        nvl("sales",
+        ?) 
+    from
+        "CLASSICMODELS"."CUSTOMER" "c" cross 
+    join
+        "CLASSICMODELS"."PRODUCT" "p" 
+    left outer join
+        (
+            select
+                "c"."CUSTOMER_NUMBER" "cn",
+                "p"."PRODUCT_ID" "pi",
+                sum(("i"."QUANTITY_ORDERED" * "i"."PRICE_EACH")) "sales" 
+            from
+                "CLASSICMODELS"."ORDER" "o" 
+            join
+                "CLASSICMODELS"."ORDERDETAIL" "i" 
+                    on "i"."ORDER_ID" = "o"."ORDER_ID" 
+            join
+                "CLASSICMODELS"."CUSTOMER" "c" 
+                    on "c"."CUSTOMER_NUMBER" = "o"."CUSTOMER_NUMBER" 
+            join
+                "CLASSICMODELS"."PRODUCT" "p" 
+                    on "p"."PRODUCT_ID" = "i"."PRODUCT_ID" 
+            group by
+                "c"."CUSTOMER_NUMBER",
+                "p"."PRODUCT_ID"
+        ) "alias_119882641" 
+            on (
+                "cn" = "c"."CUSTOMER_NUMBER" 
+                and "pi" = "p"."PRODUCT_ID"
+            ) 
+    where
+        "sales" is null 
+    order by
+        "p"."PRODUCT_ID",
+        "c"."CUSTOMER_NUMBER"    
     */
     public void findProductsNoSalesAcrossCustomers() {
 
