63c6995d46cddbcddcee5003d7de66872b5089ec
anghelleonard <leoprivacy@yahoo.com>
2022-01-25 11:21:00 +0200

    Types, Converters, and Bindings

---
+++ Chapter07/PostgreSQL/Maven/Binding/JsonBinding/src/main/java/com/classicmodels/service/ClassicModelsService.java
@@ -0,0 +1,21 @@
+package com.classicmodels.service;
+
+import com.classicmodels.repository.ClassicModelsRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClassicModelsService {
+
+    private final ClassicModelsRepository classicModelsRepository;
+
+    public ClassicModelsService(ClassicModelsRepository classicModelsRepository) {
+        this.classicModelsRepository = classicModelsRepository;
+    }
+
+    public void callAll() throws JsonProcessingException {        
+                
+        classicModelsRepository.insertManagerWithDetail();
+        classicModelsRepository.fetchDetailOfManager();
+    }
+}
