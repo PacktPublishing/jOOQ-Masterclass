360b6b97738ec4f23e1302f69d3e65ccafd5b041
anghelleonard <leoprivacy@yahoo.com>
2022-01-25 10:37:12 +0200

    Types, Converters, and Bindings

--- Chapter07/PostgreSQL/Maven/Binding/InetBinding/src/main/java/com/classicmodels/repository/ClassicModelsRepository.java
+++ Chapter07/PostgreSQL/Maven/Binding/InetBinding/src/main/java/com/classicmodels/repository/ClassicModelsRepository.java
@@ -3,7 +3,7 @@ package com.classicmodels.repository;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.List;
-import static jooq.generated.Sequences.DEPARTMENT_DEPARTMENT_ID_SEQ;
+import java.util.concurrent.ThreadLocalRandom;
 import static jooq.generated.tables.Department.DEPARTMENT;
 import jooq.generated.tables.pojos.Department;
 import jooq.generated.tables.records.DepartmentRecord;
@@ -23,7 +23,7 @@ public class ClassicModelsRepository {
 
     @Transactional
     public void insertDepInet() throws UnknownHostException {
-
+        
         /*
         // workaround to avoid defining a binding/converter
         // to test this, you need to disable the current binding
@@ -31,50 +31,59 @@ public class ClassicModelsRepository {
         
         // non type-safe (don't do this!)
         ctx.insertInto(DEPARTMENT)
-                .values(DEPARTMENT_DEPARTMENT_ID_SEQ.nextval(), "HR", "-int 8799",
-                        (short) 2231, "4", new String[]{"hiring", "interiew"},
+                .values(100, "HR", "-int 8799",
+                        2231, "4", new String[]{"hiring", "interiew"},
                         //InetAddress.getByName("128.10.124.12")) - this will not work                        
-                        field("?::inet", String.class, addr.getHostName())) 
+                        field("?::inet", String.class, addr.getHostName()),
+                        0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0) 
+                .onDuplicateKeyIgnore()
                 .execute();
         
         // type-safe (don't do this!)
         ctx.insertInto(DEPARTMENT, DEPARTMENT.NAME, DEPARTMENT.PHONE,
                 DEPARTMENT.CODE, DEPARTMENT.OFFICE_CODE, DEPARTMENT.TOPIC, DEPARTMENT.DEP_NET_IPV4)
                 .values("HR", "-int 8799",
-                        (short) 2231, "4", new String[]{"hiring", "interiew"},
+                         2231, "4", new String[]{"hiring", "interiew"},
                         //InetAddress.getByName("128.10.124.12")) - this will not work                        
                         field("?::inet", String.class, addr.getHostName())) // to test this, you need to disable the converter
-                .execute();
-         */
+                .onDuplicateKeyIgnore()
+                .execute();        
+        */
         
         // non type-safe, use the InetConverter
         ctx.insertInto(DEPARTMENT)
-                .values(DEPARTMENT_DEPARTMENT_ID_SEQ.nextval(),
-                        "HR", "-int 8799", (short) 2231, "4", new String[]{"hiring", "interiew"},
-                        InetAddress.getByName("128.10.124.12"))
+                .values(100, "HR", "-int 8799", 2231, "4", new String[]{"hiring", "interiew"},
+                        InetAddress.getByName("128.10.124.12"),
+                        0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
+                .onDuplicateKeyIgnore()
                 .execute();
 
         // type-safe, use the InetConverter
         ctx.insertInto(DEPARTMENT, DEPARTMENT.NAME, DEPARTMENT.PHONE,
                 DEPARTMENT.CODE, DEPARTMENT.OFFICE_CODE, DEPARTMENT.TOPIC, DEPARTMENT.DEP_NET_IPV4)
-                .values("HR", "-int 8799", (short) 2231, "4", new String[]{"hiring", "interiew"},
+                .values("HR", "-int 8799", 2231, "4", new String[]{"hiring", "interiew"},
                         InetAddress.getByName("128.10.124.12"))
+                .onDuplicateKeyIgnore()
                 .execute();
 
-        // use the InetConverter and Department POJO         
-        Integer nextId1 = ctx.select(DEPARTMENT_DEPARTMENT_ID_SEQ.nextval()).fetchOneInto(Integer.class);
-        Department department = new Department(nextId1,
-                "HR", "-int 8799", (short) 2231, "4", new String[]{"hiring", "interiew"},
-                InetAddress.getByName("128.10.124.12"));
-        ctx.newRecord(DEPARTMENT, department).insert();
+        // use the InetConverter and Department POJO                 
+        Department department = new Department(null,
+                "HR", "-int 8799", 
+                ThreadLocalRandom.current().nextInt(10000, 20000), // random code
+                "4", new String[]{"hiring", "interiew"},
+                InetAddress.getByName("128.10.124.12"),
+                0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0);
+        ctx.newRecord(DEPARTMENT, department).insert();        
 
         // use the InetConverter and DepartmentRecord
-        Integer nextId2 = ctx.select(DEPARTMENT_DEPARTMENT_ID_SEQ.nextval()).fetchOneInto(Integer.class);
-        DepartmentRecord departmentr = new DepartmentRecord(nextId2,
-                "HR", "-int 8799", (short) 2231, "4", new String[]{"hiring", "interiew"},
-                InetAddress.getByName("128.10.124.12"));
+        DepartmentRecord departmentr = new DepartmentRecord(
+                ThreadLocalRandom.current().nextInt(10000, 20000), // random PK
+                "HR", "-int 8799", 
+                ThreadLocalRandom.current().nextInt(10000, 20000), // random code
+                "4", new String[]{"hiring", "interiew"},
+                InetAddress.getByName("128.10.124.12"),
+                0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0);
         ctx.insertInto(DEPARTMENT).values(departmentr.valuesRow().fields()).execute();
-
     }
 
     public void fetchDepInet() {
@@ -95,8 +104,8 @@ public class ClassicModelsRepository {
                 });
         
         System.out.println("Inets: " + inets);
-         */
-        
+        */
+                
         // use the InetConverter
         List<InetAddress> inets = ctx.select(DEPARTMENT.DEP_NET_IPV4)
                 .from(DEPARTMENT)
