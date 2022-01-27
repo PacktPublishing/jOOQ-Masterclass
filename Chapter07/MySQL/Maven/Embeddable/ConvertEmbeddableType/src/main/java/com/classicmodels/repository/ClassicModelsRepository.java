e4acd100fb4864b5fa801b0fc08b7be366365414
anghelleonard <leoprivacy@yahoo.com>
2022-01-27 09:03:07 +0200

    Types, Converters, and Bindings

--- Chapter07/MySQL/Maven/Embeddable/ConvertEmbeddableType/src/main/java/com/classicmodels/repository/ClassicModelsRepository.java
+++ Chapter07/MySQL/Maven/Embeddable/ConvertEmbeddableType/src/main/java/com/classicmodels/repository/ClassicModelsRepository.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 import jooq.generated.embeddables.pojos.OfficeFullAddress;
 import jooq.generated.embeddables.records.OfficeFullAddressRecord;
 import static jooq.generated.tables.Office.OFFICE;
@@ -29,10 +30,11 @@ public class ClassicModelsRepository {
 
         // using embeddable type via OfficeFullAddressRecord   
         ctx.insertInto(OFFICE, OFFICE.OFFICE_CODE, OFFICE.OFFICE_FULL_ADDRESS, OFFICE.PHONE,
-                OFFICE.ADDRESS_LINE_SECOND, OFFICE.POSTAL_CODE)
-                .values(String.valueOf((int) (Math.random() * 1000)),
+                OFFICE.ADDRESS_LINE_SECOND, OFFICE.POSTAL_CODE, OFFICE.INTERNAL_BUDGET)
+                .values(String.valueOf(ThreadLocalRandom.current().nextInt(10000, 20000)), // random office_code
                         new OfficeFullAddressRecord("Naples", "Giuseppe Mazzini", "Campania", "Italy", "N/A"),
-                        "09822-1229-12", "N/A", "zip-2322")
+                        "09822-1229-12", "N/A", "zip-2322", 0)
+                .onDuplicateKeyIgnore()
                 .execute();
 
         // using JsonNode        
@@ -41,10 +43,11 @@ public class ClassicModelsRepository {
                 + "\"state\":\"Catalonia\",\"country\":\"Spain\",\"territory\":\"N/A\"}");
 
         ctx.insertInto(OFFICE, OFFICE.OFFICE_CODE, OFFICE.OFFICE_FULL_ADDRESS, OFFICE.PHONE,
-                OFFICE.ADDRESS_LINE_SECOND, OFFICE.POSTAL_CODE)
-                .values(String.valueOf((int) (Math.random() * 1000)),
+                OFFICE.ADDRESS_LINE_SECOND, OFFICE.POSTAL_CODE, OFFICE.INTERNAL_BUDGET)
+                .values(String.valueOf(ThreadLocalRandom.current().nextInt(10000, 20000)), // random office_code
                         JSON_CONVERTER.to(officeFullAddress),
-                        "09822-1229-12", "N/A", "zip-2322")
+                        "09822-1229-12", "N/A", "zip-2322", 0)
+                .onDuplicateKeyIgnore()
                 .execute();
     }
 
