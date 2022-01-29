bb91dfe73791b5563f7c6d84170240edce3362cc
anghelleonard <leoprivacy@yahoo.com>
2022-01-29 11:02:02 +0200

    Types, Converters, and Bindings

---
+++ Chapter07/MySQL/Gradle/Converter/ProgrammaticJsonConverter/jooq-code-generator/src/main/java/com/classicmodels/jooq/config/JooqConfig.java
@@ -0,0 +1,46 @@
+package com.classicmodels.jooq.config;
+
+import org.jooq.codegen.GenerationTool;
+import org.jooq.meta.jaxb.Configuration;
+import org.jooq.meta.jaxb.Database;
+import org.jooq.meta.jaxb.ForcedType;
+import org.jooq.meta.jaxb.Generator;
+import org.jooq.meta.jaxb.Jdbc;
+import org.jooq.meta.jaxb.Target;
+
+public class JooqConfig {
+
+    public static void main(String[] args) throws Exception {
+
+        Configuration configuration = new Configuration()
+                .withJdbc(new Jdbc()
+                        .withDriver("com.mysql.cj.jdbc.Driver")
+                        .withUrl("jdbc:mysql://localhost:3306/classicmodels?createDatabaseIfNotExist=true")
+                        .withUser("root")
+                        .withPassword("root"))
+                .withGenerator(new Generator()
+                        .withName("org.jooq.codegen.JavaGenerator")
+                        .withDatabase(new Database()
+                                .withForcedTypes(
+                                        new ForcedType()
+                                                .withUserType("com.fasterxml.jackson.databind.JsonNode")
+                                                .withConverter("com.classicmodels.converter.JsonConverter")
+                                                .withIncludeExpression("classicmodels\\.manager\\.manager_detail")
+                                                .withExcludeTypes(".*\\."))
+                                .withName("org.jooq.meta.mysql.MySQLDatabase")
+                                .withInputSchema("classicmodels")
+                                .withIncludes(".*")
+                                .withExcludes("flyway_schema_history | sequences"
+                                        + " | customer_pgs | refresh_top3_product"
+                                        + " | sale_.* | set_.* | get_.* | .*_master")
+                                .withSchemaVersionProvider("SELECT MAX(`version`) FROM `flyway_schema_history`")
+                                .withLogSlowQueriesAfterSeconds(20)
+                        )
+                        .withTarget(new Target()
+                                .withPackageName("jooq.generated")
+                                .withDirectory(System.getProperty("user.dir") 
+                                      + "/../webapp/build/generated-sources")));
+
+        GenerationTool.generate(configuration);
+    }
+}
