925f137c954c65d93242346a4c6a8705a8b3dabb
anghelleonard <leoprivacy@yahoo.com>
2022-01-30 14:44:29 +0200

    Types, Converters, and Bindings

---
+++ Chapter07/PostgreSQL/Gradle/Enum/SimpleBuiltInEnumConverter/src/main/java/com/classicmodels/repository/ClassicModelsRepository.java
@@ -0,0 +1,67 @@
+package com.classicmodels.repository;
+
+import com.classicmodels.enums.RateType;
+import com.classicmodels.enums.TrendType;
+import java.util.List;
+import jooq.generated.enums.VatType;
+import static jooq.generated.tables.Sale.SALE;
+import org.jooq.DSLContext;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+@Transactional(readOnly = true)
+public class ClassicModelsRepository {
+
+    private final DSLContext ctx;
+
+    public ClassicModelsRepository(DSLContext ctx) {
+        this.ctx = ctx;
+    }
+
+    @Transactional
+    public void insertSale() {
+
+        // rely on <forcedType/> and <enumConverter/>
+        ctx.insertInto(SALE, SALE.FISCAL_YEAR, SALE.SALE_, SALE.FISCAL_MONTH, SALE.REVENUE_GROWTH,
+                SALE.EMPLOYEE_NUMBER, SALE.RATE)
+                .values(2005, 56444.32, 1, 0.0, 1370L, RateType.PLATINUM)
+                .execute();
+
+        // rely on jOOQ generated VatType
+        ctx.insertInto(SALE, SALE.FISCAL_YEAR, SALE.SALE_, SALE.FISCAL_MONTH, SALE.REVENUE_GROWTH,
+                SALE.EMPLOYEE_NUMBER, SALE.VAT)
+                .values(2005, 56444.32, 1, 0.0, 1370L, VatType.MAX)
+                .execute();
+
+        // rely on <forcedType/> and <enumConverter/>
+        ctx.insertInto(SALE, SALE.FISCAL_YEAR, SALE.SALE_, SALE.FISCAL_MONTH, SALE.REVENUE_GROWTH,
+                SALE.EMPLOYEE_NUMBER, SALE.TREND)
+                .values(2005, 56444.32, 1, 0.0, 1370L, TrendType.UP)
+                .execute();
+    }
+
+    public void fetchSale() {
+
+        List<RateType> rates = ctx.select(SALE.RATE)
+                .from(SALE)
+                .where(SALE.RATE.isNotNull())
+                .fetch(SALE.RATE);
+
+        System.out.println("Rates: " + rates);
+        
+        List<VatType> vats = ctx.select(SALE.VAT)
+                .from(SALE)
+                .where(SALE.VAT.isNotNull())
+                .fetch(SALE.VAT);
+
+        System.out.println("Vats: " + vats);
+
+        List<TrendType> trends = ctx.select(SALE.TREND)
+                .from(SALE)
+                .where(SALE.TREND.isNotNull())
+                .fetch(SALE.TREND);
+
+        System.out.println("Trends: " + trends);
+    }
+}
