f799fd8b556a3779cafb95df73dadfcf6777c975
anghelleonard <leoprivacy@yahoo.com>
2022-01-31 08:24:57 +0200

    Types, Converters, and Bindings

---
+++ Chapter07/Oracle/Gradle/Enum/BuiltInEnumSqlConverter/src/main/java/com/classicmodels/converter/SaleRateStarConverter.java
@@ -0,0 +1,33 @@
+package com.classicmodels.converter;
+
+import com.classicmodels.enums.RateType;
+import com.classicmodels.enums.StarType;
+import org.jooq.impl.EnumConverter;
+
+public class SaleRateStarConverter extends EnumConverter<RateType, StarType> {
+
+    public final static SaleRateStarConverter SALE_RATE_STAR_CONVERTER = new SaleRateStarConverter();
+
+    public SaleRateStarConverter() {
+        super(RateType.class, StarType.class);
+    }
+
+    @Override
+    public RateType to(StarType u) {
+
+        if (u != null) {
+
+            return switch (u) {
+                case THREE_STARS ->
+                    RateType.SILVER;
+                case FOUR_STARS ->
+                    RateType.GOLD;
+                case FIVE_STARS ->
+                    RateType.PLATINUM;
+            };
+        }
+
+        return null;
+    }
+
+}
