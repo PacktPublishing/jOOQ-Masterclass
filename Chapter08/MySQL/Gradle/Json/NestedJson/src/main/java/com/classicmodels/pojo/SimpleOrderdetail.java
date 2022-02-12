30d80015814fae09bb9482fcbfe0f3160cddebcc
anghelleonard <leoprivacy@yahoo.com>
2022-02-12 10:59:58 +0200

    Fetching and mapping

---
+++ Chapter08/MySQL/Gradle/Json/NestedJson/src/main/java/com/classicmodels/pojo/SimpleOrderdetail.java
@@ -0,0 +1,63 @@
+package com.classicmodels.pojo;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Objects;
+
+public class SimpleOrderdetail implements Serializable {
+
+    private static final long serialVersionUID = 1;
+    
+    private Integer quantityOrdered;
+    private BigDecimal priceEach;
+
+    public Integer getQuantityOrdered() {
+        return quantityOrdered;
+    }
+
+    public void setQuantityOrdered(Integer quantityOrdered) {
+        this.quantityOrdered = quantityOrdered;
+    }
+
+    public BigDecimal getPriceEach() {
+        return priceEach;
+    }
+
+    public void setPriceEach(BigDecimal priceEach) {
+        this.priceEach = priceEach;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 29 * hash + Objects.hashCode(this.quantityOrdered);
+        hash = 29 * hash + Objects.hashCode(this.priceEach);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final SimpleOrderdetail other = (SimpleOrderdetail) obj;
+        if (!Objects.equals(this.quantityOrdered, other.quantityOrdered)) {
+            return false;
+        }
+        if (!Objects.equals(this.priceEach, other.priceEach)) {
+            return false;
+        }
+        return true;
+    }        
+
+    @Override
+    public String toString() {
+        return "Orderdetail{" + "quantityOrdered=" + quantityOrdered + ", priceEach=" + priceEach + '}';
+    }        
+}
