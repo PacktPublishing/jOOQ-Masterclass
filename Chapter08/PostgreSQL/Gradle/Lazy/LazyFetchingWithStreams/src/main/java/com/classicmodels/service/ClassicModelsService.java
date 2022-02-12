b744845c370b48ee79277ff47b0c32a6c6bf58f4
anghelleonard <leoprivacy@yahoo.com>
2022-02-12 16:56:54 +0200

    Fetching and mapping

---
+++ Chapter08/PostgreSQL/Gradle/Lazy/LazyFetchingWithStreams/src/main/java/com/classicmodels/service/ClassicModelsService.java
@@ -0,0 +1,23 @@
+package com.classicmodels.service;
+
+import com.classicmodels.repository.ClassicModelsRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClassicModelsService {
+
+    private final ClassicModelsRepository classicModelsRepository;
+
+    public ClassicModelsService(ClassicModelsRepository classicModelsRepository) {
+        this.classicModelsRepository = classicModelsRepository;
+    }
+
+    public void callAll() {
+
+        classicModelsRepository.eagerAndLazyFetchingWithStreams();
+        classicModelsRepository.eagerAndLazyFetchingWithStreamsAndTransactional();
+        classicModelsRepository.lazyFetchingViaFetchStream();
+        classicModelsRepository.lazyCollectingAndFetchStream();
+        classicModelsRepository.lazyFetchingGroupsViaCollect();        
+    }
+}
