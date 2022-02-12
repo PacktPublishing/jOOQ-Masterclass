a543c74378af74e7b02dc5d5283bcaf43d7c3d56
anghelleonard <leoprivacy@yahoo.com>
2022-02-12 10:30:18 +0200

    Fetching and mapping

---
+++ Chapter08/MySQL/Gradle/Basic/Records/src/main/java/com/classicmodels/pojo/NamePhone.java
@@ -0,0 +1,22 @@
+package com.classicmodels.pojo;
+
+import java.io.Serializable;
+
+public class NamePhone implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    
+    String customerName;
+    String phone;  
+
+    public NamePhone(String customerName, String phone) {
+        this.customerName = customerName;
+        this.phone = phone;
+    }        
+
+    @Override
+    public String toString() {
+        return "NamePhone{" + "customerName=" + customerName 
+                + ", customerPhone=" + phone + '}';
+    }        
+}
---
+++ Chapter08/MySQL/Gradle/Basic/Records/src/main/java/com/classicmodels/pojo/PhoneCreditLimit.java
@@ -0,0 +1,5 @@
+package com.classicmodels.pojo;
+
+import java.math.BigDecimal;
+
+public record PhoneCreditLimit(String phone, BigDecimal creditLimit) {}
