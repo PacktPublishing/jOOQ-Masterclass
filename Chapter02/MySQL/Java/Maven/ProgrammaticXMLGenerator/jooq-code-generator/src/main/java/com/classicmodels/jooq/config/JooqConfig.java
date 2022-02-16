2f4fc3d83ef18b512d66758337e5983dd83c2871
anghelleonard <leoprivacy@yahoo.com>
2022-02-16 18:59:13 +0200

    Customizing jOOQ level of involvement

---
+++ Chapter02/MySQL/Java/Maven/ProgrammaticXMLGenerator/jooq-code-generator/src/main/java/com/classicmodels/jooq/config/JooqConfig.java
@@ -0,0 +1,64 @@
+package com.classicmodels.jooq.config;
+
+import org.jooq.codegen.GenerationTool;
+import org.jooq.meta.jaxb.Configuration;
+import org.jooq.meta.jaxb.Database;
+import org.jooq.meta.jaxb.Generate;
+import org.jooq.meta.jaxb.Generator;
+import org.jooq.meta.jaxb.Jdbc;
+import org.jooq.meta.jaxb.MatcherRule;
+import org.jooq.meta.jaxb.MatcherTransformType;
+import org.jooq.meta.jaxb.Matchers;
+import org.jooq.meta.jaxb.MatchersTableType;
+import org.jooq.meta.jaxb.Property;
+import org.jooq.meta.jaxb.Strategy;
+import org.jooq.meta.jaxb.Target;
+
+public class JooqConfig {
+
+    public static void main(String[] args) throws Exception {
+
+        Configuration configuration = new Configuration()
+                .withJdbc(new Jdbc()
+                        .withDriver("com.mysql.cj.jdbc.Driver")
+                        .withUrl("jdbc:mysql://localhost:3306/classicmodels?createDatabaseIfNotExist=true")
+                        .withUser("root")
+                        .withPassword("root"))
+                .withGenerator(new Generator()
+                        .withName("org.jooq.codegen.JavaGenerator")
+                        .withDatabase(new Database()
+                                .withName("org.jooq.meta.xml.XMLDatabase")       
+                                .withProperties(
+                                        new Property().withKey("dialect").withValue("MYSQL"),
+                                        new Property().withKey("xmlFile").withValue("./../../../../../db/migration/xml/mysql/sql.xml")
+                                )
+                                .withIncludes(".*")
+                                .withExcludes("flyway_schema_history | sequences"
+                                        + " | customer_pgs | refresh_top3_product"
+                                        + " | sale_.* | set_.* | get_.* | .*_master")                                
+                                .withSchemaVersionProvider("com.classicmodels.jooq.config.MySchemaVersionProvider")                                
+                        )
+                        .withGenerate(new Generate()
+                                .withDaos(true)
+                                .withValidationAnnotations(Boolean.TRUE)
+                                .withSpringAnnotations(Boolean.TRUE)
+                        )
+                        .withStrategy(new Strategy()
+                                .withMatchers(new Matchers()
+                                        .withTables(new MatchersTableType()
+                                                .withPojoClass(new MatcherRule()
+                                                        .withExpression("Jooq_$0")
+                                                        .withTransform(MatcherTransformType.PASCAL))
+                                                .withDaoClass(new MatcherRule()
+                                                        .withExpression("$0_Repository")
+                                                        .withTransform(MatcherTransformType.PASCAL))))
+                        )
+                        .withTarget(new Target()
+                                .withPackageName("jooq.generated")
+                                .withDirectory(System.getProperty("user.dir").endsWith("webapp")
+                                        ? "target/generated-sources"
+                                        : "webapp/target/generated-sources")));
+
+        GenerationTool.generate(configuration);
+    }
+}
---
+++ Chapter02/MySQL/Java/Maven/ProgrammaticXMLGenerator/webapp/src/main/java/com/classicmodels/controller/ClassicModelsController.java
@@ -0,0 +1,42 @@
+package com.classicmodels.controller;
+
+import com.classicmodels.pojo.CustomerAndOrder;
+import com.classicmodels.service.ClassicModelsService;
+import java.time.LocalDate;
+import java.util.List;
+import jooq.generated.tables.pojos.JooqOffice;
+import jooq.generated.tables.pojos.JooqOrder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ClassicModelsController {
+
+    private final ClassicModelsService classicModelsService;
+
+    public ClassicModelsController(ClassicModelsService classicModelsService) {
+        this.classicModelsService = classicModelsService;
+    }
+
+    @GetMapping("/officesInTerritory")
+    public List<JooqOffice> fetchOfficesInTerritory(@RequestParam String territory) {
+
+        return classicModelsService.fetchOfficesInTerritory(territory);
+    }
+
+    @GetMapping("/ordersByRequiredDate")
+    public List<JooqOrder> fetchOrdersByRequiredDate(
+            @RequestParam String startDate, @RequestParam String endDate) {
+
+        return classicModelsService.fetchOrdersByRequiredDate(
+                LocalDate.parse(startDate), LocalDate.parse(endDate));
+    }
+
+    @GetMapping("/customersAndOrders")
+    public List<CustomerAndOrder> fetchCustomersAndOrders() {
+
+        return classicModelsService.fetchCustomersAndOrders();
+    }
+
+}
