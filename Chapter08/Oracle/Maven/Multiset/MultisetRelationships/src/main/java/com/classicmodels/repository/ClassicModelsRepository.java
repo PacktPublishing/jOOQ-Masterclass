41778e2fba56c410a5dc49a6a4cc66c50ff823c9
anghelleonard <leoprivacy@yahoo.com>
2022-02-17 11:40:34 +0200

    Fetching and mapping

--- Chapter08/Oracle/Maven/Multiset/MultisetRelationships/src/main/java/com/classicmodels/repository/ClassicModelsRepository.java
+++ Chapter08/Oracle/Maven/Multiset/MultisetRelationships/src/main/java/com/classicmodels/repository/ClassicModelsRepository.java
@@ -84,9 +84,28 @@ public class ClassicModelsRepository {
         System.out.println("Many-to-many:\n" + result1);
         System.out.println("Many-to-many (JSON): " + result1.formatJSON());
         System.out.println("Many-to-many (XML): " + result1.formatXML());
+        
+        //  jOOQ’s type safe implicit to-one join feature
+        var result2 = ctx.select(
+                MANAGER.MANAGER_ID, MANAGER.MANAGER_NAME,
+                multiset(
+                        select(
+                                OFFICE_HAS_MANAGER.office().OFFICE_CODE, 
+                                OFFICE_HAS_MANAGER.office().CITY, 
+                                OFFICE_HAS_MANAGER.office().STATE)
+                                .from(OFFICE_HAS_MANAGER)                                
+                                .where(MANAGER.MANAGER_ID.eq(OFFICE_HAS_MANAGER.MANAGERS_MANAGER_ID))
+                ).as("offices"))
+                .from(MANAGER)
+                .orderBy(MANAGER.MANAGER_ID)
+                .fetch();
+        
+        System.out.println("Many-to-many:\n" + result2);
+        System.out.println("Many-to-many (JSON): " + result2.formatJSON());
+        System.out.println("Many-to-many (XML): " + result2.formatXML());
 
         // Result<Record4<String, String, String, Result<Record2<Long, String>>>>
-        var result2 = ctx.select(
+        var result3 = ctx.select(
                 OFFICE.OFFICE_CODE, OFFICE.CITY, OFFICE.STATE,
                 multiset(
                         select(
@@ -100,8 +119,26 @@ public class ClassicModelsRepository {
                 .orderBy(OFFICE.OFFICE_CODE)
                 .fetch();
 
-        System.out.println("Many-to-many:\n" + result2);
-        System.out.println("Many-to-many (JSON): " + result2.formatJSON());
-        System.out.println("Many-to-many (XML): " + result2.formatXML());
+        System.out.println("Many-to-many:\n" + result3);
+        System.out.println("Many-to-many (JSON): " + result3.formatJSON());
+        System.out.println("Many-to-many (XML): " + result3.formatXML());
+        
+        //  jOOQ’s type safe implicit to-one join feature
+        var result4 = ctx.select(
+                OFFICE.OFFICE_CODE, OFFICE.CITY, OFFICE.STATE,
+                multiset(
+                        select(
+                                OFFICE_HAS_MANAGER.manager().MANAGER_ID, 
+                                OFFICE_HAS_MANAGER.manager().MANAGER_NAME)
+                                .from(OFFICE_HAS_MANAGER)
+                                .where(OFFICE.OFFICE_CODE.eq(OFFICE_HAS_MANAGER.OFFICES_OFFICE_CODE))
+                ).as("managers"))
+                .from(OFFICE)
+                .orderBy(OFFICE.OFFICE_CODE)
+                .fetch();
+        
+        System.out.println("Many-to-many:\n" + result4);
+        System.out.println("Many-to-many (JSON): " + result4.formatJSON());
+        System.out.println("Many-to-many (XML): " + result4.formatXML());
     }    
 }
--- Chapter08/SQLServer/Gradle/Multiset/MultisetRelationships/src/main/java/com/classicmodels/repository/ClassicModelsRepository.java
+++ Chapter08/SQLServer/Gradle/Multiset/MultisetRelationships/src/main/java/com/classicmodels/repository/ClassicModelsRepository.java
@@ -10,7 +10,6 @@ import static jooq.generated.tables.Productline.PRODUCTLINE;
 import org.jooq.DSLContext;
 import static org.jooq.impl.DSL.multiset;
 import static org.jooq.impl.DSL.select;
-import static org.jooq.impl.DSL.selectDistinct;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -49,7 +48,7 @@ public class ClassicModelsRepository {
         var result = ctx.select(
                 PRODUCTLINE.PRODUCT_LINE, PRODUCTLINE.TEXT_DESCRIPTION,
                 multiset(
-                        selectDistinct(PRODUCT.PRODUCT_VENDOR, PRODUCT.QUANTITY_IN_STOCK)
+                        select(PRODUCT.PRODUCT_VENDOR, PRODUCT.QUANTITY_IN_STOCK)
                                 .from(PRODUCT)
                                 .where(PRODUCTLINE.PRODUCT_LINE.eq(PRODUCT.PRODUCT_LINE))
                 ).as("products"))
@@ -68,7 +67,7 @@ public class ClassicModelsRepository {
         var result1 = ctx.select(
                 MANAGER.MANAGER_ID, MANAGER.MANAGER_NAME,
                 multiset(
-                        selectDistinct(
+                        select( // or, selectDistinct if duplicates are involved
                                 OFFICE.OFFICE_CODE, OFFICE.CITY, OFFICE.STATE)
                                 .from(OFFICE)
                                 .join(OFFICE_HAS_MANAGER)
@@ -82,12 +81,31 @@ public class ClassicModelsRepository {
         System.out.println("Many-to-many:\n" + result1);
         System.out.println("Many-to-many (JSON): " + result1.formatJSON());
         System.out.println("Many-to-many (XML): " + result1.formatXML());
+        
+        //  jOOQ’s type safe implicit to-one join feature
+        var result2 = ctx.select(
+                MANAGER.MANAGER_ID, MANAGER.MANAGER_NAME,
+                multiset(
+                        select(
+                                OFFICE_HAS_MANAGER.office().OFFICE_CODE, 
+                                OFFICE_HAS_MANAGER.office().CITY, 
+                                OFFICE_HAS_MANAGER.office().STATE)
+                                .from(OFFICE_HAS_MANAGER)                                
+                                .where(MANAGER.MANAGER_ID.eq(OFFICE_HAS_MANAGER.MANAGERS_MANAGER_ID))
+                ).as("offices"))
+                .from(MANAGER)
+                .orderBy(MANAGER.MANAGER_ID)
+                .fetch();
+        
+        System.out.println("Many-to-many:\n" + result2);
+        System.out.println("Many-to-many (JSON): " + result2.formatJSON());
+        System.out.println("Many-to-many (XML): " + result2.formatXML());
 
         // Result<Record4<String, String, String, Result<Record2<Long, String>>>>
-        var result2 = ctx.select(
+        var result3 = ctx.select(
                 OFFICE.OFFICE_CODE, OFFICE.CITY, OFFICE.STATE,
                 multiset(
-                        selectDistinct(
+                        select(
                                 MANAGER.MANAGER_ID, MANAGER.MANAGER_NAME)
                                 .from(MANAGER)
                                 .join(OFFICE_HAS_MANAGER)
@@ -98,8 +116,26 @@ public class ClassicModelsRepository {
                 .orderBy(OFFICE.OFFICE_CODE)
                 .fetch();
 
-        System.out.println("Many-to-many:\n" + result2);
-        System.out.println("Many-to-many (JSON): " + result2.formatJSON());
-        System.out.println("Many-to-many (XML): " + result2.formatXML());
+        System.out.println("Many-to-many:\n" + result3);
+        System.out.println("Many-to-many (JSON): " + result3.formatJSON());
+        System.out.println("Many-to-many (XML): " + result3.formatXML());
+        
+        //  jOOQ’s type safe implicit to-one join feature
+        var result4 = ctx.select(
+                OFFICE.OFFICE_CODE, OFFICE.CITY, OFFICE.STATE,
+                multiset(
+                        select(
+                                OFFICE_HAS_MANAGER.manager().MANAGER_ID, 
+                                OFFICE_HAS_MANAGER.manager().MANAGER_NAME)
+                                .from(OFFICE_HAS_MANAGER)
+                                .where(OFFICE.OFFICE_CODE.eq(OFFICE_HAS_MANAGER.OFFICES_OFFICE_CODE))
+                ).as("managers"))
+                .from(OFFICE)
+                .orderBy(OFFICE.OFFICE_CODE)
+                .fetch();
+        
+        System.out.println("Many-to-many:\n" + result4);
+        System.out.println("Many-to-many (JSON): " + result4.formatJSON());
+        System.out.println("Many-to-many (XML): " + result4.formatXML());
     }
 }
