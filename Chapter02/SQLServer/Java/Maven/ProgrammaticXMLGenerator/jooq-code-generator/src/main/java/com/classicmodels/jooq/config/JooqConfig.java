0ee447ba999ebaa6465a98b519ab55545499a915
anghelleonard <leoprivacy@yahoo.com>
2022-02-17 18:35:34 +0200

    Customizing jOOQ level of involvement

---
+++ Chapter02/SQLServer/Java/Maven/ProgrammaticXMLGenerator/jooq-code-generator/src/main/java/com/classicmodels/jooq/config/JooqConfig.java
@@ -0,0 +1,64 @@
+package com.classicmodels.jooq.config;
+
+import org.jooq.codegen.GenerationTool;
+import org.jooq.meta.jaxb.Configuration;
+import org.jooq.meta.jaxb.Database;
+import org.jooq.meta.jaxb.Generate;
+import org.jooq.meta.jaxb.Generator;
+import org.jooq.meta.jaxb.Jdbc;
+import org.jooq.meta.jaxb.MatcherRule;
+import org.jooq.meta.jaxb.MatcherTransformType;
+import org.jooq.meta.jaxb.Matchers;
+import org.jooq.meta.jaxb.MatchersTableType;
+import org.jooq.meta.jaxb.Property;
+import org.jooq.meta.jaxb.Strategy;
+import org.jooq.meta.jaxb.Target;
+
+public class JooqConfig {
+
+    public static void main(String[] args) throws Exception {
+
+        Configuration configuration = new Configuration()
+                .withJdbc(new Jdbc()
+                        .withDriver("com.microsoft.sqlserver.jdbc.SQLServerDriver")
+                        .withUrl("jdbc:sqlserver://localhost:1433;databaseName=classicmodels")
+                        .withUser("sa")
+                        .withPassword("root"))
+                .withGenerator(new Generator()
+                        .withName("org.jooq.codegen.JavaGenerator")
+                        .withDatabase(new Database()
+                                .withName("org.jooq.meta.xml.XMLDatabase")       
+                                .withProperties(
+                                        new Property().withKey("dialect").withValue("SQLSERVER"),
+                                        new Property().withKey("xmlFile").withValue("./../../../../../db/migration/xml/mssql/sql.xml")
+                                )
+                                .withIncludes(".*")
+                                .withExcludes("flyway_schema_history | concatenate | .*_master"
+                                         + " | get_.* | refresh_top3_product | sale_price | split_part"
+                                         + " | product_of_product_line | top_three_sales_per_employee")                                
+                                .withSchemaVersionProvider("com.classicmodels.jooq.config.MySchemaVersionProvider")                                
+                        )
+                        .withGenerate(new Generate()
+                                .withDaos(true)
+                                .withValidationAnnotations(Boolean.TRUE)
+                                .withSpringAnnotations(Boolean.TRUE)
+                        )
+                        .withStrategy(new Strategy()
+                                .withMatchers(new Matchers()
+                                        .withTables(new MatchersTableType()
+                                                .withPojoClass(new MatcherRule()
+                                                        .withExpression("Jooq_$0")
+                                                        .withTransform(MatcherTransformType.PASCAL))
+                                                .withDaoClass(new MatcherRule()
+                                                        .withExpression("$0_Repository")
+                                                        .withTransform(MatcherTransformType.PASCAL))))
+                        )
+                        .withTarget(new Target()
+                                .withPackageName("jooq.generated")
+                                .withDirectory(System.getProperty("user.dir").endsWith("webapp")
+                                        ? "target/generated-sources"
+                                        : "webapp/target/generated-sources")));
+
+        GenerationTool.generate(configuration);
+    }
+}
