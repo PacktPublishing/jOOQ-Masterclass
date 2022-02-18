08ddea54af055d172adf97b162a0d07378dc122f
anghelleonard <leoprivacy@yahoo.com>
2022-02-18 16:36:51 +0200

    CRUD, transactions, locking

--- Chapter09/MySQL/Maven/CRUD/OriginalRecords/src/main/java/com/classicmodels/MainApplication.java
+++ Chapter09/MySQL/Maven/CRUD/OriginalRecords/src/main/java/com/classicmodels/MainApplication.java
@@ -2,8 +2,9 @@ package com.classicmodels;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.r2dbc.R2dbcAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {R2dbcAutoConfiguration.class})
 public class MainApplication {
 
     public static void main(String[] args) {
