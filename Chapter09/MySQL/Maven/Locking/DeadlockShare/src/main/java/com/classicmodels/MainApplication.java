245df79114837151458c85c4be8683f79e9dad44
anghelleonard <leoprivacy@yahoo.com>
2022-02-20 07:26:39 +0200

    CRUD, transactions, locking

--- Chapter09/MySQL/Maven/Locking/DeadlockShare/src/main/java/com/classicmodels/MainApplication.java
+++ Chapter09/MySQL/Maven/Locking/DeadlockShare/src/main/java/com/classicmodels/MainApplication.java
@@ -4,6 +4,7 @@ import com.classicmodels.service.ClassicModelsService;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.r2dbc.R2dbcAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 
 // This application produces an exception of type:
@@ -12,7 +13,7 @@ import org.springframework.context.annotation.Bean;
     
 // However, the database will retry until one of the transaction (A) succeeds
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {R2dbcAutoConfiguration.class})
 public class MainApplication {    
     
     private final ClassicModelsService classicModelsService;
