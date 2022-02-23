da4f7095ba5dc75bce428a7b31f692c36aa6a193
anghelleonard <leoprivacy@yahoo.com>
2022-02-23 12:21:50 +0200

    CRUD, transactions, locking

---
+++ Chapter09/MySQL/Gradle/CRUD/StoreRecords/src/main/java/com/classicmodels/repository/ClassicModelsRepository.java
@@ -0,0 +1,44 @@
+package com.classicmodels.repository;
+
+import static jooq.generated.tables.Payment.PAYMENT;
+import jooq.generated.tables.records.PaymentRecord;
+import org.jooq.DSLContext;
+import org.jooq.Result;
+import static org.jooq.impl.DSL.row;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+@Transactional(readOnly = true)
+public class ClassicModelsRepository {
+
+    private final DSLContext ctx;
+
+    public ClassicModelsRepository(DSLContext ctx) {
+        this.ctx = ctx;
+    }
+
+    public Result<PaymentRecord> fetchAllPayment103() {
+
+        return ctx.selectFrom(PAYMENT)
+                .where(PAYMENT.CUSTOMER_NUMBER.eq(103L))
+                .fetch();
+    }
+
+    public PaymentRecord fetchPayment(Long nr, String ch) {
+
+        return ctx.selectFrom(PAYMENT)
+                .where(row(PAYMENT.CUSTOMER_NUMBER, PAYMENT.CHECK_NUMBER)
+                        .eq(row(nr, ch)))
+                .fetchSingle();
+    }
+
+    @Transactional
+    public int storePayment(PaymentRecord pr) {
+     
+        ctx.attach(pr);
+        // or, pr.attach(ctx.configuration());
+
+        return pr.store();
+    }
+}
