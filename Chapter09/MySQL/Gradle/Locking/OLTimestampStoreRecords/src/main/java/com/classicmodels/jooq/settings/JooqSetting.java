742137788cd917f6cfee658a632230997a40e483
anghelleonard <leoprivacy@yahoo.com>
2022-02-23 13:56:41 +0200

    C:\SBPBP\GitHub\Up-and-Running-with-jOOQ\Chapter09\MySQL\Gradle\Locking\OLTimestampStoreRecords

---
+++ Chapter09/MySQL/Gradle/Locking/OLTimestampStoreRecords/src/main/java/com/classicmodels/jooq/settings/JooqSetting.java
@@ -0,0 +1,17 @@
+package com.classicmodels.jooq.settings;
+
+import org.jooq.conf.Settings;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JooqSetting {
+
+    @Bean
+    public Settings jooqSettings() {
+        return new Settings()       
+                .withUpdateRecordTimestamp(true) // this is default, so it can be omitted                
+                .withExecuteWithOptimisticLocking(true)
+                .withExecuteWithOptimisticLockingExcludeUnversioned(true);                
+    }
+}
