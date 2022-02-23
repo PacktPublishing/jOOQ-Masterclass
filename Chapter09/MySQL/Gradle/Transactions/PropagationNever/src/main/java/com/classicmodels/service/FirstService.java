3b3bb1cb3eab8f19bd506f0f36c6d717300bfde1
anghelleonard <leoprivacy@yahoo.com>
2022-02-23 15:22:05 +0200

    CRUD, transactions, locking

---
+++ Chapter09/MySQL/Gradle/Transactions/PropagationNever/src/main/java/com/classicmodels/service/FirstService.java
@@ -0,0 +1,36 @@
+package com.classicmodels.service;
+
+import static jooq.generated.tables.Sale.SALE;
+import org.jooq.DSLContext;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class FirstService {
+
+    private final DSLContext ctx;
+    private final SecondService secondService;
+
+    public FirstService(DSLContext ctx, SecondService secondService) {
+        this.ctx = ctx;
+        this.secondService = secondService;
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    public void insertFirstSale() {
+
+        ctx.insertInto(SALE)
+                .set(SALE.SALE_, 12222.22)
+                .set(SALE.FISCAL_YEAR, 2022)
+                .set(SALE.FISCAL_MONTH, 1)
+                .set(SALE.REVENUE_GROWTH, 0.0)
+                .execute();
+
+        try {
+            secondService.insertSecondSale();
+        } catch (RuntimeException e) {
+            System.err.println("Exception: " + e);
+        }
+    }
+}
