caee20d99f8d17faba237027e31ac79241b116c6
anghelleonard <leoprivacy@yahoo.com>
2022-02-24 10:13:09 +0200

    Exporting, batching, bulking, loading

--- Chapter10/MySQL/Maven/Batching/BatchInserts/src/main/java/com/classicmodels/repository/ClassicModelsRepository.java
+++ Chapter10/MySQL/Maven/Batching/BatchInserts/src/main/java/com/classicmodels/repository/ClassicModelsRepository.java
@@ -30,32 +30,32 @@ public class ClassicModelsRepository {
 
         // batch inserts in a table having auto-generated primary key (several queries)
         int[] result1 = ctx.batch(
-                ctx.insertInto(SALE, SALE.FISCAL_YEAR, SALE.EMPLOYEE_NUMBER, SALE.SALE_)
-                        .values(2005, 1370L, 1282.64),
-                ctx.insertInto(SALE, SALE.FISCAL_YEAR, SALE.EMPLOYEE_NUMBER, SALE.SALE_)
-                        .values(2004, 1370L, 3938.24),
-                ctx.insertInto(SALE, SALE.FISCAL_YEAR, SALE.EMPLOYEE_NUMBER, SALE.SALE_)
-                        .values(2004, 1370L, 4676.14),
-                ctx.insertInto(SALE, SALE.FISCAL_YEAR, SALE.EMPLOYEE_NUMBER, SALE.SALE_)
-                        .values(2003, 1166L, 2223.0),
-                ctx.insertInto(SALE, SALE.FISCAL_YEAR, SALE.EMPLOYEE_NUMBER, SALE.SALE_)
-                        .values(2004, 1166L, 4531.35),
-                ctx.insertInto(SALE, SALE.FISCAL_YEAR, SALE.EMPLOYEE_NUMBER, SALE.SALE_)
-                        .values(2004, 1166L, 6751.33)
+                ctx.insertInto(SALE, SALE.FISCAL_YEAR, SALE.EMPLOYEE_NUMBER, SALE.SALE_, SALE.FISCAL_MONTH, SALE.REVENUE_GROWTH)
+                        .values(2005, 1370L, 1282.64, 1, 0.0),
+                ctx.insertInto(SALE, SALE.FISCAL_YEAR, SALE.EMPLOYEE_NUMBER, SALE.SALE_, SALE.FISCAL_MONTH, SALE.REVENUE_GROWTH)
+                        .values(2004, 1370L, 3938.24, 1, 0.0),
+                ctx.insertInto(SALE, SALE.FISCAL_YEAR, SALE.EMPLOYEE_NUMBER, SALE.SALE_, SALE.FISCAL_MONTH, SALE.REVENUE_GROWTH)
+                        .values(2004, 1370L, 4676.14, 1, 0.0),
+                ctx.insertInto(SALE, SALE.FISCAL_YEAR, SALE.EMPLOYEE_NUMBER, SALE.SALE_, SALE.FISCAL_MONTH, SALE.REVENUE_GROWTH)
+                        .values(2003, 1166L, 2223.0, 1, 0.0),
+                ctx.insertInto(SALE, SALE.FISCAL_YEAR, SALE.EMPLOYEE_NUMBER, SALE.SALE_, SALE.FISCAL_MONTH, SALE.REVENUE_GROWTH)
+                        .values(2004, 1166L, 4531.35, 1, 0.0),
+                ctx.insertInto(SALE, SALE.FISCAL_YEAR, SALE.EMPLOYEE_NUMBER, SALE.SALE_, SALE.FISCAL_MONTH, SALE.REVENUE_GROWTH)
+                        .values(2004, 1166L, 6751.33, 1, 0.0)
         ).execute();
 
         System.out.println("EXAMPLE 1.1: " + Arrays.toString(result1));
 
         // batch inserts (single query) PreparedStatement
         int[] result21 = ctx.batch(
-                ctx.insertInto(SALE, SALE.FISCAL_YEAR, SALE.EMPLOYEE_NUMBER, SALE.SALE_)
-                        .values((Integer) null, null, null))
-                .bind(2005, 1370L, 1282.64)
-                .bind(2004, 1370L, 3938.24)
-                .bind(2004, 1370L, 4676.14)
-                .bind(2003, 1166L, 2223.0)
-                .bind(2004, 1166L, 4531.35)
-                .bind(2004, 1166L, 6751.33)
+                ctx.insertInto(SALE, SALE.FISCAL_YEAR, SALE.EMPLOYEE_NUMBER, SALE.SALE_, SALE.FISCAL_MONTH, SALE.REVENUE_GROWTH)
+                        .values((Integer) null, null, null, null, null))
+                .bind(2005, 1370L, 1282.64, 1, 0.0)
+                .bind(2004, 1370L, 3938.24, 1, 0.0)
+                .bind(2004, 1370L, 4676.14, 1, 0.0)
+                .bind(2003, 1166L, 2223.0, 1, 0.0)
+                .bind(2004, 1166L, 4531.35, 1, 0.0)
+                .bind(2004, 1166L, 6751.33, 1, 0.0)
                 .execute();
 
         System.out.println("EXAMPLE 1.2.1: " + Arrays.toString(result21));
@@ -64,14 +64,14 @@ public class ClassicModelsRepository {
         int[] result22 = ctx.configuration().derive(
                 new Settings().withStatementType(StatementType.STATIC_STATEMENT))
                 .dsl().batch(
-                        ctx.insertInto(SALE, SALE.FISCAL_YEAR, SALE.EMPLOYEE_NUMBER, SALE.SALE_)
-                                .values((Integer) null, null, null))
-                .bind(2005, 1370L, 1282.64)
-                .bind(2004, 1370L, 3938.24)
-                .bind(2004, 1370L, 4676.14)
-                .bind(2003, 1166L, 2223.0)
-                .bind(2004, 1166L, 4531.35)
-                .bind(2004, 1166L, 6751.33)
+                        ctx.insertInto(SALE, SALE.FISCAL_YEAR, SALE.EMPLOYEE_NUMBER, SALE.SALE_, SALE.FISCAL_MONTH, SALE.REVENUE_GROWTH)
+                                .values((Integer) null, null, null, null, null))
+                .bind(2005, 1370L, 1282.64, 1, 0.0)
+                .bind(2004, 1370L, 3938.24, 1, 0.0)
+                .bind(2004, 1370L, 4676.14, 1, 0.0)
+                .bind(2003, 1166L, 2223.0, 1, 0.0)
+                .bind(2004, 1166L, 4531.35, 1, 0.0)
+                .bind(2004, 1166L, 6751.33, 1, 0.0)
                 .execute();
 
         System.out.println("EXAMPLE 1.2.2: " + Arrays.toString(result22));
@@ -100,11 +100,11 @@ public class ClassicModelsRepository {
     public void batchInsertRecords1() {
 
         // Records batch inserts (single query, bind values)
-        SaleRecord sr11 = new SaleRecord(null, 2005, 1223.23, 1370L, null, null, null, null);
-        SaleRecord sr12 = new SaleRecord(null, 2004, 543.33, 1166L, null, null, null, null);
-        SaleRecord sr13 = new SaleRecord(null, 2005, 9022.21, 1370L, null, null, null, null);
-        SaleRecord sr14 = new SaleRecord(null, 2003, 4333.22, 1504L, null, null, null, null);
-        SaleRecord sr15 = new SaleRecord(null, 2003, 8002.22, 1504L, null, null, null, null);
+        SaleRecord sr11 = new SaleRecord(null, 2005, 1223.23, 1370L, null, null, null, 1, 0.0, null);
+        SaleRecord sr12 = new SaleRecord(null, 2004, 543.33, 1166L, null, null, null, 1, 0.0, null);
+        SaleRecord sr13 = new SaleRecord(null, 2005, 9022.21, 1370L, null, null, null, 1, 0.0, null);
+        SaleRecord sr14 = new SaleRecord(null, 2003, 4333.22, 1504L, null, null, null, 1, 0.0, null);
+        SaleRecord sr15 = new SaleRecord(null, 2003, 8002.22, 1504L, null, null, null, 1, 0.0, null);
 
         List<SaleRecord> sales1 = List.of(sr15, sr12, sr13, sr14, sr11);
 
@@ -118,11 +118,11 @@ public class ClassicModelsRepository {
 
         // Records batch inserts (multiple query, inlined values)
         // Order of records is always preserved entirely
-        SaleRecord sr21 = new SaleRecord(null, 2005, 1223.23, 1370L, null, null, null, null);
-        SaleRecord sr22 = new SaleRecord(null, 2004, 543.33, 1166L, null, null, null, null);
-        SaleRecord sr23 = new SaleRecord(null, 2005, 9022.21, 1370L, null, null, null, null);
-        SaleRecord sr24 = new SaleRecord(null, 2003, 4333.22, 1504L, null, null, null, null);
-        SaleRecord sr25 = new SaleRecord(null, 2003, 8002.22, 1504L, null, null, null, null);
+        SaleRecord sr21 = new SaleRecord(null, 2005, 1223.23, 1370L, null, null, null, 1, 0.0, null);
+        SaleRecord sr22 = new SaleRecord(null, 2004, 543.33, 1166L, null, null, null, 1, 0.0, null);
+        SaleRecord sr23 = new SaleRecord(null, 2005, 9022.21, 1370L, null, null, null, 1, 0.0, null);
+        SaleRecord sr24 = new SaleRecord(null, 2003, 4333.22, 1504L, null, null, null, 1, 0.0, null);
+        SaleRecord sr25 = new SaleRecord(null, 2003, 8002.22, 1504L, null, null, null, 1, 0.0, null);
 
         List<SaleRecord> sales2 = List.of(sr25, sr22, sr23, sr24, sr21);
 
@@ -137,15 +137,15 @@ public class ClassicModelsRepository {
 
     public void batchInsertRecords2() {
 
-        SaleRecord sr1 = new SaleRecord(null, 2005, 1223.23, 1370L, null, null, null, null);
-        SaleRecord sr2 = new SaleRecord(null, 2004, 543.33, 1166L, null, null, null, null);
-        SaleRecord sr3 = new SaleRecord(null, 2005, 9022.21, null, null, null, null, null);
+        SaleRecord sr1 = new SaleRecord(null, 2005, 1223.23, 1370L, null, null, null, 1, 0.0, null);
+        SaleRecord sr2 = new SaleRecord(null, 2004, 543.33, 1166L, null, null, null, 1, 0.0, null);
+        SaleRecord sr3 = new SaleRecord(null, 2005, 9022.21, null, null, null, null, 1, 0.0, null);
         BankTransactionRecord bt1 = new BankTransactionRecord(
-                null, "", "", BigDecimal.ZERO, LocalDateTime.now(), 114L, "NP603840", "");
-        SaleRecord sr4 = new SaleRecord(null, 2003, 4333.22, 1504L, null, null, null, null);
-        SaleRecord sr5 = new SaleRecord(null, 2003, 8002.22, 1504L, null, null, null, "UP");
+                null, "", "", BigDecimal.ZERO, LocalDateTime.now(), 114L, "NP603840", "VisaElectron", "");
+        SaleRecord sr4 = new SaleRecord(null, 2003, 4333.22, 1504L, null, null, null, 1, 0.0, null);
+        SaleRecord sr5 = new SaleRecord(null, 2003, 8002.22, 1504L, null, null, null, 1, 0.0, "UP");
         BankTransactionRecord bt2 = new BankTransactionRecord(
-                null, "", "", BigDecimal.ZERO, LocalDateTime.now(), 114L, "NP603840", "");
+                null, "", "", BigDecimal.ZERO, LocalDateTime.now(), 114L, "NP603840", "VisaElectron", "");
 
         // There are two batches, one for SaleRecord and one for BankTransactionRecord.
         // The order of records is not preserved (check the log).
@@ -161,16 +161,22 @@ public class ClassicModelsRepository {
         sr1.setFiscalYear(2005);
         sr1.setSale(1223.23);
         sr1.setEmployeeNumber(1370L);
+        sr1.setFiscalMonth(1);
+        sr1.setRevenueGrowth(0.0);
         sr1.setTrend("UP");
 
         SaleRecord sr2 = new SaleRecord();
         sr2.setFiscalYear(2005);
         sr2.setSale(9022.21);
+        sr2.setFiscalMonth(1);
+        sr2.setRevenueGrowth(0.0);
 
         SaleRecord sr3 = new SaleRecord();
         sr3.setFiscalYear(2003);
         sr3.setSale(8002.22);
         sr3.setEmployeeNumber(1504L);
+        sr3.setFiscalMonth(1);
+        sr3.setRevenueGrowth(0.0);
 
         // There are three batches, one for each SaleRecord because the generated SQL with bind variables is not the same.
         // The order of records is preserved.
@@ -184,38 +190,42 @@ public class ClassicModelsRepository {
     public void batchInsertCollectionOfObjects() {
 
         List<SimpleSale> sales = List.of(
-                new SimpleSale(2005, 1370L, 1282.64),
-                new SimpleSale(2004, 1370L, 3938.24),
-                new SimpleSale(2004, 1370L, 4676.14)
+                new SimpleSale(2005, 1370L, 1282.64, 1, 0.0),
+                new SimpleSale(2004, 1370L, 3938.24, 1, 0.0),
+                new SimpleSale(2004, 1370L, 4676.14, 1, 0.0)
         );
 
         BatchBindStep batch = ctx.batch(
-                ctx.insertInto(SALE, SALE.FISCAL_YEAR, SALE.EMPLOYEE_NUMBER, SALE.SALE_)
-                        .values((Integer) null, null, null)
+                ctx.insertInto(SALE, SALE.FISCAL_YEAR, SALE.EMPLOYEE_NUMBER, SALE.SALE_, SALE.FISCAL_MONTH, SALE.REVENUE_GROWTH)
+                        .values((Integer) null, null, null, null, null)
         );
 
-        sales.forEach(s -> batch.bind(s.getFiscalYear(), s.getEmployeeNumber(), s.getSale()));
+        sales.forEach(s -> batch.bind(s.getFiscalYear(), s.getEmployeeNumber(), s.getSale(), s.getFiscalMonth(), s.getRevenueGrowth()));
         batch.execute();
     }
 
     public void forceNumberOfBatches() {
 
-        // by default the generated INSERT has 3 placeholders
-        // insert into `classicmodels`.`sale` (`fiscal_year`, `sale`, `trend`) values (?, ?, ?)
+        // by default the generated INSERT has 5 placeholders
+        // insert into `classicmodels`.`sale` (`fiscal_year`, `sale`, `trend`, `fiscal_month`, `revenue_growth`) values (?, ?, ?, ?, ?)
         SaleRecord sr1 = new SaleRecord();
         sr1.setSaleId(pk());
         sr1.setFiscalYear(2003);
         sr1.setTrend("UP");
         sr1.setSale(34493.22);
+        sr1.setFiscalMonth(1);
+        sr1.setRevenueGrowth(0.0);
 
-        // by default the generated INSERT has 4 placeholders
-        // insert into `classicmodels`.`sale` (`fiscal_year`, `sale`, `employee_number`, `hot`) values (?, ?, ?, ?)
+        // by default the generated INSERT has 6 placeholders
+        // insert into `classicmodels`.`sale` (`fiscal_year`, `sale`, `employee_number`, `hot`, `fiscal_month`, `revenue_growth`) values (?, ?, ?, ?, ?, ?)
         SaleRecord sr2 = new SaleRecord();
         sr2.setSaleId(pk());
         sr2.setEmployeeNumber(1370L);
         sr2.setSale(4522.34);
         sr2.setFiscalYear(2005);
         sr2.setHot((byte) 1);
+        sr2.setFiscalMonth(1);
+        sr2.setRevenueGrowth(0.0);
 
         // in this context, there will be 2 batches, but we can force a single batch if
         // we force a insert having the same string, and for this we can
@@ -225,7 +235,7 @@ public class ClassicModelsRepository {
         sr2.changed(true);
 
         // a single batch is executed having this INSERT
-        // insert into `classicmodels`.`sale` (`sale_id`, `fiscal_year`, `sale`, `employee_number`, `hot`, `rate`, `vat`, `trend`) values (?, ?, ?, ?, ?, ?, ?, ?)
+        // insert into `classicmodels`.`sale` (`sale_id`, `fiscal_year`, `sale`, `employee_number`, `hot`, `rate`, `vat`, `fiscal_month`, `revenue_growth`, `trend`) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
         ctx.batchInsert(sr1, sr2).execute();
     }
     
