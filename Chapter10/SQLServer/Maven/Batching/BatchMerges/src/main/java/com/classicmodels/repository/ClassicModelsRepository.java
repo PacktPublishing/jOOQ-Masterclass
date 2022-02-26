64a26e3ff6ade72c1ee4718f4bf80cc7d3e1b545
anghelleonard <leoprivacy@yahoo.com>
2022-02-26 18:14:17 +0200

    Exporting, batching, bulking, loading

--- Chapter10/SQLServer/Maven/Batching/BatchMerges/src/main/java/com/classicmodels/repository/ClassicModelsRepository.java
+++ Chapter10/SQLServer/Maven/Batching/BatchMerges/src/main/java/com/classicmodels/repository/ClassicModelsRepository.java
@@ -21,11 +21,11 @@ public class ClassicModelsRepository {
 
     public void batchMerges1() {
 
-        SaleRecord sr1 = new SaleRecord(1L, 2005, 1223.23, 1370L, null, null, null, null);
-        SaleRecord sr2 = new SaleRecord(2L, 2004, 543.33, null, null, null, null, null);
-        SaleRecord sr3 = new SaleRecord(3L, 2005, 9022.21, 1370L, null, null, null, null);
-        SaleRecord sr4 = new SaleRecord(10000L, 2003, 4333.22, 1504L, null, null, null, "UP");
-        SaleRecord sr5 = new SaleRecord(99999L, 2003, 8002.22, 1504L, null, null, null, null);
+        SaleRecord sr1 = new SaleRecord(1L, 2005, 1223.23, 1370L, null, null, null, 1, 0.0, null);
+        SaleRecord sr2 = new SaleRecord(2L, 2004, 543.33, null, null, null, null, 1, 0.0, null);
+        SaleRecord sr3 = new SaleRecord(3L, 2005, 9022.21, 1370L, null, null, null, 1, 0.0, null);
+        SaleRecord sr4 = new SaleRecord(10000L, 2003, 4333.22, 1504L, null, null, null, 1, 0.0, "UP");
+        SaleRecord sr5 = new SaleRecord(99999L, 2003, 8002.22, 1504L, null, null, null, 1, 0.0, null);
 
         // SQL Server doesn't allow us to update ids, so we mark them unchanged 
         sr1.changed(0, false);
@@ -54,15 +54,15 @@ public class ClassicModelsRepository {
 
     public void batchMerges2() {
 
-        SaleRecord sr1 = new SaleRecord(1L, 2005, 1223.23, 1370L, null, null, null, null);
-        SaleRecord sr2 = new SaleRecord(2L, 2004, 543.33, null, null, null, null, null);
-        SaleRecord sr3 = new SaleRecord(3L, 2005, 9022.21, 1370L, null, null, null, null);
+        SaleRecord sr1 = new SaleRecord(1L, 2005, 1223.23, 1370L, null, null, null, 1, 0.0, null);
+        SaleRecord sr2 = new SaleRecord(2L, 2004, 543.33, null, null, null, null, 1, 0.0, null);
+        SaleRecord sr3 = new SaleRecord(3L, 2005, 9022.21, 1370L, null, null, null, 1, 0.0, null);
         BankTransactionRecord bt1 = new BankTransactionRecord(
-                pk(), "", "", BigDecimal.ONE, LocalDateTime.now(), 112L, "BO864823", "");
-        SaleRecord sr4 = new SaleRecord(1000L, 2003, 4333.22, 1504L, null, null, null, "UP");
-        SaleRecord sr5 = new SaleRecord(9999L, 2003, 8002.22, 1504L, null, null, null, null);
+                pk(), "", "", BigDecimal.ONE, LocalDateTime.now(), 112L, "BO864823", "", "");
+        SaleRecord sr4 = new SaleRecord(1000L, 2003, 4333.22, 1504L, null, null, null, 1, 0.0, "UP");
+        SaleRecord sr5 = new SaleRecord(9999L, 2003, 8002.22, 1504L, null, null, null, 1, 0.0, null);
         BankTransactionRecord bt2 = new BankTransactionRecord(
-                pk(), "", "", BigDecimal.ONE, LocalDateTime.now(), 112L, "HQ55022", "");
+                pk(), "", "", BigDecimal.ONE, LocalDateTime.now(), 112L, "HQ55022", "", "");
 
         // SQL Server doesn't allow us to update ids, so we mark them unchanged 
         sr1.changed(0, false);
@@ -90,15 +90,21 @@ public class ClassicModelsRepository {
         sr1.setSale(1223.23);
         sr1.setEmployeeNumber(1370L);
         sr1.setTrend("UP");
+        sr1.setFiscalMonth(1);
+        sr1.setRevenueGrowth(0.0);
 
         SaleRecord sr2 = new SaleRecord();
         sr2.setFiscalYear(2005);
         sr2.setSale(9022.21);
+        sr2.setFiscalMonth(1);
+        sr2.setRevenueGrowth(0.0);
 
         SaleRecord sr3 = new SaleRecord();
         sr3.setFiscalYear(2003);
         sr3.setSale(8002.22);
         sr3.setEmployeeNumber(1504L);
+        sr3.setFiscalMonth(1);
+        sr3.setRevenueGrowth(0.0);
 
         // There will 3 batches, 1 batch for each SaleRecord since  
         // each SaleRecord has a different generated SQL with bind variables
