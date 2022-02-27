ba30b1be90f3a879412c48c591e335e2a0c823a4
anghelleonard <leoprivacy@yahoo.com>
2022-02-27 12:11:57 +0200

    Exporting, batching, bulking, loading

--- Chapter10/Oracle/Maven/Batching/CombineBatchStatements/src/main/java/com/classicmodels/repository/ClassicModelsRepository.java
+++ Chapter10/Oracle/Maven/Batching/CombineBatchStatements/src/main/java/com/classicmodels/repository/ClassicModelsRepository.java
@@ -23,18 +23,18 @@ public class ClassicModelsRepository {
 
         // combine batch
         int[] result = ctx.batch(
-                        ctx.insertInto(SALE, SALE.FISCAL_YEAR, SALE.EMPLOYEE_NUMBER, SALE.SALE_).values(BigInteger.valueOf(2005), 1370L, 1282.64),
-                        ctx.insertInto(SALE, SALE.FISCAL_YEAR, SALE.EMPLOYEE_NUMBER, SALE.SALE_).values(BigInteger.valueOf(2004), 1370L, 3938.24),
-                        ctx.insertInto(SALE, SALE.FISCAL_YEAR, SALE.EMPLOYEE_NUMBER, SALE.SALE_).values(BigInteger.valueOf(2004), 1370L, 4676.14),
+                        ctx.insertInto(SALE, SALE.FISCAL_YEAR, SALE.EMPLOYEE_NUMBER, SALE.SALE_, SALE.FISCAL_MONTH, SALE.REVENUE_GROWTH).values(2005, 1370L, 1282.64, 1, 0.0),
+                        ctx.insertInto(SALE, SALE.FISCAL_YEAR, SALE.EMPLOYEE_NUMBER, SALE.SALE_, SALE.FISCAL_MONTH, SALE.REVENUE_GROWTH).values(2004, 1370L, 3938.24, 1, 0.0),
+                        ctx.insertInto(SALE, SALE.FISCAL_YEAR, SALE.EMPLOYEE_NUMBER, SALE.SALE_, SALE.FISCAL_MONTH, SALE.REVENUE_GROWTH).values(2004, 1370L, 4676.14, 1, 0.0),
                         ctx.update(EMPLOYEE)
                                 .set(EMPLOYEE.SALARY, EMPLOYEE.SALARY.plus(1_000))
-                                .where(EMPLOYEE.SALARY.between(BigInteger.valueOf(100_000), BigInteger.valueOf(120_000))),
+                                .where(EMPLOYEE.SALARY.between(100_000, 120_000)),
                         ctx.update(EMPLOYEE)
                                 .set(EMPLOYEE.SALARY, EMPLOYEE.SALARY.plus(5_000))
-                                .where(EMPLOYEE.SALARY.between(BigInteger.valueOf(65_000), BigInteger.valueOf(80_000))),
+                                .where(EMPLOYEE.SALARY.between(65_000, 80_000)),
                         ctx.update(EMPLOYEE)
                                 .set(EMPLOYEE.SALARY, EMPLOYEE.SALARY.plus(10_000))
-                                .where(EMPLOYEE.SALARY.between(BigInteger.valueOf(55_000), BigInteger.valueOf(60_000))),
+                                .where(EMPLOYEE.SALARY.between(55_000, 60_000)),
                         ctx.deleteFrom(BANK_TRANSACTION).where(BANK_TRANSACTION.TRANSACTION_ID.eq(1L)),
                         ctx.deleteFrom(BANK_TRANSACTION).where(BANK_TRANSACTION.TRANSACTION_ID.eq(2L)),
                         ctx.deleteFrom(BANK_TRANSACTION).where(BANK_TRANSACTION.TRANSACTION_ID.eq(3L)),
