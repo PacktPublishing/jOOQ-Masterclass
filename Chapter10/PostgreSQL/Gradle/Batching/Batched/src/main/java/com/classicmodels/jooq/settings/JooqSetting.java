d60367faf6b141201a1617f819c3b6897a1be17e
anghelleonard <leoprivacy@yahoo.com>
2022-02-28 13:42:28 +0200

    Exporting, batching, bulking, loading

---
+++ Chapter10/PostgreSQL/Gradle/Batching/Batched/src/main/java/com/classicmodels/jooq/settings/JooqSetting.java
@@ -0,0 +1,15 @@
+package com.classicmodels.jooq.settings;
+
+import org.jooq.conf.Settings;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JooqSetting {
+
+    @Bean
+    public Settings jooqSettings() {
+        return new Settings()
+                .withBatchSize(10);
+    }
+}
