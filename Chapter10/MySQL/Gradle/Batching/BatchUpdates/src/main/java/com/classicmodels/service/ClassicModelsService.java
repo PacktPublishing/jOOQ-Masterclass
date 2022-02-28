ccc08cd2018c86e3989f8ee8d210e2cf95c73e31
anghelleonard <leoprivacy@yahoo.com>
2022-02-28 11:41:14 +0200

    Exporting, batching, bulking, loading

---
+++ Chapter10/MySQL/Gradle/Batching/BatchUpdates/src/main/java/com/classicmodels/service/ClassicModelsService.java
@@ -0,0 +1,23 @@
+package com.classicmodels.service;
+
+import com.classicmodels.repository.ClassicModelsRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClassicModelsService {
+
+    private final ClassicModelsRepository classicModelsRepository;
+
+    public ClassicModelsService(ClassicModelsRepository classicModelsRepository) {
+        this.classicModelsRepository = classicModelsRepository;
+    }
+
+    public void callAll() {
+
+        classicModelsRepository.batchUpdateStatements();       
+        classicModelsRepository.batchUpdateRecords1();
+        classicModelsRepository.batchUpdateRecords2();
+        classicModelsRepository.batchUpdateRecords3();
+        classicModelsRepository.batchUpdateCollectionOfObjects();
+    }
+}
