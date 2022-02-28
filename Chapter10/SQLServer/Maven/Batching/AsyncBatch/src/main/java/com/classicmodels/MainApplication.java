db478b24918f72ad31941d8fc303916bdc0f9251
anghelleonard <leoprivacy@yahoo.com>
2022-02-28 07:44:08 +0200

    Exporting, batching, bulking, loading

---
+++ Chapter10/SQLServer/Maven/Batching/AsyncBatch/src/main/java/com/classicmodels/MainApplication.java
@@ -0,0 +1,33 @@
+package com.classicmodels;
+
+import com.classicmodels.service.ClassicModelsService;
+import java.util.Arrays;
+import java.util.List;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.r2dbc.R2dbcAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication(exclude = {R2dbcAutoConfiguration.class})
+public class MainApplication {
+
+    private final ClassicModelsService classicModelsService;
+
+    public MainApplication(ClassicModelsService classicModelsService) {
+        this.classicModelsService = classicModelsService;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+
+    @Bean
+    public ApplicationRunner init() {
+        return args -> {
+
+           List<int[]> result = classicModelsService.batchAsync();
+           result.forEach(r -> System.out.println(Arrays.toString(r)));
+        };
+    }
+}
