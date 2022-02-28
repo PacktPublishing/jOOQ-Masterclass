6095ec753c9c4d51cb50592e4e309e66da310231
anghelleonard <leoprivacy@yahoo.com>
2022-02-28 17:19:37 +0200

    Exporting, batching, bulking, loading

---
+++ Chapter10/Oracle/Gradle/Batching/AsyncBatch/src/main/java/com/classicmodels/repository/ClassicModelsRepository.java
@@ -0,0 +1,50 @@
+package com.classicmodels.repository;
+
+import java.util.concurrent.CompletableFuture;
+import static jooq.generated.tables.Employee.EMPLOYEE;
+import static jooq.generated.tables.Sale.SALE;
+import org.jooq.DSLContext;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ClassicModelsRepository {
+
+    private final DSLContext ctx;
+
+    public ClassicModelsRepository(DSLContext ctx) {
+        this.ctx = ctx;
+    }
+
+    @Async
+    public CompletableFuture<int[]> batchInsertSalesAsync() {
+
+        return ctx.batch(
+                ctx.insertInto(SALE, SALE.FISCAL_YEAR, SALE.EMPLOYEE_NUMBER, SALE.SALE_, SALE.FISCAL_MONTH, SALE.REVENUE_GROWTH).values(2005, 1370L, 1282.64, 1, 0.0),
+                ctx.insertInto(SALE, SALE.FISCAL_YEAR, SALE.EMPLOYEE_NUMBER, SALE.SALE_, SALE.FISCAL_MONTH, SALE.REVENUE_GROWTH).values(2004, 1370L, 3938.24, 1, 0.0),
+                ctx.insertInto(SALE, SALE.FISCAL_YEAR, SALE.EMPLOYEE_NUMBER, SALE.SALE_, SALE.FISCAL_MONTH, SALE.REVENUE_GROWTH).values(2004, 1370L, 4676.14, 1, 0.0),
+                ctx.insertInto(SALE, SALE.FISCAL_YEAR, SALE.EMPLOYEE_NUMBER, SALE.SALE_, SALE.FISCAL_MONTH, SALE.REVENUE_GROWTH).values(2003, 1166L, 2223.0, 1, 0.0),
+                ctx.insertInto(SALE, SALE.FISCAL_YEAR, SALE.EMPLOYEE_NUMBER, SALE.SALE_, SALE.FISCAL_MONTH, SALE.REVENUE_GROWTH).values(2004, 1166L, 4531.35, 1, 0.0),
+                ctx.insertInto(SALE, SALE.FISCAL_YEAR, SALE.EMPLOYEE_NUMBER, SALE.SALE_, SALE.FISCAL_MONTH, SALE.REVENUE_GROWTH).values(2004, 1166L, 6751.33, 1, 0.0))
+                .executeAsync().toCompletableFuture();
+    }
+
+    @Async
+    public CompletableFuture<int[]> batchUpdateEmployeeAsync() {
+
+        return ctx.batch(
+                ctx.update(EMPLOYEE)
+                        .set(EMPLOYEE.SALARY, EMPLOYEE.SALARY.plus(1_000))
+                        .where(EMPLOYEE.SALARY.between(100_000, 120_000)),
+                ctx.update(EMPLOYEE)
+                        .set(EMPLOYEE.SALARY, EMPLOYEE.SALARY.plus(5_000))
+                        .where(EMPLOYEE.SALARY.between(65_000, 80_000)),
+                ctx.update(EMPLOYEE)
+                        .set(EMPLOYEE.SALARY, EMPLOYEE.SALARY.plus(10_000))
+                        .where(EMPLOYEE.SALARY.between(55_000, 60_000)),
+                ctx.update(EMPLOYEE)
+                        .set(EMPLOYEE.SALARY, EMPLOYEE.SALARY.plus(15_000))
+                        .where(EMPLOYEE.SALARY.between(50_000, 50_000))) // or simply, eq(50_000)
+                .executeAsync().toCompletableFuture();
+    }
+}
