dc6883c816ed1632dcd4ac9f80dde3b773ca60f3
anghelleonard <leoprivacy@yahoo.com>
2022-03-02 14:23:21 +0200

    jOOQ Keys

--- Chapter11/PostgreSQL/Maven/Keys/src/main/java/com/classicmodels/repository/ClassicModelsRepository.java
+++ Chapter11/PostgreSQL/Maven/Keys/src/main/java/com/classicmodels/repository/ClassicModelsRepository.java
@@ -30,13 +30,13 @@ public class ClassicModelsRepository {
     }
 
     /* Insert and return primary key */
-    
     @Transactional
     public void insertIntoAndReturnPrimaryKey() {
 
         // Record1<Long>
-        var insertedId = ctx.insertInto(SALE, SALE.FISCAL_YEAR, SALE.SALE_, SALE.EMPLOYEE_NUMBER)
-                .values(2004, 2311.42, 1370L)
+        var insertedId = ctx.insertInto(SALE, SALE.FISCAL_YEAR, SALE.SALE_, SALE.EMPLOYEE_NUMBER,
+                SALE.FISCAL_MONTH, SALE.REVENUE_GROWTH)
+                .values(2004, 2311.42, 1370L, 1, 0.0)
                 .returningResult(SALE.getIdentity().getField())
                 // or, .returningResult(SALE.SALE_ID)
                 .fetchOne(); // get directly the long value, .fetchOne().value1();
@@ -44,57 +44,61 @@ public class ClassicModelsRepository {
         System.out.println("Inserted ID:\n" + insertedId);
 
         // Result<Record1<Long>>
-        var insertedIds = ctx.insertInto(SALE, SALE.FISCAL_YEAR, SALE.SALE_, SALE.EMPLOYEE_NUMBER)
-                .values(2004, 2311.42, 1370L)
-                .values(2003, 900.21, 1504L)
-                .values(2005, 1232.2, 1166L)
+        var insertedIds = ctx.insertInto(SALE, SALE.FISCAL_YEAR, SALE.SALE_, SALE.EMPLOYEE_NUMBER,
+                SALE.FISCAL_MONTH, SALE.REVENUE_GROWTH)
+                .values(2004, 2311.42, 1370L, 1, 0.0)
+                .values(2003, 900.21, 1504L, 1, 0.0)
+                .values(2005, 1232.2, 1166L, 1, 0.0)
                 .returningResult(SALE.getIdentity().getField())
                 // or, .returningResult(SALE.SALE_ID)
                 .fetch();
 
         System.out.println("Inserted IDs:\n" + insertedIds);
-        
+
         // use lastID()
-        ctx.insertInto(SALE, SALE.FISCAL_YEAR, SALE.SALE_, SALE.EMPLOYEE_NUMBER)
-                .values(2002, 9876.96, 1504L)
+        ctx.insertInto(SALE, SALE.FISCAL_YEAR, SALE.SALE_, SALE.EMPLOYEE_NUMBER,
+                SALE.FISCAL_MONTH, SALE.REVENUE_GROWTH)
+                .values(2002, 9876.96, 1504L, 1, 0.0)
                 .execute();
-        
+
         // PAY ATTENTION TO THE FACT THAT, MEANWHILE, A CONCURRENT TRANSACTION CAN MODIFY THE CURRENT VALUE
         // SO, THERE IS NO GUARANTEE THAT THE BELOW FETCHED *lastId* IS THE PRIMARY KEY OF THE PREVIOUS INSERT
-        
         // if you cannot provide an identity
         var lastId = ctx.lastID();
-        
+
         System.out.println("Last ID: " + lastId);
     }
-    
+
     /* Primary keys and updatable records */
-    
     @Transactional
     public void insertAndReturnPrimaryKey() {
-                     
+
         // insert record
         SaleRecord sr = ctx.newRecord(SALE);
 
         sr.setFiscalYear(2021);
         sr.setSale(4500.25);
         sr.setEmployeeNumber(1504L);
+        sr.setFiscalMonth(1);
+        sr.setRevenueGrowth(0.0);
 
         sr.insert();
 
         System.out.println("The inserted record ID: " + sr.getSaleId());
-        
+
         // insert POJO
         Sale s = new Sale();
         s.setFiscalYear(2020);
         s.setSale(643.23);
         s.setEmployeeNumber(1370L);
-        
+        s.setFiscalMonth(1);
+        s.setRevenueGrowth(0.0);
+
         saleRepository.insert(s);
-        
+
         System.out.println("The inserted POJO ID: " + s.getSaleId());
-    }        
-    
+    }
+
     @Transactional
     public void suppressPrimaryKeyReturnOnUpdatableRecord() {
 
@@ -107,10 +111,12 @@ public class ClassicModelsRepository {
         srNoReturnId.setFiscalYear(2021);
         srNoReturnId.setSale(4500.25);
         srNoReturnId.setEmployeeNumber(1504L);
+        srNoReturnId.setFiscalMonth(1);
+        srNoReturnId.setRevenueGrowth(0.0);
 
         srNoReturnId.insert();
 
-        System.out.println("The inserted record ID (should be null): " + srNoReturnId.getSaleId());        
+        System.out.println("The inserted record ID (should be null): " + srNoReturnId.getSaleId());
     }
 
     @Transactional
@@ -137,10 +143,9 @@ public class ClassicModelsRepository {
                 .set(SALE.SALE_ID, sr.getSaleId() + 1)
                 .where(SALE.SALE_ID.eq(sr.getSaleId()))
                 .execute();
-    }    
+    }
 
     /* Compare composed keys */
-    
     public void compareComposedPrimaryKey() {
 
         var result1 = ctx.selectFrom(PRODUCTLINE)
@@ -151,16 +156,16 @@ public class ClassicModelsRepository {
 
         var result2 = ctx.selectFrom(PRODUCTLINE)
                 .where(row(PRODUCTLINE.PRODUCT_LINE, PRODUCTLINE.CODE)
-                        .eq(row("Classic Cars", 599302L)))
+                        .eq(row("Classic Cars", 599302L))) // or, .eq("Classic Cars", 599302L))
                 .fetchSingle();
         System.out.println("Result 2:\n" + result2);
-        
+
         // using in() (similar, you can use notIn(), and so on)
         var result3 = ctx.selectFrom(PRODUCTLINE)
-                .where(row(PRODUCTLINE.PRODUCT_LINE, PRODUCTLINE.CODE)                        
+                .where(row(PRODUCTLINE.PRODUCT_LINE, PRODUCTLINE.CODE)
                         .in(row("Classic Cars", 599302L),
-                            row("Trains", 123333L),
-                            row("Motorcycles", 599302L)))
+                                row("Trains", 123333L),
+                                row("Motorcycles", 599302L)))
                 .fetch();
         System.out.println("Result 3:\n" + result3);
     }
@@ -185,7 +190,7 @@ public class ClassicModelsRepository {
 
         var next10Vals = ctx.fetch(EMPLOYEE_SEQ.nextvals(10));
         System.out.println("Next 10 vals:\n" + next10Vals);
-        
+
         List<Long> next10LongVals = ctx.fetch(EMPLOYEE_SEQ.nextvals(10)).into(Long.class);
         System.out.println("Next 10 vals:\n" + next10LongVals);
     }
@@ -195,19 +200,22 @@ public class ClassicModelsRepository {
 
         // Avoid: ERROR: CURRVAL of sequence "sale_seq" is not yet defined in this session
         // SALE_SEQ.nextval(); - you can call this, but an INSERT will also call NEXTVAL
-        ctx.insertInto(SALE, SALE.FISCAL_YEAR, SALE.SALE_, SALE.EMPLOYEE_NUMBER)
-                .values(2020, 900.25, 1611L)
+        ctx.insertInto(SALE, SALE.FISCAL_YEAR, SALE.SALE_, SALE.EMPLOYEE_NUMBER,
+                SALE.FISCAL_MONTH, SALE.REVENUE_GROWTH)
+                .values(2020, 900.25, 1611L, 1, 0.0)
                 .execute();
 
         // PAY ATTENTION TO THE FACT THAT, MEANWHILE, A CONCURRENT TRANSACTION CAN MODIFY THE CURRENT VALUE
         // SO, THERE IS NO GUARANTEE THAT THE BELOW FETCHED *cr* IS THE PRIMARY KEY OF THE PREVIOUS INSERT
         // IF YOU NEED THE PK OF THE ABOVE INSERT THEN RELY ON INSERT ... RETURNING
-        /* var pk = ctx.insertInto(SALE)
-                .values(default_(), 2020, 900.25, 1611L, 
-                        default_(), RateType.GOLD, VatType.MIN, default_())
+        /*
+         var pk = ctx.insertInto(SALE, SALE.FISCAL_YEAR, SALE.SALE_, SALE.EMPLOYEE_NUMBER,
+                SALE.FISCAL_MONTH, SALE.REVENUE_GROWTH)
+                .values(2020, 900.25, 1611L, 1, 0.0)
                 .returningResult(SALE.SALE_ID)
-                .execute(); 
-         */
+                .execute();
+        */
+
         var cr = ctx.select(SALE_SEQ.currval()).fetchSingle().value1();
         long crLong = ctx.select(SALE_SEQ.currval()).fetchSingleInto(Long.class); // or, fetchOneInto()
 
@@ -229,14 +237,14 @@ public class ClassicModelsRepository {
         ctx.deleteFrom(SALE)
                 .where(SALE.SALE_ID.eq(ctx.select(SALE_SEQ.currval()).fetchSingle().value1()))
                 .execute();
-        
+
         // this is not prone to the same issue because there will be a single UPDATE/DELETE, so
         // this time, you'll affect the latest current value, whichever that value is
         ctx.update(SALE)
                 .set(SALE.FISCAL_YEAR, 2005)
                 .where(SALE.SALE_ID.eq(SALE_SEQ.currval()))
                 .execute();
-        
+
         ctx.deleteFrom(SALE)
                 .where(SALE.SALE_ID.eq(SALE_SEQ.currval()))
                 .execute();
@@ -247,8 +255,9 @@ public class ClassicModelsRepository {
 
         // Avoid: ERROR: CURRVAL of sequence "sale_seq" is not yet defined in this session
         // SALE_SEQ.nextval(); - you can call this, but an INSERT will also call NEXTVAL
-        ctx.insertInto(SALE, SALE.FISCAL_YEAR, SALE.SALE_, SALE.EMPLOYEE_NUMBER)
-                .values(2030, 900.25, 1611L)
+        ctx.insertInto(SALE, SALE.FISCAL_YEAR, SALE.SALE_, SALE.EMPLOYEE_NUMBER, 
+                SALE.FISCAL_MONTH, SALE.REVENUE_GROWTH)
+                .values(2030, 900.25, 1611L, 1, 0.0)
                 .execute();
 
         // This updates the record having the current value, which can be
@@ -266,8 +275,9 @@ public class ClassicModelsRepository {
         // default (e.g., NOT NULL DEFAULT NEXTVAL ('sale_seq')) there is no need 
         // to call currval() or nextval(). Simply omit the PK and let the database 
         // to generate it.
-        ctx.insertInto(SALE, SALE.FISCAL_YEAR, SALE.EMPLOYEE_NUMBER, SALE.SALE_)
-                .values(2005, 1370L, 1282.64);
+        ctx.insertInto(SALE, SALE.FISCAL_YEAR, SALE.EMPLOYEE_NUMBER, SALE.SALE_,
+                SALE.FISCAL_MONTH, SALE.REVENUE_GROWTH)
+                .values(2005, 1370L, 1282.641, 1, 0.0);
 
         // But, for SEQUENCE owned by non-auto-generated rows, you have to rely on nextval()/nextvals()
         // For instance, you can INSERT 10 employees via *EMPLOYEE_SEQ.nextval()* 
@@ -286,9 +296,12 @@ public class ClassicModelsRepository {
         List<Long> idsLong = ctx.fetch(EMPLOYEE_SEQ.nextvals(10)).into(Long.class);
 
         // This is also useful for Records to pre-set IDs:
-        // EmployeeRecord er = new EmployeeRecord(ids.get(0).value1(), // or, idsLong.get(0) 
-        //        "Lionel", "Andre", "x8990", "landre@gmail.com", "1", 
-        //                57000, 1143L, "Sales Rep", null, null);        
+        /*
+         EmployeeRecord er = new EmployeeRecord(ids.get(0).value1(), // or, idsLong.get(0) 
+                "Lionel", "Andre", "x8990", "landre@gmail.com", "1", 
+                        57000, 0, 1143L, "Sales Rep", null, null);        
+        */
+        
         for (int i = 0; i < ids.size(); i++) {
 
             ctx.insertInto(EMPLOYEE, EMPLOYEE.EMPLOYEE_NUMBER, EMPLOYEE.LAST_NAME, EMPLOYEE.FIRST_NAME,
