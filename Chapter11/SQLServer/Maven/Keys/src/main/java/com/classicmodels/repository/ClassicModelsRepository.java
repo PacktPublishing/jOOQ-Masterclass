d2c40f7f05adc950f7e592457300816e33023010
anghelleonard <leoprivacy@yahoo.com>
2022-03-02 18:14:16 +0200

    jOOQ Keys

--- Chapter11/SQLServer/Maven/Keys/src/main/java/com/classicmodels/repository/ClassicModelsRepository.java
+++ Chapter11/SQLServer/Maven/Keys/src/main/java/com/classicmodels/repository/ClassicModelsRepository.java
@@ -2,7 +2,7 @@ package com.classicmodels.repository;
 
 import java.util.List;
 import static jooq.generated.Sequences.EMPLOYEE_SEQ;
-import static jooq.generated.Sequences.SALE_SEQ;
+import static jooq.generated.Sequences.PRODUCT_UID_SEQ;
 import static jooq.generated.tables.Employee.EMPLOYEE;
 import static jooq.generated.tables.Product.PRODUCT;
 import static jooq.generated.tables.Productline.PRODUCTLINE;
@@ -37,8 +37,9 @@ public class ClassicModelsRepository {
     public void insertIntoAndReturnPrimaryKey() {
 
         // Record1<Long>
-        var insertedId = ctx.insertInto(SALE, SALE.FISCAL_YEAR, SALE.SALE_, SALE.EMPLOYEE_NUMBER)
-                .values(2004, 2311.42, 1370L)
+        var insertedId = ctx.insertInto(SALE, SALE.FISCAL_YEAR, SALE.SALE_, SALE.EMPLOYEE_NUMBER,
+                SALE.FISCAL_MONTH, SALE.REVENUE_GROWTH)
+                .values(2004, 2311.42, 1370L, 1, 0.0)
                 .returningResult(SALE.getIdentity().getField())
                 // or, .returningResult(SALE.SALE_ID)
                 .fetchOne(); // get directly the long value, .fetchOne().value1();
@@ -46,10 +47,11 @@ public class ClassicModelsRepository {
         System.out.println("Inserted ID:\n" + insertedId);
 
         // Result<Record1<Long>>
-        var insertedIds = ctx.insertInto(SALE, SALE.FISCAL_YEAR, SALE.SALE_, SALE.EMPLOYEE_NUMBER)
-                .values(2004, 2311.42, 1370L)
-                .values(2003, 900.21, 1504L)
-                .values(2005, 1232.2, 1166L)
+        var insertedIds = ctx.insertInto(SALE, SALE.FISCAL_YEAR, SALE.SALE_, SALE.EMPLOYEE_NUMBER,
+                SALE.FISCAL_MONTH, SALE.REVENUE_GROWTH)
+                .values(2004, 2311.42, 1370L, 1, 0.0)
+                .values(2003, 900.21, 1504L, 1, 0.0)
+                .values(2005, 1232.2, 1166L, 1, 0.0)
                 .returningResult(SALE.getIdentity().getField())
                 // or, .returningResult(SALE.SALE_ID)
                 .fetch();
@@ -57,8 +59,9 @@ public class ClassicModelsRepository {
         System.out.println("Inserted IDs:\n" + insertedIds);                
         
         // use lastID()
-        ctx.insertInto(SALE, SALE.FISCAL_YEAR, SALE.SALE_, SALE.EMPLOYEE_NUMBER)
-                .values(2002, 5411.42, 1504L)
+        ctx.insertInto(SALE, SALE.FISCAL_YEAR, SALE.SALE_, SALE.EMPLOYEE_NUMBER,
+                SALE.FISCAL_MONTH, SALE.REVENUE_GROWTH)
+                .values(2002, 5411.42, 1504L, 1, 0.0)
                 .execute();
         
         // PAY ATTENTION TO THE FACT THAT, MEANWHILE, A CONCURRENT TRANSACTION CAN MODIFY THE CURRENT VALUE
@@ -81,6 +84,8 @@ public class ClassicModelsRepository {
         sr.setFiscalYear(2021);
         sr.setSale(4500.25);
         sr.setEmployeeNumber(1504L);
+        sr.setFiscalMonth(1);
+        sr.setRevenueGrowth(0.0);
 
         sr.insert();
 
@@ -91,6 +96,8 @@ public class ClassicModelsRepository {
         s.setFiscalYear(2020);
         s.setSale(643.23);
         s.setEmployeeNumber(1370L);
+        s.setFiscalMonth(1);
+        s.setRevenueGrowth(0.0);
         
         saleRepository.insert(s);
         
@@ -109,6 +116,8 @@ public class ClassicModelsRepository {
         srNoReturnId.setFiscalYear(2021);
         srNoReturnId.setSale(4500.25);
         srNoReturnId.setEmployeeNumber(1504L);
+        srNoReturnId.setFiscalMonth(1);
+        srNoReturnId.setRevenueGrowth(0.0);
 
         srNoReturnId.insert();
 
@@ -159,64 +168,58 @@ public class ClassicModelsRepository {
 
         // Avoid: ERROR: ORA-08002: sequence SALE_SEQ.CURRVAL is not yet defined in this session
         // SALE_SEQ.nextval(); - you can call this, but an INSERT will also call NEXTVAL
-        ctx.insertInto(SALE, SALE.FISCAL_YEAR, SALE.SALE_, SALE.EMPLOYEE_NUMBER)
-                .values(2020, 900.25, 1611L)
+        ctx.insertInto(SALE, SALE.FISCAL_YEAR, SALE.SALE_, SALE.EMPLOYEE_NUMBER,
+                SALE.FISCAL_MONTH, SALE.REVENUE_GROWTH)
+                .values(2020, 900.25, 1611L, 1, 0.0)
                 .execute();
 
         // PAY ATTENTION TO THE FACT THAT, MEANWHILE, A CONCURRENT TRANSACTION CAN MODIFY THE CURRENT VALUE
         // SO, THERE IS NO GUARANTEE THAT THE BELOW FETCHED *cr* IS THE PRIMARY KEY OF THE PREVIOUS INSERT
         // IF YOU NEED THE PK OF THE ABOVE INSERT THEN RELY ON INSERT ... RETURNING
-        /* var pk = ctx.insertInto(SALE)
-                .values(default_(), 2020, 900.25, 1611L, 
-                        default_(), RateType.GOLD, VatType.MIN, default_())
+        /* var pk = ctx.insertInto(SALE, SALE.FISCAL_YEAR, SALE.SALE_, SALE.EMPLOYEE_NUMBER,
+                SALE.FISCAL_MONTH, SALE.REVENUE_GROWTH)
+                .values(2020, 900.25, 1611L, 1, 0.0)
                 .returningResult(SALE.SALE_ID)
                 .execute(); 
          */
-        var cr = ctx.select(SALE_SEQ.currval()).fetchSingle().value1();
-        long crLong = ctx.select(SALE_SEQ.currval()).fetchSingleInto(Long.class); // or, fetchOneInto()
+        var cr = ctx.select(PRODUCT_UID_SEQ.currval()).fetchSingle().value1();
+        long crLong = ctx.select(PRODUCT_UID_SEQ.currval()).fetchSingleInto(Long.class); // or, fetchOneInto()
 
-        // UPDATE the SALE having as ID the fetched *cr* 
+        // UPDATE the PRODUCT having as PRODUCT_UID the fetched *cr* 
         // (it is possible that this is not the current value anymore)
-        ctx.update(SALE)
-                .set(SALE.FISCAL_YEAR, 2005)
-                .where(SALE.SALE_ID.eq(cr))
+        ctx.update(PRODUCT)
+                .set(PRODUCT.QUANTITY_IN_STOCK, 0)
+                .where(PRODUCT.PRODUCT_UID.le(cr))
                 .execute();
 
-        // DELETE the SALE having as ID the fetched *cr* 
+        // DELETE the PRODUCT having as ID the fetched *cr* 
         // (it is possible that this is not the current value anymore)        
-        ctx.deleteFrom(SALE)
-                .where(SALE.SALE_ID.eq(cr))
+        ctx.deleteFrom(PRODUCT)
+                .where(PRODUCT.PRODUCT_UID.eq(cr))
                 .execute();
 
         // this is prone to the same issue because it results in a SELECT and a DELETE and,
         // between them a concurrent transaction can affect the current value
-        ctx.deleteFrom(SALE)
-                .where(SALE.SALE_ID.eq(ctx.select(SALE_SEQ.currval()).fetchSingle().value1()))
+        ctx.deleteFrom(PRODUCT)
+                .where(PRODUCT.PRODUCT_UID.eq(ctx.select(PRODUCT_UID_SEQ.currval()).fetchSingle().value1()))
                 .execute();
         
         // this is not prone to the same issue because there will be a single UPDATE/DELETE, so
         // this time, you'll affect the latest current value, whichever that value is
-        ctx.update(SALE)
-                .set(SALE.FISCAL_YEAR, 2005)
-                .where(SALE.SALE_ID.eq(SALE_SEQ.currval()))
+        ctx.update(PRODUCT)
+                .set(PRODUCT.QUANTITY_IN_STOCK, 0)
+                .where(PRODUCT.PRODUCT_UID.eq(PRODUCT_UID_SEQ.currval()))
                 .execute();
         
-        ctx.deleteFrom(SALE)
-                .where(SALE.SALE_ID.eq(SALE_SEQ.currval()))
+        ctx.deleteFrom(PRODUCT)
+                .where(PRODUCT.PRODUCT_UID.eq(PRODUCT_UID_SEQ.currval()))
                 .execute();
     }  
 
     @Transactional
-    public void nextSequenceVal() {
+    public void nextSequenceVal() {        
 
-        // For a SEQUENCE that is auto-generated from a (BIG)SERIAL or is set as 
-        // default (e.g., NOT NULL DEFAULT NEXTVAL ('sale_seq')) there is no need 
-        // to call currval() or nextval(). Simply omit the PK and let the database 
-        // to generate it.
-        ctx.insertInto(SALE, SALE.FISCAL_YEAR, SALE.EMPLOYEE_NUMBER, SALE.SALE_)
-                .values(2005, 1370L, 1282.64D);
-
-        // But, for SEQUENCE owned by non-auto-generated rows, you have to rely on nextval()/nextvals()
+        // For SEQUENCE owned by non-auto-generated rows, you have to rely on nextval()/nextvals()
         // For instance, you can INSERT 10 employees via *EMPLOYEE_SEQ.nextval()* 
         for (int i = 0; i < 10; i++) {
             ctx.insertInto(EMPLOYEE, EMPLOYEE.EMPLOYEE_NUMBER, EMPLOYEE.LAST_NAME, EMPLOYEE.FIRST_NAME,
@@ -233,9 +236,12 @@ public class ClassicModelsRepository {
         List<Long> idsLong = ctx.fetch(EMPLOYEE_SEQ.nextvals(10)).into(Long.class);
 
         // This is also useful for Records to pre-set IDs:
-        // EmployeeRecord er = new EmployeeRecord(ids.get(0).value1(), // or, idsLong.get(0)
-        //        "Lionel", "Andre", "x8990", "landre@gmail.com", "1", 
-        //                57000, 1143L, "Sales Rep", null, null);        
+        /*
+         EmployeeRecord er = new EmployeeRecord(ids.get(0).value1(), // or, idsLong.get(0) 
+                "Lionel", "Andre", "x8990", "landre@gmail.com", "1", 
+                        57000, 0, 1143L, "Sales Rep", null, null);        
+        */
+        
         for (int i = 0; i < ids.size(); i++) {
 
             ctx.insertInto(EMPLOYEE, EMPLOYEE.EMPLOYEE_NUMBER, EMPLOYEE.LAST_NAME, EMPLOYEE.FIRST_NAME,
@@ -261,7 +267,7 @@ public class ClassicModelsRepository {
 
         var result2 = ctx.selectFrom(PRODUCTLINE)
                 .where(row(PRODUCTLINE.PRODUCT_LINE, PRODUCTLINE.CODE)
-                        .eq(row("Classic Cars", 599302L)))
+                        .eq(row("Classic Cars", 599302L))) // or, .eq("Classic Cars", 599302L))
                 .fetchSingle();
         System.out.println("Result 2:\n" + result2);
         
