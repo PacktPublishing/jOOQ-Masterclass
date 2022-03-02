ed3c7b208eb8bbcd63c0961fb1fb416f75450605
anghelleonard <leoprivacy@yahoo.com>
2022-03-02 13:52:47 +0200

    jOOQ Keys

--- Chapter11/PostgreSQL/Maven/DetectIdentity/src/main/java/com/classicmodels/MainApplication.java
+++ Chapter11/PostgreSQL/Maven/DetectIdentity/src/main/java/com/classicmodels/MainApplication.java
@@ -4,11 +4,10 @@ import com.classicmodels.service.ClassicModelsService;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.r2dbc.R2dbcAutoConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
-@ComponentScan(basePackages = { "jooq.generated.tables.daos", "com.classicmodels.*" })
+@SpringBootApplication(exclude = {R2dbcAutoConfiguration.class})
 public class MainApplication {
 
     private final ClassicModelsService classicModelsService;
