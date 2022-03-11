4a2c82c8307a63ec8a6516ecddbfaf386fc5e67f
anghelleonard <leoprivacy@yahoo.com>
2022-03-11 18:22:08 +0200

    Exploiting SQL functions

---
+++ Chapter13/SQLServer/Gradle/WindowFunctions/LeadLag/src/main/java/com/classicmodels/service/ClassicModelsService.java
@@ -0,0 +1,28 @@
+package com.classicmodels.service;
+
+import com.classicmodels.repository.ClassicModelsRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClassicModelsService {
+
+    private final ClassicModelsRepository classicModelsRepository;
+
+    public ClassicModelsService(ClassicModelsRepository classicModelsRepository) {
+        this.classicModelsRepository = classicModelsRepository;
+    }
+
+    public void callAll() {
+
+        classicModelsRepository.leadLagOrder();
+        classicModelsRepository.leadSalaryByOffice();
+
+        classicModelsRepository.lagYOY();
+        classicModelsRepository.monthOverMonthGrowthRateSale();
+
+        classicModelsRepository.leadLagSalary();        
+        classicModelsRepository.employeeFunnel();
+        
+        classicModelsRepository.timeSeriesAnalysis();
+    }
+}
