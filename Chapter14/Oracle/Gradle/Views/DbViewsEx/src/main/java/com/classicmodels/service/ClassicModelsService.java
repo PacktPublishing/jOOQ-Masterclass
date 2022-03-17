7d2003a5a5a8b4f5be7242050e3be53ce91bdf56
anghelleonard <leoprivacy@yahoo.com>
2022-03-17 13:11:04 +0200

    Derived Tables, CTE, and Views

---
+++ Chapter14/Oracle/Gradle/Views/DbViewsEx/src/main/java/com/classicmodels/service/ClassicModelsService.java
@@ -0,0 +1,21 @@
+package com.classicmodels.service;
+
+import com.classicmodels.repository.ClassicModelsRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClassicModelsService {
+
+    private final ClassicModelsRepository classicModelsRepository;
+
+    public ClassicModelsService(ClassicModelsRepository classicModelsRepository) {
+        this.classicModelsRepository = classicModelsRepository;
+    }
+
+    public void callAll() {
+
+        classicModelsRepository.view1();        
+        classicModelsRepository.view2();        
+        classicModelsRepository.view3();        
+    }
+}
