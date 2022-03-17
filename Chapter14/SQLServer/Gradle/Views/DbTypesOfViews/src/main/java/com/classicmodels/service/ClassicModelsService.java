f1b668187a0d531ca1df3afd31e88c5fbb94adbe
anghelleonard <leoprivacy@yahoo.com>
2022-03-17 09:32:08 +0200

    Derived Tables, CTE, and Views

---
+++ Chapter14/SQLServer/Gradle/Views/DbTypesOfViews/src/main/java/com/classicmodels/service/ClassicModelsService.java
@@ -0,0 +1,24 @@
+package com.classicmodels.service;
+
+import com.classicmodels.repository.ClassicModelsRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClassicModelsService {
+
+    private final ClassicModelsRepository classicModelsRepository;
+
+    public ClassicModelsService(ClassicModelsRepository classicModelsRepository) {
+        this.classicModelsRepository = classicModelsRepository;
+    }
+
+    public void callAll() {
+
+        classicModelsRepository.stprView();
+        classicModelsRepository.ccView();
+        classicModelsRepository.tcView();
+        classicModelsRepository.groupedView();        
+        classicModelsRepository.unionedView();
+        classicModelsRepository.nestedView();
+    }
+}
