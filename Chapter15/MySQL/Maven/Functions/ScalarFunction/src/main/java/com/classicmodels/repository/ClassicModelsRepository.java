4580840e4c7150f287397090bf3f6f20bd970c28
anghelleonard <leoprivacy@yahoo.com>
2022-03-18 17:31:16 +0200

    Calling and Creating Stored Functions and Procedures

--- Chapter15/MySQL/Maven/Functions/ScalarFunction/src/main/java/com/classicmodels/repository/ClassicModelsRepository.java
+++ Chapter15/MySQL/Maven/Functions/ScalarFunction/src/main/java/com/classicmodels/repository/ClassicModelsRepository.java
@@ -19,7 +19,7 @@ import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 @Repository
-@Transactional(readOnly = true)
+@Transactional
 public class ClassicModelsRepository {
 
     private final DSLContext ctx;
