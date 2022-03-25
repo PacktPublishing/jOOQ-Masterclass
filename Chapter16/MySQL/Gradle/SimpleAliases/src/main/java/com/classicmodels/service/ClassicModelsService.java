f6b89bd97479cf7c190acc8d31900ddb9db0b758
anghelleonard <leoprivacy@yahoo.com>
2022-03-25 15:09:11 +0200

    Tackling aliases and SQL templating

---
+++ Chapter16/MySQL/Gradle/SimpleAliases/src/main/java/com/classicmodels/service/ClassicModelsService.java
@@ -0,0 +1,27 @@
+package com.classicmodels.service;
+
+import com.classicmodels.repository.ClassicModelsRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClassicModelsService {
+
+    private final ClassicModelsRepository classicModelsRepository;
+
+    public ClassicModelsService(ClassicModelsRepository classicModelsRepository) {
+        this.classicModelsRepository = classicModelsRepository;
+    }
+
+    public void callAll() {
+
+        classicModelsRepository.simpleTableAlias();
+        classicModelsRepository.simpleColumnAlias();
+
+        classicModelsRepository.tableEquality();
+        classicModelsRepository.fieldEquality();
+
+        classicModelsRepository.simpleSelectAndAs();
+        
+        classicModelsRepository.renderQuotedAndCase();
+    }
+}
