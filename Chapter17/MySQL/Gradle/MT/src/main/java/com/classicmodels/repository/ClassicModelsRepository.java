d511cbb758739a7cfbaa7995f134977f8f2c6f48
anghelleonard <leoprivacy@yahoo.com>
2022-03-27 16:57:20 +0300

    Multitenancy in jOOQ

---
+++ Chapter17/MySQL/Gradle/MT/src/main/java/com/classicmodels/repository/ClassicModelsRepository.java
@@ -0,0 +1,43 @@
+package com.classicmodels.repository;
+
+import static jooq.generated.tables.Product.PRODUCT;
+import org.jooq.DSLContext;
+import org.jooq.conf.MappedSchema;
+import org.jooq.conf.RenderMapping;
+import org.jooq.conf.Settings;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+@Transactional(readOnly = true)
+public class ClassicModelsRepository {
+
+    private final DSLContext ctx;
+
+    public ClassicModelsRepository(DSLContext ctx) {
+        this.ctx = ctx;
+    }
+
+    @Transactional
+    public void tenant() {
+
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+        if (auth != null && auth.isAuthenticated()) {
+            
+            String authority = auth.getAuthorities().iterator().next().getAuthority();
+            String database = authority.substring(5).toLowerCase();
+
+            ctx.configuration().derive(new Settings()
+                    .withRenderMapping(new RenderMapping()
+                            .withSchemata(
+                                    new MappedSchema().withInput("development")
+                                            .withOutput(database)))).dsl()
+                    .insertInto(PRODUCT, PRODUCT.PRODUCT_NAME, PRODUCT.QUANTITY_IN_STOCK)
+                    .values("Product", 100)
+                    .execute();
+        }
+    }
+}
