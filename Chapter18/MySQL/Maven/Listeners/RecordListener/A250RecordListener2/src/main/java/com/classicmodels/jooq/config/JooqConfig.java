d28e8b7c2b3b3d444cfdc365ba9862bbdd835c27
anghelleonard <leoprivacy@yahoo.com>
2022-03-30 08:22:30 +0300

    jOOQ SPI (providers and listeners)

---
+++ Chapter18/MySQL/Maven/Listeners/RecordListener/A250RecordListener2/src/main/java/com/classicmodels/jooq/config/JooqConfig.java
@@ -0,0 +1,26 @@
+package com.classicmodels.jooq.config;
+
+import com.classicmodels.listener.MyRecordListener;
+import org.jooq.impl.DefaultConfiguration;
+import org.jooq.impl.DefaultRecordListenerProvider;
+import org.springframework.boot.autoconfigure.jooq.DefaultConfigurationCustomizer;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JooqConfig implements DefaultConfigurationCustomizer {
+
+    @Override
+    public void customize(DefaultConfiguration configuration) {
+
+        configuration.set(new DefaultRecordListenerProvider(new MyRecordListener()));
+        
+        // or
+        // configuration.set(new DefaultRecordListenerProvider(new MyRecordListener1()),
+        //                   new DefaultRecordListenerProvider(new MyRecordListener2()),
+        //                   ...);        
+        // configuration.setRecordListener(new MyRecordListener1(), new MyRecordListener2(), ...);
+        // configuration.set(new MyRecordListener());
+        // configuration.set(new MyRecordListener1(), new MyRecordListener2(), ...);
+    }
+
+}
