a011ff57ffc0ec73a5ea1e26b48b7e3bfb209206
anghelleonard <leoprivacy@yahoo.com>
2022-08-15 08:27:47 +0300

    jOOQ Masterclass

---
+++ BONUS/DeleteDuplicatesRowId/src/main/java/com/classicmodels/repository/ClassicModelsRepository.java
@@ -0,0 +1,31 @@
+package com.classicmodels.repository;
+
+import static jooq.generated.tables.Order.ORDER;
+import static jooq.generated.tables.Orderdetail.ORDERDETAIL;
+import org.jooq.DSLContext;
+import static org.jooq.impl.DSL.min;
+import static org.jooq.impl.DSL.select;
+import static org.jooq.util.oracle.OracleDSL.rowid;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+@Transactional(readOnly = true)
+public class ClassicModelsRepository {
+
+    private final DSLContext ctx;
+
+    public ClassicModelsRepository(DSLContext ctx) {
+        this.ctx = ctx;
+    }
+
+    // Inspired by: https://blogs.oracle.com/sql/post/how-to-find-and-delete-duplicate-rows-with-sql       
+    public void delete() {
+
+        ctx.deleteFrom(ORDERDETAIL)
+                .where(rowid().notIn(select(min(rowid()))
+                        .from(ORDERDETAIL)
+                        .groupBy(ORDERDETAIL.PRODUCT_ID, ORDERDETAIL.QUANTITY_ORDERED)))
+                .execute();
+    }
+}
